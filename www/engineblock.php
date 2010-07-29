24f530e8ddf3bec59e8a4e5fd2763c3963647bfa
Boy Baukema <boy.baukema@flex.surfnet.nl>
2010-07-29 15:25:15 +0000

    Improved handing of requests with no query string
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@11 528793b8-127e-4309-9901-b74759c7208d

--- www/engineblock.php
+++ www/engineblock.php
@@ -45,10 +45,57 @@ function engineBlockSetupMetadata()
     }
 }
 
+function engineBlockOutfilter(array $metaData, array $response, array &$attributes)
+{
+    engineBlockEnrichAttributes($attributes);
+    engineBlockRegisterUser($attributes);
+}
 
-function engineBlockEnrichAttributes(array $metaData, array $response, array &$attributes)
+function engineBlockEnrichAttributes(array &$attributes)
 {
     $attributes['over-18'] = array('yup');
+    $attributes['coin-team-member'] = array('yes');
+}
+
+function engineBlockRegisterUser($attributes)
+{
+    if (!defined('ENGINEBLOCK_USER_DB_DSN') && ENGINEBLOCK_USER_DB_DSN) {
+        return false;
+    }
+
+    $uid = $attributes['uid'][0];
+
+    $dbh = new PDO(ENGINEBLOCK_USER_DB_DSN, ENGINEBLOCK_USER_DB_USER, ENGINEBLOCK_USER_DB_PASSWORD);
+    $statement = $dbh->prepare("INSERT INTO `users` (uid, last_seen) VALUES (?, NOW()) ON DUPLICATE KEY UPDATE last_seen = NOW()");
+    $statement->execute(array($uid));
+
+    $sqlValues = array();
+    $bindValues = array('uid'=>$uid);
+
+    $nameCount = 1;
+    $valueCount = 1;
+    foreach ($attributes as $attributeName => $attributeValues) {
+        if ($attributeName==='uid') {
+            continue;
+        }
+
+        $bindValues['attributename' . $nameCount] = $attributeName;
+
+        foreach ($attributeValues as $attributeValue) {
+            $sqlValues[] = "(:uid, :attributename{$nameCount}, :attributevalue{$valueCount})";
+            $bindValues['attributevalue' . $valueCount] = $attributeValue;
+            $valueCount++;
+        }
+        $nameCount++;
+    }
+    
+    // No other attributes than uid found
+    if (empty($sqlValues)) {
+        return false;
+    }
+
+    $statement = $dbh->prepare("INSERT IGNORE INTO `user_attributes` (`user_uid`, `name`, `value`) VALUES " . implode(',', $sqlValues));
+    $statement->execute($bindValues);
 }
 
 function engineBlockTranslateAttributeName($name)
