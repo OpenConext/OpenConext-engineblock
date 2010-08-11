7b0623713e461d2efe6d68bdca1a341bed709946
Hans Zandbelt <hans.zandbelt@surfnet.nl>
2010-08-11 12:30:14 +0000

    correct autloading filename
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@40 528793b8-127e-4309-9901-b74759c7208d

--- tests/autloading.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require dirname(__FILE__).'/../library/EngineBlock/ApplicationSingleton.php';
-
-$application = EngineBlock_ApplicationSingleton::getInstance();
-spl_autoload_register(array($application, 'autoLoad'));
