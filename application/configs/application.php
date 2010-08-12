9edf45f1aec4fe57f31107a352f4867b13f8265b
Hans Zandbelt <hans.zandbelt@surfnet.nl>
2010-08-12 12:27:54 +0000

    basic implementation of getUser and addUser for UserDirectory based on LDAP (add config too)
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@48 528793b8-127e-4309-9901-b74759c7208d

--- application/configs/application.php
+++ application/configs/application.php
@@ -2,5 +2,16 @@
 
 $config = array(
     'default_timezone' => 'Europe/Amsterdam',
-    
+
+    // TODO: sort out the minimal set of required Zend LDAP settings (for now these just work...)
+    'ldap' => array(
+    	'host'              => 'coin-db.dev.coin.surf.net',
+        'useSsl'			=> TRUE,
+    	'username'          => 'cn=engineblock,dc=coin,dc=surfnet,dc=nl',
+    	'password'          => '631E9383FD20',
+    	'bindRequiresDn'    => true,
+    	'accountDomainName' => 'dev.coin.surf.net',
+    	'baseDn'            => 'dc=coin,dc=surfnet,dc=nl',
+    ),
+
 );
