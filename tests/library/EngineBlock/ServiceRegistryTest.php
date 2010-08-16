0a0840f63bfb07a743fd2165f0f59384cbce8b05
Boy Baukema <boy.baukema@flex.surfnet.nl>
2010-08-16 13:37:28 +0000

    Replaced tabs by spaces
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@61 528793b8-127e-4309-9901-b74759c7208d

--- tests/library/EngineBlock/ServiceRegistryTest.php
+++ tests/library/EngineBlock/ServiceRegistryTest.php
@@ -17,115 +17,113 @@ require_once 'EngineBlock/ServiceRegistry.php';
  */
 class EngineBlock_ServiceRegistryTest extends PHPUnit_Framework_TestCase 
 {
-	
-	/**
-	 * @var EngineBlock_ServiceRegistry
-	 */
-	private $EngineBlock_ServiceRegistry;
-	
-	/**
-	 * Prepares the environment before running a test.
-	 */
-	protected function setUp() 
-	{
-		parent::setUp ();
-		
-		// TODO Auto-generated ServiceRegistryTest::setUp()
-		
+    
+    /**
+     * @var EngineBlock_ServiceRegistry
+     */
+    private $EngineBlock_ServiceRegistry;
+    
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp() 
+    {
+        parent::setUp ();
+        
+        // TODO Auto-generated ServiceRegistryTest::setUp()
+        
 
-		$this->EngineBlock_ServiceRegistry = new EngineBlock_ServiceRegistry(/* parameters */);
-	    $this->EngineBlock_ServiceRegistry->setRestClient(new RestClientMock());
-	}
-	
-	/**
-	 * Cleans up the environment after running a test.
-	 */
-	protected function tearDown() 
-	{
-		// TODO Auto-generated ServiceRegistryTest::tearDown()
-		
+        $this->EngineBlock_ServiceRegistry = new EngineBlock_ServiceRegistry(/* parameters */);
+        $this->EngineBlock_ServiceRegistry->setRestClient(new RestClientMock());
+    }
+    
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown() 
+    {
+        // TODO Auto-generated ServiceRegistryTest::tearDown()
+        
 
-		$this->EngineBlock_ServiceRegistry = null;
-		
-		parent::tearDown ();
-	}
-	
-	/**
-	 * Constructs the test case.
-	 */
-	public function __construct() 
-	{
-		// TODO Auto-generated constructor
-	}
-	
-	/**
-	 * Tests EngineBlock_ServiceRegistry->getMetadata()
-	 */
-	public function testGetMetadata() 
-	{
-		
-	   $result = $this->EngineBlock_ServiceRegistry->getMetadata("http://ivotestsp.local");
-	   $this->assertTrue(is_array($result));
-	   $this->assertEquals("A description", $result["description:en"]);
-	}
-	
-	public function testGetMetaDataForKey() 
-	{
-		
-		$result = $this->EngineBlock_ServiceRegistry->getMetaDataForKey("http://ivotestsp.local", "certData");
-		$this->assertTrue(is_string($result));	
-		$this->assertEquals("aaaaabbbbb", $result);	
-	}
-	
-	public function testGetMetaDataForKeys() 
-	{
-		$result = $this->EngineBlock_ServiceRegistry->getMetaDataForKeys("http://ivotestsp.local", array("name:en", "description:en"));
-		$this->assertEquals(2, count($result));
-		$this->assertEquals("Ivo's SP", $result["name:en"]);
-		$this->assertEquals("A description", $result["description:en"]);
-	}
-	
-	public function testIsConnectionAllowed() 
-	{
-		$result = $this->EngineBlock_ServiceRegistry->isConnectionAllowed("http://ivotestsp.local", "http://doesntexist.local");
-		$this->assertFalse($result);
-		
-		$result = $this->EngineBlock_ServiceRegistry->isConnectionAllowed("http://ivotestsp.local", "http://ivoidp");
-		$this->assertTrue($result);
-	}
-	
-	public function testGetArp()
-	{
-		$result = $this->EngineBlock_ServiceRegistry->getArp("http://ivotestsp.local");
+        $this->EngineBlock_ServiceRegistry = null;
+        
+        parent::tearDown ();
+    }
+    
+    /**
+     * Constructs the test case.
+     */
+    public function __construct() 
+    {
+        // TODO Auto-generated constructor
+    }
+    
+    /**
+     * Tests EngineBlock_ServiceRegistry->getMetadata()
+     */
+    public function testGetMetadata() 
+    {
+        
+       $result = $this->EngineBlock_ServiceRegistry->getMetadata("http://ivotestsp.local");
+       $this->assertTrue(is_array($result));
+       $this->assertEquals("A description", $result["description:en"]);
+    }
+    
+    public function testGetMetaDataForKey() 
+    {
+        
+        $result = $this->EngineBlock_ServiceRegistry->getMetaDataForKey("http://ivotestsp.local", "certData");
+        $this->assertTrue(is_string($result));    
+        $this->assertEquals("aaaaabbbbb", $result);    
+    }
+    
+    public function testGetMetaDataForKeys() 
+    {
+        $result = $this->EngineBlock_ServiceRegistry->getMetaDataForKeys("http://ivotestsp.local", array("name:en", "description:en"));
+        $this->assertEquals(2, count($result));
+        $this->assertEquals("Ivo's SP", $result["name:en"]);
+        $this->assertEquals("A description", $result["description:en"]);
+    }
+    
+    public function testIsConnectionAllowed() 
+    {
+        $result = $this->EngineBlock_ServiceRegistry->isConnectionAllowed("http://ivotestsp.local", "http://doesntexist.local");
+        $this->assertFalse($result);
+        
+        $result = $this->EngineBlock_ServiceRegistry->isConnectionAllowed("http://ivotestsp.local", "http://ivoidp");
+        $this->assertTrue($result);
+    }
+    
+    public function testGetArp()
+    {
+        $result = $this->EngineBlock_ServiceRegistry->getArp("http://ivotestsp.local");
         $this->assertEquals(3, count($result));
         $this->assertEquals("someArp", $result["name"]);
         $this->assertEquals(4, count($result["attributes"]));
         $this->assertEquals("sn", $result["attributes"][0]);
     
-	}
-	
-	public function testFindIdentifiersByMetadata()
-	{
-		$result = $this->EngineBlock_ServiceRegistry->findIdentifiersByMetadata("url:en", "www.google.com");
+    }
+    
+    public function testFindIdentifiersByMetadata()
+    {
+        $result = $this->EngineBlock_ServiceRegistry->findIdentifiersByMetadata("url:en", "www.google.com");
         $this->assertEquals(1, count($result));
         $this->assertEquals("http://ivotestsp.local", $result[0]);
       
-	}
-	
-	public function testGetIdpList()
-	{
-		$result = $this->EngineBlock_ServiceRegistry->getIdpList();
-		$this->assertEquals(2, count($result));
-		$this->assertEquals("Ivo's IDP", $result["http://ivotestidp.local"]["name:en"]);
-	}
-	
+    }
+    
+    public function testGetIdpList()
+    {
+        $result = $this->EngineBlock_ServiceRegistry->getIdpList();
+        $this->assertEquals(2, count($result));
+        $this->assertEquals("Ivo's IDP", $result["http://ivotestidp.local"]["name:en"]);
+    }
+    
     public function testGetSpList()
     {
         $result = $this->EngineBlock_ServiceRegistry->getSpList();
         $this->assertEquals(2, count($result));
         $this->assertEquals("Ivo's SP", $result["http://ivotestsp.local"]["name:en"]);
     }
-	
-
 }
 
