fe642ef512cd39226d6e045cddfcec48852aaa33
Boy Baukema <boy.baukema@flex.surfnet.nl>
2010-08-30 12:38:10 +0000

    Minor doc and code layout fixes
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@119 528793b8-127e-4309-9901-b74759c7208d

--- application/modules/Social/Controller/Rest.php
+++ application/modules/Social/Controller/Rest.php
@@ -6,22 +6,22 @@ class Social_Controller_Rest extends EngineBlock_Controller_Abstract
 {
     public function indexAction($url)
     {
-    	$this->setNoRender(); // let shindig do the rendering
-    	
+        $this->setNoRender(); // let shindig do the rendering
+        
         set_include_path(get_include_path() . PATH_SEPARATOR . ENGINEBLOCK_FOLDER_SHINDIG);
         
         include_once('src/common/Config.php');
         include_once('src/common/File.php');
- 
-        // You can't inject a Config, so force it
-        // to try loading and ignore errors from config file not being there :(
+
+        // You can't inject a Config, so force it to try loading
+        // and ignore errors from config file not being there :(
         global $shindigConfig;
         $shindigConfig = array();
         @Config::setConfig(array('allow_plaintext_token'=>true,
-                                 'person_service' => 'EngineBlock_OpenSocial_ShindigService',
-								 'activity_service' => 'EngineBlock_OpenSocial_ShindigService',
-					             'group_service' => 'EngineBlock_OpenSocial_ShindigService',
-								 ));
+                                 'person_service'       => 'EngineBlock_OpenSocial_ShindigService',
+                                 'activity_service'     => 'EngineBlock_OpenSocial_ShindigService',
+                                 'group_service'        => 'EngineBlock_OpenSocial_ShindigService',
+        ));
 
         spl_autoload_register(array(get_class($this), 'shindigAutoLoad'));
         
@@ -39,7 +39,7 @@ class Social_Controller_Rest extends EngineBlock_Controller_Abstract
             $servletInstance->$methodName();
         }
         else {
-        	echo "Invalid method";
+            echo "Invalid method";
             // @todo Error out
         }
     }
@@ -69,7 +69,7 @@ class Social_Controller_Rest extends EngineBlock_Controller_Abstract
             $locations = array_merge(explode(',', $extensionClassPaths), $locations);
         }
 
-        // Check for the presense of this class in our all our directories.
+        // Check for the presence of this class in our all our directories.
         $fileName = $className . '.php';
         foreach ($locations as $path) {
             if (file_exists(ENGINEBLOCK_FOLDER_SHINDIG."{$path}/$fileName")) {
