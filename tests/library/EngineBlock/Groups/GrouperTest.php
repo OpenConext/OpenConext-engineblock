6941854d4041d191641fb821ad21409b63535a1a
Boy Baukema <boy.baukema@flex.surfnet.nl>
2010-09-03 09:41:00 +0000

    Disabled grouper test, doesnt work when there is no grouper to connect to
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@145 528793b8-127e-4309-9901-b74759c7208d

--- tests/library/EngineBlock/Groups/GrouperTest.php
+++ tests/library/EngineBlock/Groups/GrouperTest.php
@@ -18,44 +18,44 @@ class EngineBlock_Groups_GrouperTest extends PHPUnit_Framework_TestCase
     /**
      * Prepares the environment before running a test.
      */
-    protected function setUp()
-    {
-        parent::setUp();
-        
-        $application = EngineBlock_ApplicationSingleton::getInstance();
-        $config = array();
-        require ENGINEBLOCK_FOLDER_APPLICATION . 'configs/application.php';
-        $application->setConfiguration($config['ivodev']);
-    
-        
-        // TODO Auto-generated EngineBlock_Groups_GrouperTest::setUp()
-        $this->EngineBlock_Groups_Grouper = new EngineBlock_Groups_Grouper(/* parameters */);
-    }
-
-    /**
-     * Cleans up the environment after running a test.
-     */
-    protected function tearDown()
-    {
-        // TODO Auto-generated EngineBlock_Groups_GrouperTest::tearDown()
-        $this->EngineBlock_Groups_Grouper = null;
-        parent::tearDown();
-    }
-
-    /**
-     * Constructs the test case.
-     */
-    public function __construct()
-    {    // TODO Auto-generated constructor
-    }
-
-    /**
-     * Tests EngineBlock_Groups_Grouper->getGroups()
-     */
-    public function testGetGroups()
-    {
-        $groups = $this->EngineBlock_Groups_Grouper->getGroups("urn:collab:person:fontys.nl:874501@fontys.nl");
-        var_dump($groups);
-    }
+//    protected function setUp()
+//    {
+//        parent::setUp();
+//
+//        $application = EngineBlock_ApplicationSingleton::getInstance();
+//        $config = array();
+//        require ENGINEBLOCK_FOLDER_APPLICATION . 'configs/application.php';
+//        $application->setConfiguration($config['ivodev']);
+//
+//
+//        // TODO Auto-generated EngineBlock_Groups_GrouperTest::setUp()
+//        $this->EngineBlock_Groups_Grouper = new EngineBlock_Groups_Grouper(/* parameters */);
+//    }
+//
+//    /**
+//     * Cleans up the environment after running a test.
+//     */
+//    protected function tearDown()
+//    {
+//        // TODO Auto-generated EngineBlock_Groups_GrouperTest::tearDown()
+//        $this->EngineBlock_Groups_Grouper = null;
+//        parent::tearDown();
+//    }
+//
+//    /**
+//     * Constructs the test case.
+//     */
+//    public function __construct()
+//    {    // TODO Auto-generated constructor
+//    }
+//
+//    /**
+//     * Tests EngineBlock_Groups_Grouper->getGroups()
+//     */
+//    public function testGetGroups()
+//    {
+//        $groups = $this->EngineBlock_Groups_Grouper->getGroups("urn:collab:person:fontys.nl:874501@fontys.nl");
+//        var_dump($groups);
+//    }
 }
 
