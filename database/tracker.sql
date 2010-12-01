48848c0fd939fa887278766987ffa1fdd7e84cb3
Boy Baukema <boy.baukema@flex.surfnet.nl>
2010-12-01 14:08:49 +0000

    Removed obsolete patch file
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@296 528793b8-127e-4309-9901-b74759c7208d

--- database/tracker.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `log_logins` (
-  `loginstamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `userid` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `spentityid` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `idpentityid` varchar(255) collate utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
