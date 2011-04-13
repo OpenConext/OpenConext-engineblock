2f5142c0748d29bb11dd7138e0fee68b7dc828a0
Boy Baukema <boy.baukema@flex.surfnet.nl>
2011-04-13 09:00:52 +0000

    Fixed rejects file handling
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/serviceregistry/branches/1.8.0@458 528793b8-127e-4309-9901-b74759c7208d

--- bin/apply_janus_patches.sh
+++ bin/apply_janus_patches.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+REJFILE="/tmp/janus_patches-rej-$(date +%s)"
 for FILENAME in janus_patches/*.patch
 do
-  patch -Np0 -r /dev/null < $FILENAME
+  patch -Np0 -r $REJFILE < $FILENAME
 done
+rm $REJFILE
+echo "Removed $REJFILE"
