c92dacbc8fe4e102cf0d80f39bae764c0499b883
Boy Baukema <boy.baukema@flex.surfnet.nl>
2011-05-02 14:08:12 +0000

    Updated config for new cron structure
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/serviceregistry/trunk@499 528793b8-127e-4309-9901-b74759c7208d

--- config/module_serviceregistry.php
+++ config/module_serviceregistry.php
@@ -4,5 +4,8 @@
  */
 
 $config = array (
-    'cron_tags' => array('hourly'),
+    'meta_refresh_cron_tags'      => array('hourly'),
+    'validate_entity_certificate_cron_tags' => array('daily'),
+    'validate_entity_endpoints_cron_tags' => array('daily'),
+    'ca_bundle_file' => '/etc/pki/tls/certs/ca-bundle.pem',
 );
