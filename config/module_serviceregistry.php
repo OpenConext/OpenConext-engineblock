b0b4aa4a200b59c67a71b99fddb458a21da402f3
Boy Baukema <boy.baukema@flex.surfnet.nl>
2011-05-04 16:19:54 +0000

    Use .crt file instead of custom .pem file for Root CAs
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/serviceregistry/trunk@511 528793b8-127e-4309-9901-b74759c7208d

--- config/module_serviceregistry.php
+++ config/module_serviceregistry.php
@@ -7,5 +7,5 @@ $config = array (
     'meta_refresh_cron_tags'      => array('hourly'),
     'validate_entity_certificate_cron_tags' => array('daily'),
     'validate_entity_endpoints_cron_tags' => array('daily'),
-    'ca_bundle_file' => '/etc/pki/tls/certs/ca-bundle.pem',
+    'ca_bundle_file' => '/etc/pki/tls/certs/ca-bundle.crt',
 );
