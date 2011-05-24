0fec8c7458b3bc960f18881c8ec0d0818158a03a
Boy Baukema <boy.baukema@flex.surfnet.nl>
2011-05-24 08:38:00 +0000

    DEV-US0834 Added license headers
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@530 528793b8-127e-4309-9901-b74759c7208d

--- library/EngineBlock/ServiceRegistry/Janus/Client.php
+++ library/EngineBlock/ServiceRegistry/Janus/Client.php
@@ -1,5 +1,27 @@
 <?php
-
+/**
+ * SURFconext EngineBlock
+ *
+ * LICENSE
+ *
+ * Copyright 2011 SURFnet bv, The Netherlands
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * @category  SURFconext EngineBlock
+ * @package
+ * @copyright Copyright © 2010-2011 SURFnet SURFnet bv, The Netherlands (http://www.surfnet.nl)
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ */
 
 class EngineBlock_ServiceRegistry_Janus_Client extends EngineBlock_Rest_Client
 {
@@ -14,7 +36,6 @@ class EngineBlock_ServiceRegistry_Janus_Client extends EngineBlock_Rest_Client
      * @var String
      */
     protected $_secret = NULL;
-    
     
     public function __construct($url, $user, $secret)
     {
