9ba09724885e090ca9837f9ad70dfb071310d469
Okke Harsta <okke.harsta@surfnet.nl>
2011-06-29 08:03:55 +0000

    Added help translations
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@761 528793b8-127e-4309-9901-b74759c7208d

--- languages/en.php
+++ languages/en.php
@@ -26,6 +26,36 @@ return array(
 
     <p>If your question is not in the FAQ or if the answer you get doesnít help, then send an e-mail to <a href="mailto:info@surfconext.nl">info@surfconext.nl</a></p>',
 
+    "close_question"        =>      "Sluit",
+
+    //Help questions
+    "question_screen"                   =>      "Why this screen?",
+    "answer_screen"                     =>      "<p>You can log in to this service with your institutional account. In this screen, you fill in the institution where you want to log in.</p>",
+    "question_surfconext"               =>      "What is SURFfederatie?",
+    "answer_surfconext"                 =>      "<p>SURFfederatie allows you to access the online services provided by a large number of external providers ñ such as webshops, video services, and scientific/scholarly articles ñ with the user name and password issued to you by your institution. SURFfederatie is a secure central ìintermediaryî between your institution and the providers of the online services.</p>",
+    "question_log_in"                   =>      "How do you log in via SURFfederatie?",
+    "answer_log_in"                     =>      "<ul>
+	                        <li>Click on your institution in this screen.</li>
+							<li>You will then be taken to the log-in page for your institution, where you can log in.</li>
+							<li>Your institution will notify SURFfederatie that you have logged in successfully.</li>
+							<li>You will be taken to the service for which you have logged in. You can then make use of that service.</li>
+                        </ul>",
+    "question_security"                 =>      "Is SURFfederatie secure?",
+    "answer_security"                   =>      "<p>Your institution and SURFfederatie both believe that user privacy is extremely important.<br />
+<br />
+Personal details are only provided to a service provider if that is necessary so as to use the service. Contractual agreements between your institution, SURFfederatie, and the service provider guarantee that your personal details will be dealt with carefully.<br />
+<br />
+If you have any questions about how your personal details will be dealt with within SURFfederatie, you can contact your institution or the service provider where you are trying to log in.
+</p>",
+    "question_institution_not_listed"   =>      "My institution is not on the list. What should I do?",
+    "answer_institution_not_listed"     =>      "<p>If your institution is not listed, then it is probably not yet connected to SURFfederatie. Go back to the page for the service. In some cases, there will be alternative ways of logging in.</p>",
+    "question_institution_no_access"    =>      "My institution does not provide access to the service. What should I do?",
+    "answer_institution_no_access"      =>      "<p>It is possible that your institution is connected to SURFfederatie but has not (or not yet) made any arrangements with the service provider about the use of the service. That means that you do not have access to the service. Click on your institution. You can then submit a request to the institution for access to the service. Your request may lead to your institution making the necessary arrangements with the service provider.</p>",
+    "question_asked_institution_access"  =>      "I have asked my institution for access but I still can't get access. Why not?",
+    "answer_asked_institution_access"    =>      "<p>SURFnet has forwarded your request to the right person at your institution, who will take the necessary action. SURFnet has no control over how fast you will receive an answer or be given access.</p>",
+    "question_cannot_select"            =>      "I cannot select my institution. What should I do?",
+    "answer_cannot_select"              =>      "<p>The SURFfederatie options screen can be used in the most popular browsers, including Internet Explorer 7 and 8, Firefox, Chrome, and Safari. Other browsers may not be supported. Your browser must also support the use of cookies and JavaScript.</p>",
+
     //Form
     'sorry'                 => 'Unfortunately,',
     'form_description'      => 'does not yet have access to this service. What should you do?</h2>
--- languages/nl.php
+++ languages/nl.php
@@ -26,6 +26,36 @@ return array(
 
     <p>Staat je vraag er niet bij, of ben je niet tevreden met een antwoord? Stuur dan een mail aan <a href="mailto:info@surfconext.nl">info@surfconext.nl</a></p>',
 
+    "close_question"        =>      "Close",
+
+    //Help questions
+    "question_screen"                   =>      "Waarom dit scherm?",
+    "answer_screen"                     =>      "<p>Je kunt met je instellingsaccount inloggen bij deze dienst. In dit scherm geef je aan bij welke instelling je wilt inloggen.</p>",
+    "question_surfconext"               =>      "Wat is SURFfederatie?",
+    "answer_surfconext"                 =>      "<p>SURFfederatie zorgt dat jij met je gebruikersnaam en wachtwoord van je instelling toegang krijgt tot online diensten van een groot aantal externe aanbieders, zoals webshops, videodiensten en (wetenschappelijke) artikelen. SURFfederatie is een centrale, betrouwbare 'bemiddelaar' tussen jouw instelling en de aanbieders van de online diensten.</p>",
+    "question_log_in"                   =>      "Hoe werkt inloggen via SURFfederatie?",
+    "answer_log_in"                     =>      "<ul>
+	                        <li>Je selecteert in dit scherm je eigen instelling.</li>
+							<li>Je wordt doorgestuurd naar een inlogpagina van je eigen instelling.Daar log je in.</li>
+							<li>Je instelling geeft door aan SURFfederatie dat je succesvol bent ingelogd.</li>
+							<li>Je wordt doorgestuurd naar de dienst waarop je ingelogd hebt; deze kun je nu gebruiken.</li>
+                        </ul>",
+    "question_security"                 =>      "Is SURFfederatie betrouwbaar?",
+    "answer_security"                   =>      "<p>Jouw instelling en SURFfederatie hechten veel belang aan de privacy van gebruikers.<br />
+<br />
+Persoonsgegevens worden alleen verstrekt aan een dienstaanbieder wanneer dat noodzakelijk is voor het gebruik van de dienst. Contractuele afspraken tussen jouw instelling, SURFfederatie en de dienstaanbieder waarborgen dat er zorgvuldig wordt omgegaan met jouw persoonsgegevens.<br />
+<br />
+Heb je vragen over de omgang met je persoonsgegevens binnen de SURFfederatie, dan kun je terecht bij je eigen instelling of de dienstaanbieder waarbij je probeert in te loggen.
+</p>",
+    "question_institution_not_listed"   =>      "Ik zie mijn instelling er niet tussen staan, wat nu?",
+    "answer_institution_not_listed"     =>      "<p>Staat jouw instelling niet in de lijst? Dan is jouw instelling waarschijnlijk nog niet aangesloten bij SURFfederatie. Ga terug naar de pagina van de dienst; soms biedt een dienst ook alternatieve manieren van inloggen aan.</p>",
+    "question_institution_no_access"    =>      "Mijn instelling geeft geen toegang tot deze dienst, wat nu?",
+    "answer_institution_no_access"      =>      "<p>Het kan zijn dat je instelling wel is aangesloten op SURFfederatie, maar (nog) geen afspraken heeft gemaakt met een dienstaanbieder over het gebruik van deze dienst. Dan heb je geen toegang tot de dienst. Klik op jouw instelling. Je kunt dan een verzoek tot toegang kunt indienen bij je instelling. Wellicht is jouw verzoek voor je instelling aanleiding om alsnog afspraken met deze dienstaanbieder te maken.</p>",
+    "question_asked_institution_access"  =>      "Ik heb toegang aangevraagd voor mijn instelling, maar mijn instelling heeft nog steeds geen toegang. Waarom niet?",
+    "answer_asked_institution_access"    =>      "<p>SURFnet stuurt je verzoek door aan de juiste persoon binnen jouw instelling. Deze moet daarop actie ondernemen. SURFnet heeft geen controle over de snelheid waarmee je antwoord of toegang krijgt.</p>",
+    "question_cannot_select"            =>      "Ik kan mijn instelling niet selecteren, wat nu?",
+    "answer_cannot_select"              =>      "<p>Het keuzescherm van SURFfederatie is te gebruiken in de meest gangbare browsers waaronder, Internet Explorer 7 en 8, Firefox, Chrome en Safari. Andere browsers worden mogelijk niet ondersteund. Verder moet je browser het gebruik van cookies en javascript toestaan.</p>",
+
     //Form
     'sorry'                 => 'Helaas,',
     'form_description'      => 'heeft nog geen toegang tot deze dienst. Wat nu?</h2>
@@ -53,7 +83,7 @@ return array(
     'profile_header_my_groups'          => 'Mijn Groepen',
     'profile_header_surfteams'          => 'SURFteams',
     'profile_header_auth_needed'        => 'Authenticatie vereist',
-    'profile_header_leave_surfconext'   => 'Leave SURFconext',
+    'profile_header_leave_surfconext'   => 'Verlaat SURFconext',
     'profile_store_info'                => 'Van uw instelling hebben wij de volgende gegevens ontvangen. Deze gegevens, alsmede uw groepsrelaties, worden opgeslagen in (en gebruikt door) SURFconext. Tevens is het mogelijk dat deze gegevens worden verstrekt aan aanverwante diensten.',
     'profile_group_membership_desc'     => 'Uw lidmaatschap voor uw groepen van verschillende aanbieders.',
     'profile_no_groups'                 => 'Geen Groepen',
