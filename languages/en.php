96337c079f8a9a26334906c504c77af3d8895480
Stein Welberg <stein.welberg@flex.surfnet.nl>
2011-06-29 06:53:34 +0000

    Update language files for profile
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@756 528793b8-127e-4309-9901-b74759c7208d

--- languages/en.php
+++ languages/en.php
@@ -1,47 +1,62 @@
 <?php
 
 return array(
-    'english' => 'English',
-    'my_profile_header' => 'My Profile',
+    'english'               => 'English',
 
-		//General
-	"back"					=>	"back",
+    //General
+    'back'                  => 'back',
+    'attribute'             => 'Attribute',
+    'value'                 => 'Value',
 
-	//Index
-	"search"				=>	"or search for a institution",
-	"index_header"			=>	"Where would you like to log in?",
-	"index_description"		=>	"Select your institution and log in there for <i>%s</i>",
-	"our_suggestion"		=>	"Our suggestion:",
-	"no_access"				=>	"No access",
-	"no_access_more_info"	=>	"No access. &raquo;",
-	"no_results"			=>	"No results found",
+    //Index
+    'search'                => 'or search for a institution',
+    'index_header'          => 'Where would you like to log in?',
+    'index_description'     => 'Select your institution and log in there for <i>%s</i>',
+    'our_suggestion'        => 'Our suggestion:',
+    'no_access'             => 'No access',
+    'no_access_more_info'   => 'No access. &raquo;',
+    'no_results'            => 'No results found',
 
-	//Footer
-	"footer"				=>	"This service is made possible by <a href='http://www.surfconext.nl/'>SURFconext</a> part of SURFnet.",
+    //Footer
+    'footer'                => 'This service is made possible by <a href="http://www.surfconext.nl/">SURFconext</a> part of SURFnet.',
 
-	//Help
-	"help"					=>	"Help",
-	"help_description"		=>	"<p>Check the FAQ if you have any questions about this screen or about SURFconext.</p>
+    //Help
+    'help'                  => 'Help',
+    'help_description'      => '<p>Check the FAQ if you have any questions about this screen or about SURFconext.</p>
 
-	<p>If your question is not in the FAQ or if the answer you get doesnít help, then send an e-mail to <a href='mailto:info@surfconext.nl'>info@surfconext.nl</a></p>",
+    <p>If your question is not in the FAQ or if the answer you get doesnít help, then send an e-mail to <a href="mailto:info@surfconext.nl">info@surfconext.nl</a></p>',
 
-	//Form
-	"sorry"					=>	"Unfortunately,",
-	"form_description"		=>	"does not yet have access to this service. What should you do?</h2>
-			<p>If you want to access this service, please fill in the form below. We will then forward your request to the right person at your institution.</p>",
-	"name"					=>	"Name",
-	"name_error"			=>	"Enter your name",
-	"email"					=>	"Email",
-	"email_error"			=>	"Enter your e-mailadres",
-	"comment"				=>	"Comment",
-	"comment_error"			=>	"Enter an comment",
-	"cancel"				=>	"Cancel",
-	"send"					=>	"Send",
+    //Form
+    'sorry'                 => 'Unfortunately,',
+    'form_description'      => 'does not yet have access to this service. What should you do?</h2>
+            <p>If you want to access this service, please fill in the form below. We will then forward your request to the right person at your institution.</p>',
+    'name'                  => 'Name',
+    'name_error'            => 'Enter your name',
+    'email'                 => 'Email',
+    'email_error'           => 'Enter your e-mailadres',
+    'comment'               => 'Comment',
+    'comment_error'         => 'Enter an comment',
+    'cancel'                => 'Cancel',
+    'send'                  => 'Send',
 
-	"send_confirm"			=>	"Your request has been sent",
-	"send_confirm_desc"		=>	"<p>SURFnet has sent your request to the right person at your institution. That person will take the necessary action in response to your request. It is possible that the necessary arrangements still need to be made between your institution and the service provider.</p>
+    'send_confirm'          => 'Your request has been sent',
+    'send_confirm_desc'     => '<p>SURFnet has sent your request to the right person at your institution. That person will take the necessary action in response to your request. It is possible that the necessary arrangements still need to be made between your institution and the service provider.</p>
 
-	<p>SURFnet has forwarded your request, but it has no control over how fast you will receive an answer or be given access.</p>
+    <p>SURFnet has forwarded your request, but it has no control over how fast you will receive an answer or be given access.</p>
 
-	<p>If you have any questions about your request, please contact <a href='mailto:info@surffederatie.nl'>info@surffederatie.nl</a></p>"
+    <p>If you have any questions about your request, please contact <a href="mailto:info@surffederatie.nl">info@surffederatie.nl</a></p>',
+
+    //Profile
+    'profile_header'                    => 'SURFconext - My Profile',
+    'profile_sub_header'                => 'My Profile',
+    'profile_header_my_groups'          => 'My Groups',
+    'profile_header_surfteams'          => 'SURFteams',
+    'profile_header_auth_needed'        => 'Authentication required',
+    'profile_header_leave_surfconext'   => 'Leave SURFconext',
+    'profile_store_info'                => 'The following profile data have been provided by your home institution. These data as well as your group membership data (e.g.SURFteams) will be stored in SURFconext and shared with services accessed via SURFconext.',
+    'profile_group_membership_desc'     => 'Your membership to various groups for various providers.',
+    'profile_no_groups'                 => 'No Groups',
+    'profile_extra_groups_desc'         => 'In order to get extra groups you need to authorize their use.',
+    'profile_leave_surfconext_desc'     => 'If you want to leave SURFconext and delete all the information that is stored about you, please click the link below. Please note that you are also logged out.',
+    'profile_leave_surfconext_link'     => 'Delete my information!',
 );
--- languages/nl.php
+++ languages/nl.php
@@ -1,49 +1,63 @@
 <?php
 
 return array(
-    'dutch' => 'Nederlands',
-    'my_profile_header' => 'Mijn Profiel',
+    'dutch'                 => 'Nederlands',
 
-	//General
-	"back"					=>	"terug",
+    //General
+    'back'                  => 'terug',
+    'attribute'             => 'Attribuut',
+    'value'                 => 'Waarde',
 
-	//Index
-	"search"				=>	"of zoek een instelling",
-	"index_header"			=>	"Waar wil je inloggen?",
-	"index_description"		=>	"Selecteer jouw instelling en log daar in voor <i>%s</i>",
-	"our_suggestion"		=>	"Onze suggestie:",
-	"no_access"				=>	"Geen toegang.",
-	"no_access_more_info"	=>	"Geen toegang. &raquo;",
-	"no_results"			=>	"Geen resultaten gevonden",
+    //Index
+    'search'                => 'of zoek een instelling',
+    'index_header'          => 'Waar wil je inloggen?',
+    'index_description'     => 'Selecteer jouw instelling en log daar in voor <i>%s</i>',
+    'our_suggestion'        => 'Onze suggestie:',
+    'no_access'             => 'Geen toegang.',
+    'no_access_more_info'   => 'Geen toegang. &raquo;',
+    'no_results'            => 'Geen resultaten gevonden',
 
-	//Footer
-	"footer"			=>	"Deze dienst maakt gebruik van <a href='http://www.surfconext.nl//'>SURFconext</a> van SURFnet.",
+    //Footer
+    'footer'                => 'Deze dienst maakt gebruik van <a href="http://www.surfconext.nl/">SURFconext</a> van SURFnet.',
 
-	//Help
-	"help_header"			=>	"Help",
-	"help_description"		=>	"<p>Heb je vragen over dit scherm of de SURFconext, kijk dan naar de antwoorden bij de veelgestelde vragen.</p>
+    //Help
+    'help_header'           => 'Help',
+    'help_description'      => '<p>Heb je vragen over dit scherm of de SURFconext, kijk dan naar de antwoorden bij de veelgestelde vragen.</p>
 
-	<p>Staat je vraag er niet bij, of ben je niet tevreden met een antwoord? Stuur dan een mail aan <a href='mailto:info@surfconext.nl'>info@surfconext.nl</a></p>",
+    <p>Staat je vraag er niet bij, of ben je niet tevreden met een antwoord? Stuur dan een mail aan <a href="mailto:info@surfconext.nl">info@surfconext.nl</a></p>',
 
-	//Form
-	"sorry"					=>	"Helaas,",
-	"form_description"		=>	"heeft nog geen toegang tot deze dienst. Wat nu?</h2>
-			<p>Wil je toch graag toegang tot deze dienst, vul dan
-	  onderstaand formulier in. Wij sturen je verzoek door naar de juiste persoon binnen jouw instelling.</p>",
-	"name"					=>	"Naam",
-	"name_error"			=>	"Vul je naam in",
-	"email"					=>	"Email",
-	"email_error"			=>	"Vul je e-mailadres in",
-	"comment"				=>	"Toelichting",
-	"comment_error"		=>	"Vul een toelichting in",
-	"cancel"				=>	"Annuleren",
-	"send"					=>	"Verstuur",
+    //Form
+    'sorry'                 => 'Helaas,',
+    'form_description'      => 'heeft nog geen toegang tot deze dienst. Wat nu?</h2>
+            <p>Wil je toch graag toegang tot deze dienst, vul dan
+      onderstaand formulier in. Wij sturen je verzoek door naar de juiste persoon binnen jouw instelling.</p>',
+    'name'                  => 'Naam',
+    'name_error'            => 'Vul je naam in',
+    'email'                 => 'Email',
+    'email_error'           => 'Vul je e-mailadres in',
+    'comment'               => 'Toelichting',
+    'comment_error'         => 'Vul een toelichting in',
+    'cancel'                => 'Annuleren',
+    'send'                  => 'Verstuur',
 
-	"send_confirm"			=>	"Je verzoek is verzonden",
-	"send_confirm_desc"		=>	"<p>SURFnet stuurt je verzoek door aan de juiste persoon binnen jouw instelling. Het is aan deze persoon om actie te nemen op jouw verzoek. Het kan zijn dat er nog afspraken gemaakt moeten worden tussen jouw instelling en de dienstaanbieder.</p>
+    'send_confirm'          => 'Je verzoek is verzonden',
+    'send_confirm_desc'     => '<p>SURFnet stuurt je verzoek door aan de juiste persoon binnen jouw instelling. Het is aan deze persoon om actie te nemen op jouw verzoek. Het kan zijn dat er nog afspraken gemaakt moeten worden tussen jouw instelling en de dienstaanbieder.</p>
 
-	<p>SURFnet faciliteert het doorsturen van je verzoek maar heeft geen controle over de snelheid waarmee je antwoord of toegang krijgt.</p>
+    <p>SURFnet faciliteert het doorsturen van je verzoek maar heeft geen controle over de snelheid waarmee je antwoord of toegang krijgt.</p>
 
-	<p>Heb je vragen over je verzoek, neem dan contact op met <a href='mailto:info@surffederatie.nl'>info@surffederatie.nl</a></p>"
+    <p>Heb je vragen over je verzoek, neem dan contact op met <a href="mailto:info@surffederatie.nl">info@surffederatie.nl</a></p>',
 
+    //Profile
+    'profile_header'                    => 'SURFconext - Mijn Profiel',
+    'profile_sub_header'                => 'Mijn Profiel',
+    'profile_header_my_groups'          => 'Mijn Groepen',
+    'profile_header_surfteams'          => 'SURFteams',
+    'profile_header_auth_needed'        => 'Authenticatie vereist',
+    'profile_header_leave_surfconext'   => 'Leave SURFconext',
+    'profile_store_info'                => 'Van uw instelling hebben wij de volgende gegevens ontvangen. Deze gegevens, alsmede uw groepsrelaties, worden opgeslagen in (en gebruikt door) SURFconext. Tevens is het mogelijk dat deze gegevens worden verstrekt aan aanverwante diensten.',
+    'profile_group_membership_desc'     => 'Uw lidmaatschap voor uw groepen van verschillende aanbieders.',
+    'profile_no_groups'                 => 'Geen Groepen',
+    'profile_extra_groups_desc'         => 'Om uw instellingsgroepen te zien moet u het gebruik hiervoor binnen SURFconext autoriseren.',
+    'profile_leave_surfconext_desc'     => 'Als u geen gebruik meer wilt maken van SURFconext en alle informatie die over u wordt opgeslagen wilt verwijderen, klik dan op de onderstaande link. NB. U wordt hierdoor ook automatisch uitgelogd.',
+    'profile_leave_surfconext_link'     => 'Verwijder mijn gegevens!',
 );
