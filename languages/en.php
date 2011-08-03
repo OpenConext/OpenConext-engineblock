d069bcfeedce8425c8711b4c1cf7f153484426ff
Stein Welberg <stein.welberg@flex.surfnet.nl>
2011-08-03 11:41:53 +0000

    Add svn 1.7.1 tag for engineblock containing a couple of fixes that couldn't wait to be deployed in Production
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/tags/engineblock-v1.7.1@897 528793b8-127e-4309-9901-b74759c7208d

--- languages/en.php
+++ languages/en.php
@@ -16,9 +16,9 @@ return array(
 
     //WAYF
     'idp_selection_title'       => 'Identity Provider Selection - %s',
-    'idp_selection_subheader'   => 'Where would you like to log in?',
-    'search'                    => 'or search for a institution',
-    'idp_selection_desc'        => 'Select your institution and log in there for <i>%s</i>',
+    'idp_selection_subheader'   => 'Login via your institution',
+    'search'                    => 'or search for an institution',
+    'idp_selection_desc'        => 'Select your institution and continue to login to <i>%s</i>',
     'our_suggestion'            => 'Our suggestion:',
     'no_access'                 => 'No access',
     'no_access_more_info'       => 'No access. &raquo;',
@@ -26,63 +26,64 @@ return array(
     'error_header'              => 'Error',
 
     //Footer
-    'footer'                => 'This service is made possible by <a href="http://www.surfconext.nl/">SURFconext</a> part of SURFnet.',
+    'footer'                => 'This service is powered by <a href="http://www.surfconext.nl/">SURFconext</a> - brought to you by SURFnet.',
 
     //Help
     'help_header'           => 'Help',
-    'help_description'      => '<p>Check the FAQ if you have any questions about this screen or about SURFconext.</p>
+    'help_description'      => '<p>Check the FAQ below if you have any questions about this screen or about SURFconext.</p>
 
-    <p>If your question is not in the FAQ or if the answer you get doesnít help, then send an e-mail to <a href="mailto:info@surfconext.nl">info@surfconext.nl</a></p>',
+    <p>For more detailed information, please visit <a href="https://wiki.surfnetlabs.nl/display/conextsupport/">the SURFconext support page</a>
+        or contact the SURFconext helpdesk at <a href="mailto:help@surfconext.nl">help@surfconext.nl</a></p>',
 
-    "close_question"        =>      "Sluit",
+    'close_question'        =>      'Close',
 
     //Help questions
-    "question_screen"                   =>      "Why this screen?",
-    "answer_screen"                     =>      "<p>You can log in to this service with your institutional account. In this screen, you fill in the institution where you want to log in.</p>",
-    "question_surfconext"               =>      "What is SURFfederatie?",
-    "answer_surfconext"                 =>      "<p>SURFfederatie allows you to access the online services provided by a large number of external providers ñ such as webshops, video services, and scientific/scholarly articles ñ with the user name and password issued to you by your institution. SURFfederatie is a secure central ìintermediaryî between your institution and the providers of the online services.</p>",
-    "question_log_in"                   =>      "How do you log in via SURFfederatie?",
-    "answer_log_in"                     =>      "<ul>
-	                        <li>Click on your institution in this screen.</li>
-							<li>You will then be taken to the log-in page for your institution, where you can log in.</li>
-							<li>Your institution will notify SURFfederatie that you have logged in successfully.</li>
-							<li>You will be taken to the service for which you have logged in. You can then make use of that service.</li>
-                        </ul>",
-    "question_security"                 =>      "Is SURFfederatie secure?",
-    "answer_security"                   =>      "<p>Your institution and SURFfederatie both believe that user privacy is extremely important.<br />
+    'question_screen'                   =>      'Why this screen?',
+    'answer_screen'                     =>      '<p>You can log in to this service with your institutional account. In this screen, you select the institution you are affiliated to.</p>',
+    'question_surfconext'               =>      'What is SURFconext?',
+    'answer_surfconext'                 =>      '<p>SURFconext is a next generation collaboration infrastructure that creates new opportunities to collaborate online based on a combination of federation technology (SURFfederatie), group management, concepts from social networking and applications from different providers. SURFfederatie is an important component that allows you to access online services with the user name and password issued to you by your own institution. Both SURFfederatie and SURFconext are brought to you by SURFnet.</p>',
+    'question_log_in'                   =>      'How do you log in via SURFconext?',
+    'answer_log_in'                     =>      '<ul>
+                            <li>Click on your institution in this screen.</li>
+                            <li>You will then be redirected to the log-in page of your institution where you can log in.</li>
+                            <li>Your institution will notify SURFconext via SURFfederatie that you have logged in successfully.</li>
+                            <li>You will be taken to the service for which you have logged in. You can then start using that service.</li>
+                        </ul>',
+    'question_security'                 =>      'Is SURFconext secure?',
+    'answer_security'                   =>      '<p>Your institution and SURFnet believe that user privacy is extremely important.<br />
 <br />
-Personal details are only provided to a service provider if that is necessary so as to use the service. Contractual agreements between your institution, SURFfederatie, and the service provider guarantee that your personal details will be dealt with carefully.<br />
+Personal details are only provided to a service provider if these details are needed to use the service. Contractual agreements between your institution, SURFfederatie/SURFconext, and the service provider guarantee that your personal details will be handled and processed with care.<br />
 <br />
-If you have any questions about how your personal details will be dealt with within SURFfederatie, you can contact your institution or the service provider where you are trying to log in.
-</p>",
-    "question_institution_not_listed"   =>      "My institution is not on the list. What should I do?",
-    "answer_institution_not_listed"     =>      "<p>If your institution is not listed, then it is probably not yet connected to SURFfederatie. Go back to the page for the service. In some cases, there will be alternative ways of logging in.</p>",
-    "question_institution_no_access"    =>      "My institution does not provide access to the service. What should I do?",
-    "answer_institution_no_access"      =>      "<p>It is possible that your institution is connected to SURFfederatie but has not (or not yet) made any arrangements with the service provider about the use of the service. That means that you do not have access to the service. Click on your institution. You can then submit a request to the institution for access to the service. Your request may lead to your institution making the necessary arrangements with the service provider.</p>",
-    "question_asked_institution_access"  =>      "I have asked my institution for access but I still can't get access. Why not?",
-    "answer_asked_institution_access"    =>      "<p>SURFnet has forwarded your request to the right person at your institution, who will take the necessary action. SURFnet has no control over how fast you will receive an answer or be given access.</p>",
-    "question_cannot_select"            =>      "I cannot select my institution. What should I do?",
-    "answer_cannot_select"              =>      "<p>The SURFfederatie options screen can be used in the most popular browsers, including Internet Explorer 7 and 8, Firefox, Chrome, and Safari. Other browsers may not be supported. Your browser must also support the use of cookies and JavaScript.</p>",
+If you have any questions about your privacy and the policy applied, please visit <a href="https://wiki.surfnetlabs.nl/display/conextsupport/">the SURFconext support page</a> for more information or contact the SURFconext helpdesk at <a href="mailto:help@surfconext.nl">help@surfconext.nl</a>.
+</p>',
+    'question_institution_not_listed'   =>      'My institution is not listed. What should I do?',
+    'answer_institution_not_listed'     =>      '<p>If your institution is not listed, then it is either not linked to the SURFfederatie or your institution may not allow access to this particular service. Go back to the page for the service. In some cases, there will be alternative ways of logging in.</p>',
+    'question_institution_no_access'    =>      'My institution does not allow access to the service. What should I do?',
+    'answer_institution_no_access'      =>      '<p>It is possible that your institution is connected to SURFfederatie but has not (or not yet) made any arrangements with the service provider about the use of the service. We will forward your request to your institution. Based on your request your institution may consider to add this service to its service portfolio.</p>',
+    'question_asked_institution_access'  =>      'I have asked my institution for access but I still cannot get access. Why not?',
+    'answer_asked_institution_access'    =>      '<p>Apparently, your institution has not arranged a license yet or, access to this service is not on the roadmap of your institution. Such decisions are beyond the scope and control of SURFnet.</p>',
+    'question_cannot_select'            =>      'In my browser I cannot select my institution. What should I do?',
+    'answer_cannot_select'              =>      '<p>The dialog box to select your institution can be used in most popular browsers, including Internet Explorer 7 and 8, Firefox, Chrome, and Safari. Other browsers may not be supported. Your browser must support the use of cookies and JavaScript.</p>',
 
     //Form
     'sorry'                 => 'Unfortunately,',
     'form_description'      => 'does not yet have access to this service. What should you do?</h2>
-            <p>If you want to access this service, please fill in the form below. We will then forward your request to the right person at your institution.</p>',
+            <p>If you want to access this service, please fill in the form below. We will then forward your request to the person responsible for the services portfolio management at your institution.</p>',
     'name'                  => 'Name',
     'name_error'            => 'Enter your name',
     'email'                 => 'Email',
-    'email_error'           => 'Enter your e-mailadres',
+    'email_error'           => 'Enter your email address',
     'comment'               => 'Comment',
-    'comment_error'         => 'Enter an comment',
+    'comment_error'         => 'Enter a comment',
     'cancel'                => 'Cancel',
     'send'                  => 'Send',
 
     'send_confirm'          => 'Your request has been sent',
-    'send_confirm_desc'     => '<p>SURFnet has sent your request to the right person at your institution. That person will take the necessary action in response to your request. It is possible that the necessary arrangements still need to be made between your institution and the service provider.</p>
+    'send_confirm_desc'     => '<p>Your request has been forwarded to your institution. Further settlement and decisions on the availability of this service will be taken by the ICT staff of your institution.</p>
 
-    <p>SURFnet has forwarded your request, but it has no control over how fast you will receive an answer or be given access.</p>
+    <p>SURFnet has forwarded your request, but the decision and planning to make this service available depends on the ICT policy of your institution.</p>
 
-    <p>If you have any questions about your request, please contact <a href="mailto:info@surffederatie.nl">info@surffederatie.nl</a></p>',
+    <p>If you have any questions about your request, please contact <a href="mailto:help@surfconext.nl">help@surfconext.nl</a></p>',
 
     //Profile
     'profile_header'                    => 'SURFconext - My Profile',
@@ -92,22 +93,21 @@ If you have any questions about how your personal details will be dealt with wit
     'profile_header_auth_needed'        => 'Authentication required',
     'profile_header_leave_surfconext'   => 'Leave SURFconext',
     'profile_store_info'                => 'The following profile data have been provided by your home institution. These data as well as your group membership data (e.g.SURFteams) will be stored in SURFconext and shared with services accessed via SURFconext.',
-    'profile_group_membership_desc'     => 'Your membership to various groups for various providers.',
+    'profile_group_membership_desc'     => 'You are a member of the following groups.',
     'profile_no_groups'                 => 'No Groups',
     'profile_extra_groups_desc'         => 'In order to get extra groups you need to authorize their use.',
-    'profile_leave_surfconext_desc'     => 'If you want to leave SURFconext and delete all the information that is stored about you, please click the link below. Please note that you are also logged out.',
-    'profile_leave_surfconext_link'     => 'Delete my information!',
+    'profile_leave_surfconext_desc'     => 'You currently use SURFconext to access one or more 3rd party application(s) with your institutional account. In addition, your group membership(s) may be shared with these applications. If you would like to quit using SURFconext and delete your SURFconext profile, please click the link below.',
+    'profile_leave_surfconext_link'     => 'Delete my SURFconext profile!',
 
     //Delete User
-    'deleteuser_success_header'         => 'Your information has been successfully deleted',
-    'deleteuser_success_subheader'      => 'Bye Bye...',
-    'deleteuser_success_desc'           => 'Your information has been successfully deleted from SURFconext. To complete the process you <strong>MUST</strong>
-    restart your browser. If you don\'t do this, we cannot guarantee that your profile is completely erased from SURFconext.',
+    'deleteuser_success_header'         => 'SURFconext exit procedure',
+    'deleteuser_success_subheader'      => 'You are almost done..',
+    'deleteuser_success_desc'           => '<strong>Important!</strong> To finalize the exit procedure you need to close your browser.',
 
     //Consent
     'consent_header'                    => 'Attribute Release',
     'consent_subheader'                 => 'Please provide consent for <i>%s</i>',
-    'consent_store_info'                => 'The following profile data have been provided by your home institution. These data as well as your group membership data (e.g.SURFteams) will be stored in SURFconext and shared with services accessed via SURFconext.',
+    'consent_store_info'                => 'The following profile data have been provided by your institution. These data as well as your group membership data (e.g.SURFteams) will be stored in SURFconext and shared with services accessed via SURFconext.',
     'consent_terms_of_service'          => '<p>I agree with the release of my profile data.</p>
         I agree with the SURFnet Terms of Service for the use of SURFconext
         in <a href="https://wiki.surfnetlabs.nl/display/conextsupport/Terms+of+Service+%%28EN%%29" target="_blank">English</a>
@@ -115,16 +115,15 @@ If you have any questions about how your personal details will be dealt with wit
         / in <a href="https://wiki.surfnetlabs.nl/display/conextsupport/Terms+of+Service+%%28NL%%29" target="_blank">Dutch</a>
         <img src="%s" alt="opens in a new window" style="border: 0pt none; float: none;" /><br />
         <br />
-        Additional Terms of Services may apply to 3rd party services.',
+        Please note that additional Terms of Services may apply to 3rd party services.',
     'consent_accept'                    => 'I Accept',
     'consent_decline'                   => 'I Decline',
 
     //Error screens
     'error_help_desc'               => '<p>
-        If this does not solve your problem, visit
-        <a href="https://wiki.surfnetlabs.nl/display/conextsupport/">the support page</a>.
-        Here you can find more information about possible causes
-        and how you can contact the support team.
+        If this does not solve your problem, please visit
+        <a href="https://wiki.surfnetlabs.nl/display/conextsupport/">the SURFconext support page</a>
+        or contact the SURFconext helpdesk at <a href="mailto:help@surfconext.nl">help@surfconext.nl</a>.
     </p>',
     'error_no_consent'              => 'No consent given',
     'error_no_consent_desc'         => 'No consent given to release your data to the service, unable to proceed.<br /><br /> In order to proceed you need to restart your browser, login again, and provide consent.',
@@ -149,13 +148,14 @@ If you have any questions about how your personal details will be dealt with wit
     'error_unknown_issuer'          => 'Error - Unknown issuer..',
     'error_unknown_issuer_desc'     => '<p>
         We received a message from someone we don&#39;t know yet..
-        If you are an implementer and want to connect your service with SURFconext,
-        please go to <a href="https://wiki.surfnetlabs.nl/display/conextsupport/">the support page</a>
-        and give us your endpoint data.
+        If you are a Service Provider and want to connect your service with SURFconext,
+        please visit <a href="https://wiki.surfnetlabs.nl/display/conextsupport/">the SURFconext support page</a>
+        or contact the SURFconext team at <a href="mailto:info@surfconext.nl">info@surfconext.nl</a>.
         <br /><br />
     </p>',
     'error_vo_membership_required'      => 'Membership of a Virtual Organisation required',
-    'error_vo_membership_required_desc' => 'You have successfully authenticated at your Identity Provider, however in order to use this service you have to be a member of a Virtual Organisation.',
+    'error_vo_membership_required_desc' => 'You have successfully authenticated at your Identity Provider, however in order to use this service you have to be a member of a Virtual Organisation. Please visit <a href="https://wiki.surfnetlabs.nl/display/conextsupport/">the SURFconext support page</a>
+        or contact the SURFconext helpdesk at <a href="mailto:help@surfconext.nl">help@surfconext.nl</a> if you have any questions.',
     'error_generic'                     => 'Error - An error occured..',
     'error_generic_desc'                => '<p>
         It is not possible to sign in. Please try again.
