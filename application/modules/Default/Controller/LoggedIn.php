f63f44f5a0fb1d444a891ade60e00203fcf208c8
Stein Welberg <stein.welberg@flex.surfnet.nl>
2011-09-07 13:41:46 +0000

    Update 1.10.0 tag with latest changes to profile page and release both urn:mace and urn:oid attributes
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/tags/engineblock-v1.10.0@1032 528793b8-127e-4309-9901-b74759c7208d

--- application/modules/Default/Controller/LoggedIn.php
+++ application/modules/Default/Controller/LoggedIn.php
@@ -32,9 +32,12 @@ abstract class Default_Controller_LoggedIn extends EngineBlock_Controller_Abstra
      */
     protected $ATTRIBUTES_FILTER = array(
         'urn:oid:2.5.4.42',
+        'urn:oid:2.5.4.3',
         'urn:oid:2.5.4.4',
         'urn:oid:2.16.840.1.113730.3.1.241',
+        'urn:oid:0.9.2342.19200300.100.1.1',
         'urn:oid:0.9.2342.19200300.100.1.3',
+        'urn:oid:1.3.6.1.4.1.1466.115.121.1.15',
         'urn:oid:1.3.6.1.4.1.5923.1.1.1.6',
         'coin:'
     );
