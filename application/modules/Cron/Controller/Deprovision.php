a7904c07bc7e7a18b368fde4cb2e7b9949dd8bf5
Stein Welberg <stein.welberg@flex.surfnet.nl>
2011-09-20 09:53:52 +0000

    Fix bug, add deprovisioning config to view
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@1109 528793b8-127e-4309-9901-b74759c7208d

--- application/modules/Cron/Controller/Deprovision.php
+++ application/modules/Cron/Controller/Deprovision.php
@@ -30,6 +30,7 @@ class Cron_Controller_Deprovision extends EngineBlock_Controller_Abstract
         $this->previewOnly = (($this->_getRequest()->getQueryParameter('preview')) ? true : false);
         $deprovisionEngine = new EngineBlock_Deprovisioning();
         $this->deprovisionPreview = $deprovisionEngine->deprovision($this->previewOnly);
+        $this->deprovisionConfig = EngineBlock_ApplicationSingleton::getInstance()->getConfiguration()->cron->deprovision;
 
         $this->_redirectToController("Index");
     }
