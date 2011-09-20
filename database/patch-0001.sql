3dc32e2ae180d093c1f24352a5f19c83c6a3ba92
Boy Baukema <boy.baukema@flex.surfnet.nl>
2011-09-20 08:40:02 +0000

    Updated patches for DbPatch
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/serviceregistry/trunk@1103 528793b8-127e-4309-9901-b74759c7208d

--- database/patch-0001.sql
+++
@@ -1,4 +0,0 @@
--- Service Registry patch for refreshing metadata based on validUntil and cacheUntil
--- in SAML2 metadata XML.
-ALTER TABLE `janus__entity` ADD `metadata_valid_until` DATETIME NULL AFTER `metadataurl` ,
-ADD `metadata_cache_until` DATETIME NULL AFTER `metadata_valid_until`
---
+++ database/patch/patch-0003.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Add metadata_valid_until and metadata_cache_until fields for metadata refreshing.
+ */
+
+/**
+ * DbPatch makes the following variables available to PHP patches:
+ *
+ * @var $this       DbPatch_Command_Patch_PHP
+ * @var $writer     DbPatch_Core_Writer
+ * @var $db         Zend_Db_Adapter_Abstract
+ * @var $phpFile    string
+ */
+
+$dbConfig = $db->getConfig();
+
+$rows = $db->fetchAll(
+    "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA=? AND TABLE_NAME=? AND COLUMN_NAME=?",
+    array(
+         $dbConfig['dbname'],
+         'janus__entity',
+         'metadata_valid_until'
+    )
+);
+if (count($rows) === 0) {
+    $db->query("ALTER TABLE `janus__entity` ADD `metadata_valid_until` DATETIME NULL AFTER `metadataurl`");
+}
+
+$rows = $db->fetchAll(
+    "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA=? AND TABLE_NAME=? AND COLUMN_NAME=?",
+    array(
+         $dbConfig['dbname'],
+         'janus__entity',
+         'metadata_cache_until'
+    )
+);
+if (count($rows) === 0) {
+    $db->query("ALTER TABLE `janus__entity` ADD `metadata_cache_until` DATETIME NULL AFTER `metadata_valid_until`");
+}
