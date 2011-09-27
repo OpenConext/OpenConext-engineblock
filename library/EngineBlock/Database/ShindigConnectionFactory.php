f8543ae07d46709a930765e9524fb011a0357128
Boy Baukema <boy.baukema@flex.surfnet.nl>
2011-09-27 18:54:06 +0000

    Fixed indentation
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@1163 528793b8-127e-4309-9901-b74759c7208d

--- library/EngineBlock/Database/ShindigConnectionFactory.php
+++ library/EngineBlock/Database/ShindigConnectionFactory.php
@@ -27,11 +27,11 @@
 class EngineBlock_Database_ShindigConnectionFactory extends EngineBlock_Database_ConnectionFactory
 {
     protected function _getDatabaseSettings()
-        {
-            $configuration = $this->_getConfiguration();
-            if (!isset($configuration->databaseShindig)) {
-                throw new EngineBlock_Exception("No shindig database settings?!");
-            }
-            return $configuration->databaseShindig;
+    {
+        $configuration = $this->_getConfiguration();
+        if (!isset($configuration->databaseShindig)) {
+            throw new EngineBlock_Exception("No shindig database settings?!");
         }
+        return $configuration->databaseShindig;
+    }
 }
