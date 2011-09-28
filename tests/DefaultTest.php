dbb33875ca90151fa94cd72e14eaa0e66b88bed5
Boy Baukema <boy.baukema@flex.surfnet.nl>
2011-09-28 11:23:53 +0000

    Added dummy test to shut Bamboo up
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/serviceregistry/trunk@1181 528793b8-127e-4309-9901-b74759c7208d

---
+++ tests/DefaultTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class DefaultTestCase extends PHPUnit_Framework_TestCase
+{
+    public function testNothing()
+    {
+        $this->assertTrue(true, "All is right with the universe");
+    }
+}
