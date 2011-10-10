a3f242b703c047bc70caf42df64ef81da9a4e041
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2011-10-10 09:40:47 +0000

    Added test too check if provisioned data is updated in engineblock when user has changed last name
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/engineblock/trunk@1239 528793b8-127e-4309-9901-b74759c7208d

--- features/bootstrap/Context/Provisioning.php
+++ features/bootstrap/Context/Provisioning.php
@@ -27,6 +27,30 @@ class Provisioning extends BehatContext
     }
 
     /**
+     * Checks a given attribute against the value loaded via open social api
+     *
+     * @Then /^the open social attribute "([^"]*)" of user "([^"]*)" should be "([^"]*)"$/
+     * @param   string $attribute
+     * @param   string $userName
+     * @param   string $value
+     * @return  void
+     */
+    public function theOpenSocialAttributeOfUserShouldBe($attribute, $userName, $value)
+    {
+        $userData = $this->_loadUserDataViaOpenSocialApi($userName);
+
+        $loadedValue = self::getNestedValue($userData, $attribute);
+
+        if(null === $loadedValue) {
+            throw new \Exception('Attribute ' . $attribute . ' Does not exist');
+        }
+
+        if($value != $loadedValue) {
+            throw new \Exception('Attribute ' . $attribute . ' is not: "' . $value . '" but: "' . $loadedValue . '"');
+        }
+    }
+
+    /**
      * Loads user data via open social api
      *
      * @param string $userName
@@ -50,4 +74,49 @@ class Provisioning extends BehatContext
 
         return $userData;
     }
+
+    /**
+     * Finds value in nested array specified by path
+     *
+     * @param   array    $haystack
+     * @param   string   $path       location split by separator
+     * @param   string   $separator  separator used (defaults to dot)
+     * @return  mixed    $haystack   (reduced)
+     * @todo [move] this to util class
+     */
+    public static function getNestedValue(array $haystack, $path, $separator = '.')
+    {
+        $pathParts = explode($separator, $path);
+        foreach($pathParts as $partName) {
+            // Reduce result
+            if(is_array($haystack) && array_key_exists($partName, $haystack)) {
+                $haystack = $haystack[$partName];
+            } else {
+                return null;
+            }
+        }
+
+        return $haystack;
+    }
+
+    /**
+     * Checks if form errors occured
+     *
+     * @Given /^I check for form errors$/
+     * @todo make name more specific?
+     * @return void
+     * @throws Exception if error occurs
+     */
+    public function iCheckForFormErrors()
+    {
+        $page = $this->getMainContext()->getSession()->getPage();
+        $errorElement = $page->find('css', 'ul.error li');
+        if(!empty($errorElement)) {
+            throw new \Exception(
+                $errorElement->getText()
+                . "\n(Note: This error message can also mean that invalid characters are used!)"
+            );
+        }
+    }
+
 }
