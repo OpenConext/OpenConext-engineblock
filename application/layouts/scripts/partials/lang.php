595d83f078758fd3f97e1ecd6ae4fc1ae268396f
Boy Baukema <boy.baukema@flex.surfnet.nl>
2012-04-25 14:00:05 +0200

    Refactor: extract common elements to partials

---
+++ application/layouts/scripts/partials/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+// Determine the selected language, default to english
+$lang = 'en';
+if (isset($_GET['lang'])) {
+    $lang = $_GET['lang'];
+} elseif (isset($_COOKIE['lang'])) {
+    $lang = $_COOKIE['lang'];
+}
+if (!in_array($lang, array('nl','en'))) {
+    $lang = 'en';
+}
---
+++ application/layouts/scripts/partials/nav.php
@@ -0,0 +1,9 @@
+<!-- Language selection -->
+<ul class="nav">
+    <li class="<?php if ($lang==='en'): ?>active<?php endif; ?>">
+        <a href="<?php echo EngineBlock_View::setLanguage('en'); ?>">EN</a>
+    </li>
+    <li class="<?php if ($lang==='nl'): ?>active<?php endif; ?>">
+        <a href="<?php echo EngineBlock_View::setLanguage('nl'); ?>">NL</a>
+    </li>
+</ul>
