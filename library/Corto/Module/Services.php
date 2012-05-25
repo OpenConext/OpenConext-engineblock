71d3f3835fd12057550bf6235adf0c7a06d9a8ce
Boy Baukema <boy.baukema@flex.surfnet.nl>
2012-05-25 14:56:55 +0200

    Mark updating of consent usage data as low priority to improve performance

--- library/Corto/Module/Services.php
+++ library/Corto/Module/Services.php
@@ -986,7 +986,7 @@ class Corto_Module_Services extends Corto_Module_Abstract
             }
 
             // Update usage date
-            $statement = $dbh->prepare("UPDATE {$table} SET usage_date = NOW() WHERE attribute = ?");
+            $statement = $dbh->prepare("UPDATE LOW PRIORITY {$table} SET usage_date = NOW() WHERE attribute = ?");
             $statement->execute(array($attributesHash));
 
             return true;
