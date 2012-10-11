9f28c036af1372f552a516eedeb375ad738c63bc
Boy Baukema <boy.baukema@flex.surfnet.nl>
2012-10-11 09:45:38 +0200

    BACKLOG-713: erronuous output for /sp/metadata

--- library/EngineBlock/Corto/Module/Service/Metadata.php
+++ library/EngineBlock/Corto/Module/Service/Metadata.php
@@ -8,8 +8,10 @@ class EngineBlock_Corto_Module_Service_Metadata extends EngineBlock_Corto_Module
         $entityDetails = $this->_getCurrentEntity($serviceName);
 
         // Override the EntityID and SSO location to optionally append VO id
-        $entityDetails['EntityID'] = $this->_server->getUrl('idpMetadataService');
-        $entityDetails['SingleSignOnService']['Location'] = $this->_server->getUrl('singleSignOnService');
+        if ($serviceName==='idpMetadataService') {
+            $entityDetails['EntityID'] = $this->_server->getUrl($serviceName);
+            $entityDetails['SingleSignOnService']['Location'] = $this->_server->getUrl('singleSignOnService');
+        }
 
         try {
             // See if an sp-entity-id was specified for which we need to use alternate keys (key rollover)
@@ -62,6 +64,7 @@ class EngineBlock_Corto_Module_Service_Metadata extends EngineBlock_Corto_Module
      * Get the current configuration for EngineBlock in it's IdP role from the metadata we received from
      * the Service Registry.
      *
+     * @param string $serviceName
      * @return array Entity configuration
      */
     protected function _getCurrentEntity($serviceName)
