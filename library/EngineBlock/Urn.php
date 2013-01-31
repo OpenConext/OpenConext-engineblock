88497074174d1d73bb0947870998a59f7c144631
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2013-01-31 09:16:07 +0100

    Added todo

--- library/EngineBlock/Urn.php
+++ library/EngineBlock/Urn.php
@@ -5,6 +5,7 @@
 class EngineBlock_Urn
 {
     const SAML1_1_NAMEID_FORMAT_UNSPECIFIED = 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified';
+    // @todo add 1.1. emailAddress, if so, what should EB do with it?
     const SAML2_NAMEID_FORMAT_PERSISTENT = 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent';
     const SAML2_NAMEID_FORMAT_TRANSIENT = 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient';
 }
