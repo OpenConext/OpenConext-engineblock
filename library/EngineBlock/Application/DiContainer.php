93d705ac34efd7c8811af85c5b8280f29ff34bd0
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2013-02-04 13:12:21 +0100

    Corrected formatting

--- library/EngineBlock/Application/DiContainer.php
+++ library/EngineBlock/Application/DiContainer.php
@@ -4,19 +4,24 @@ class EngineBlock_Application_DiContainer extends Pimple
     const XML_CONVERTER = 'xmlConverter';
     const CONSENT_FACTORY = 'consentFactory';
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->registerXmlConverter();
         $this->registerConsentFactory();
     }
 
-    protected function registerXmlConverter() {
-        $this[self::XML_CONVERTER] = $this->share(function (EngineBlock_Application_DiContainer $container) {
+    protected function registerXmlConverter()
+    {
+        $this[self::XML_CONVERTER] = $this->share(function (EngineBlock_Application_DiContainer $container)
+        {
             return new EngineBlock_Corto_XmlToArray();
         });
     }
 
-    protected function registerConsentFactory() {
-        $this[self::CONSENT_FACTORY] = $this->share(function (EngineBlock_Application_DiContainer $container) {
+    protected function registerConsentFactory()
+    {
+        $this[self::CONSENT_FACTORY] = $this->share(function (EngineBlock_Application_DiContainer $container)
+        {
             return new EngineBlock_Corto_Model_Consent_Factory();
         });
     }
--- library/EngineBlock/Application/TestDiContainer.php
+++ library/EngineBlock/Application/TestDiContainer.php
@@ -7,21 +7,21 @@ class EngineBlock_Application_TestDiContainer extends EngineBlock_Application_Di
     /**
      * Registers a mocked xml converter
      */
-    protected function registerXmlConverter() {
-
-
-        $this[self::XML_CONVERTER] = $this->share(function (EngineBlock_Application_DiContainer $container) {
+    protected function registerXmlConverter()
+    {
+        $this[self::XML_CONVERTER] = $this->share(function (EngineBlock_Application_DiContainer $container)
+        {
             return Phake::mock('EngineBlock_Corto_XmlToArray');
         });
-
-
     }
 
     /**
      * Registers a factory which returns mocked consents
      */
-    protected function registerConsentFactory() {
-        $this[self::CONSENT_FACTORY] = $this->share(function (EngineBlock_Application_DiContainer $container) {
+    protected function registerConsentFactory()
+    {
+        $this[self::CONSENT_FACTORY] = $this->share(function (EngineBlock_Application_DiContainer $container)
+        {
             $consentFactoryMock = Phake::mock('EngineBlock_Corto_Model_Consent_Factory');
             Phake::when($consentFactoryMock)
                 ->create(Phake::anyParameters())
