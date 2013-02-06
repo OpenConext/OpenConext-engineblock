745db0cf460b097bb843df8b656c91938c80f80f
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2013-02-06 13:24:04 +0100

    BACKLOG-910 added extra testcase

--- tests/library/EngineBlock/Log/Writer/Syslog/MessageSplitterTest.php
+++ tests/library/EngineBlock/Log/Writer/Syslog/MessageSplitterTest.php
@@ -30,6 +30,12 @@ class EngineBlock_Log_Writer_Syslog_MessageSplitterTest extends PHPUnit_Framewor
                     'message' => 'QUEUED TIMESTAMP: 2013-02-06T12:21:49+01:00| EB[2fg8dc6i39etfmfmgm2hc989c1][51123ccc0c925] Identifier "filterCommandFactory" is not defined. [dumped 12 objects]'
                 ),
                 'expectedMessage' => 'QUEUED TIMESTAMP: 2013-02-06T12:21:49+01:00| EB[2fg8dc6i39etfmfmgm2hc989c1][51123ccc0c925] Identifier "filterCommandFactory" is not defined. [dumped 12 objects]'
+            ),
+            array(
+                'event' => array(
+                    'message' => '> QUEUED TIMESTAMP: 2013-02-06T11:42:33+01:00| EB[6c4cd5qm6cvkm34p0b9ao6uhv1][51123397cf144][DUMP \'previous exception\' (12/12)] InvalidArgumentException Object\n(\n    [message:protected] => Identifier "filterCommandFactory" is not defined.\n    [string:Exception:private] => \n    [code:protected] => 0\n    [file:protected] => /opt/www-on-host/OpenConext-engineblock/vendor/pimple/pimple/lib/Pimple.php\n    [line:protected] => 78\n    [trace:Exception:private] => Array\n        (\n            [0] => Array\n "...'
+                ),
+                'expectedMessage' => '> QUEUED TIMESTAMP: 2013-02-06T11:42:33+01:00| EB[6c4cd5qm6cvkm34p0b9ao6uhv1][51123397cf144][DUMP \'previous exception\' (12/12)] InvalidArgumentException Object\n(\n    [message:protected] => Identifier "filterCommandFactory" is not defined.\n    [string:Exception:private] => \n    [code:protected] => 0\n    [file:protected] => /opt/www-on-host/OpenConext-engineblock/vendor/pimple/pimple/lib/Pimple.php\n    [line:protected] => 78\n    [trace:Exception:private] => Array\n        (\n            [0] => Array\n "...'
             )
         );
     }
