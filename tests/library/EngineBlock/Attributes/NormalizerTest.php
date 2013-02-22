34ca7a7c4d4d65b1ff536413cbcff8075a49ae68
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2013-02-22 12:10:32 +0100

    BACKLOG-929 Removed obsolete autoloader from test, PHPunit already knows how to autoload from config

--- tests/library/EngineBlock/Attributes/NormalizerTest.php
+++ tests/library/EngineBlock/Attributes/NormalizerTest.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once(dirname(__FILE__) . '/../../../autoloading.inc.php');
-
 class Test_EngineBlock_Attributes_NormalizerTest extends \PHPUnit_Framework_TestCase
 {
     public function testNormalizeUnnecessary()
