9ea191855d41f38a97a5b89e448f84afbb77bd3d
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2013-04-17 13:29:23 +0200

    BACKLOG-983 Added cache related unit tests

--- tests/library/EngineBlock/Config/CacheProxyTest.php
+++ tests/library/EngineBlock/Config/CacheProxyTest.php
@@ -2,22 +2,84 @@
 class EngineBlock_Config_CacheProxyTest
     extends PHPUnit_Framework_TestCase
 {
-    public function testMultipleFilesAreMergedAndLoaded()
+    /**
+     * @var array
+     */
+    private $files;
+
+    private $expectedConfigArray;
+
+    public function setup()
     {
-        $files = array(
+        $this->files = array(
             ENGINEBLOCK_FOLDER_ROOT . 'tests/resources/config/config1.ini',
             ENGINEBLOCK_FOLDER_ROOT . 'tests/resources/config/config2.ini',
         );
 
-        $cacheProxy = new EngineBlock_Config_CacheProxy($files);
-        /** @var $config Zend_Config */
-        $config = $cacheProxy->load();
-
-        $expectedConfigArray = array(
+        $this->expectedConfigArray = array(
             'foo1' => 'bar1',
             'foo2' => 'bar2',
             'override' => 'overwritten'
         );
-        $this->assertEquals($expectedConfigArray, $config->toArray());
+    }
+
+    public function testMultipleFilesAreMergedAndLoaded()
+    {
+        $cacheProxy = new EngineBlock_Config_CacheProxy($this->files);
+        /** @var $config Zend_Config */
+        $config = $cacheProxy->load();
+
+
+        $this->assertEquals($this->expectedConfigArray, $config->toArray());
+    }
+
+    public function testConfigIsStoredInCache()
+    {
+        $applicationCacheMock = Phake::mock('Zend_Cache_Backend_Apc');
+        $cacheProxy = new EngineBlock_Config_CacheProxy($this->files, $applicationCacheMock);
+        /** @var $config Zend_Config */
+        $cacheProxy->load();
+
+        Phake::verify($applicationCacheMock)->save(Phake::anyParameters());
+    }
+
+    public function testConfigIsLoadedFromCache()
+    {
+        $configMock = Phake::mock('EngineBlock_Config_Ini');
+        $cachedData = array('foo' => 'cached');
+        Phake::when($configMock)->toArray()->thenReturn($cachedData);
+
+        $applicationCacheMock = Phake::mock('Zend_Cache_Backend_Apc');
+        Phake::when($applicationCacheMock)->test(Phake::anyParameters())->thenReturn(time());
+        Phake::when($applicationCacheMock)->load(Phake::anyParameters())->thenReturn($configMock);
+
+        $cacheProxy = new EngineBlock_Config_CacheProxy($this->files, $applicationCacheMock);
+        /** @var $config Zend_Config */
+        $config = $cacheProxy->load();
+        $this->assertInstanceOf('EngineBlock_Config_Ini', $config);
+        $this->assertEquals($cachedData, $config->toArray());
+    }
+
+    public function testConfigIsNotLoadedFromCacheIfCacheIsTooOld()
+    {
+        $applicationCacheMock = Phake::mock('Zend_Cache_Backend_Apc');
+        Phake::when($applicationCacheMock)->test(Phake::anyParameters())->thenReturn(1);
+
+        $cacheProxy = new EngineBlock_Config_CacheProxy($this->files, $applicationCacheMock);
+        /** @var $config Zend_Config */
+        $config = $cacheProxy->load();
+        $this->assertEquals($this->expectedConfigArray, $config->toArray());
+    }
+
+    public function testConfigIsNotLoadedFromCacheIfResultIsInvalid()
+    {
+        $applicationCacheMock = Phake::mock('Zend_Cache_Backend_Apc');
+        Phake::when($applicationCacheMock)->test(Phake::anyParameters())->thenReturn(time());
+        Phake::when($applicationCacheMock)->load(Phake::anyParameters())->thenReturn('incorrectResult');
+
+        $cacheProxy = new EngineBlock_Config_CacheProxy($this->files, $applicationCacheMock);
+        /** @var $config Zend_Config */
+        $config = $cacheProxy->load();
+        $this->assertEquals($this->expectedConfigArray, $config->toArray());
     }
 }
