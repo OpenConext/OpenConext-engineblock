0ec212ef54a27e29eb693b577c06fc0b356a4d23
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2013-04-18 11:19:51 +0200

    BACKLOG-1018 Added comment

--- library/EngineBlock/Validator/Uri.php
+++ library/EngineBlock/Validator/Uri.php
@@ -1,7 +1,9 @@
 <?php
 /**
  * Alternative for Zend_Uri::check() which does not correctly validate Uri's following the spec:
- * http://www.rfc-editor.org/errata_search.php?rfc=3986 
+ * http://www.rfc-editor.org/errata_search.php?rfc=3986
+ *
+ * Note that this is a VERY permissive regex
  */
 class EngineBlock_Validator_Uri
 {
