9d0027d2e27893eb5f1b2aaae571eac7cb05e0cb
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2013-05-03 15:49:36 +0200

    BACKLOG-1033 Removed host xdebug config

--- bin/pollResque.sh
+++ bin/pollResque.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-export PHP_IDE_CONFIG="serverName=engine.demo.openconext.org"
-export XDEBUG_CONFIG="idekey=PhpStorm, remote_connect_back=0, remote_host=172.18.5.1"
 VVERBOSE=true APP_INCLUDE=bin/init.php QUEUE=logintracking,consent ./bin/pollResque.php
