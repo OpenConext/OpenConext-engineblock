0a3a3e5fa6ec3cb8f4aaf36395e7e4bb39a05d35
lucasvanlierop <lucas@vanlierop.org>
2013-05-15 16:22:18 +0200

    BACKLOG-822 Added (hardcoded) Single logoutservice to idp sso descriptor

---
+++ library/EngineBlock/Corto/Mapper/Metadata/Entity/SsoDescriptor/SingleLogoutService.php
@@ -0,0 +1,26 @@
+<?php
+
+class EngineBlock_Corto_Mapper_Metadata_Entity_SsoDescriptor_SingleLogoutService
+{
+    /** @var  array */
+    private $_entity;
+
+    public function __construct(array $entity)
+    {
+        $this->_entity = $entity;
+    }
+
+    /**
+     * @param array $rootElement
+     * @return array
+     */
+    public function mapTo(array $rootElement)
+    {
+        $rootElement['md:SingleLogoutService'] = array(
+            EngineBlock_Corto_XmlToArray::ATTRIBUTE_PFX . 'Binding' => EngineBlock_Corto_Module_Services::BINDING_TYPE_HTTP_REDIRECT,
+            EngineBlock_Corto_XmlToArray::ATTRIBUTE_PFX . 'Location' => "https://engine.surfconext.nl/logout"
+        );
+        return $rootElement;
+    }
+
+}
