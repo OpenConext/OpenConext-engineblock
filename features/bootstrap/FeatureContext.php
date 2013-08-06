7ee628c51a52032e27f6fa4d9e76a2cb19403f30
lucasvanlierop <lucas@vanlierop.org>
2013-08-06 11:12:53 +0200

    Added first call to engine to request an sso

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -24,4 +24,78 @@ class FeatureContext extends BehatContext
     {
         // Initialize your context here
     }
+
+    /**
+     * @Given /^I request Engine to log me in$/
+     */
+    public function iRequestEngineToLogMeIn()
+    {
+        $engineUrl = 'https://engine.demo.openconext.org';
+
+        $destinationUrl = $engineUrl . '/authentication/idp/single-sign-on';
+        $assertionConsumerServiceURL = 'https://profile.demo.openconext.org/simplesaml/module.php/saml/sp/saml2-acs.php/default-sp';
+        $issuerUrl = 'https://profile.demo.openconext.org/simplesaml/module.php/saml/sp/metadata.php/default-sp';
+        $samlPAuthNRequest = $this->factorySamlPAuthNRequest($assertionConsumerServiceURL, $destinationUrl, $issuerUrl);
+
+
+        $message = $this->encodeSamlMessage($samlPAuthNRequest);
+        $engineRequestUrl = $destinationUrl .'?SAMLRequest=' . urlencode($message);
+
+        $session = $this->getSession();
+        $session->visit($engineRequestUrl);
+    }
 }
+    /**
+     * @param $samlMessage
+     * @return string
+     */
+    private function encodeSamlMessage($samlMessage)
+    {
+        return base64_encode(gzdeflate($samlMessage));
+    }
+
+    /**
+     * @return \Behat\Mink\Session
+     */
+    private function getSession()
+    {
+        $client = new \Behat\Mink\Driver\Goutte\Client();
+        $driver = new \Behat\Mink\Driver\GoutteDriver($client);
+        $client->setClient(new \Guzzle\Http\Client('', array(
+            'ssl.certificate_authority' => false
+        )));
+
+        // init session:
+        $session = new \Behat\Mink\Session($driver);
+
+        // start session:
+        $session->start();
+
+        return $session;
+    }
+
+    private function factorySamlPAuthNRequest(
+        $destinationUrl,
+        $assertionConsumerServiceURL,
+        $issuerUrl
+    )
+    {
+        $samlpAuthNRequest = <<<SAML
+<samlp:AuthnRequest xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"
+                    xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
+                    ID="_5a62d5699e34038989f590149c0df34678126614da"
+                    Version="2.0"
+                    IssueInstant="2013-08-03T02:52:40Z"
+                    Destination="$destinationUrl"
+                    AssertionConsumerServiceURL="$assertionConsumerServiceURL"
+                    ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
+                    >
+    <saml:Issuer>$issuerUrl</saml:Issuer>
+    <samlp:NameIDPolicy Format="urn:oasis:names:tc:SAML:2.0:nameid-format:transient"
+                        AllowCreate="true"
+                        />
+</samlp:AuthnRequest>
+SAML;
+
+        return $samlpAuthNRequest;
+    }
---
+++ features/test.feature
@@ -0,0 +1,13 @@
+Feature: Your first feature
+  In order to start using Behat
+  As a manager or developer
+  I need to try
+
+  Scenario: EngineBlock fails on ....
+    Given I request Engine to log me in
+    And The Idp
+    #Then I should see 'https://profile.demo.openconext.org/simplesaml/module.php/saml/sp/metadata.php/default-sp
+#    And I follow http%3A%2F%2Fmock-idp
+#    And I press http%3A%2F%2Fmock-idp
+#    When An IdP sends InvalidNameIDPolicy status code
+#    Then I should see something
