77a5099e6b4ae010f932d3ff46b12c2c84821f24
lucasvanlierop <lucas@vanlierop.org>
2013-08-12 16:10:17 +0200

    BACKLOG-1048 Added mocking of allowed connections and arp

--- library/EngineBlock/Application/FunctionalTestDiContainer.php
+++ library/EngineBlock/Application/FunctionalTestDiContainer.php
@@ -12,6 +12,9 @@ class EngineBlock_Application_FunctionalTestDiContainer extends EngineBlock_Appl
             $resourcesDir = realpath(ENGINEBLOCK_FOLDER_ROOT . 'tests/resources/serviceregistry');
             Phake::when($serviceRegistryClient)->getIdpList()->thenReturn(require_once $resourcesDir . '/idpList.php');
             Phake::when($serviceRegistryClient)->getSPList()->thenReturn(require_once $resourcesDir . '/spList.php');
+            Phake::when($serviceRegistryClient)->getAllowedIdps(Phake::anyParameters())->thenReturn(require_once $resourcesDir . '/allowedIdps.php');
+            Phake::when($serviceRegistryClient)->isConnectionAllowed(Phake::anyParameters())->thenReturn(array(true));
+            Phake::when($serviceRegistryClient)->getArp(Phake::anyParameters())->thenReturn(new StdClass());
 
             return $serviceRegistryClient;
         });
