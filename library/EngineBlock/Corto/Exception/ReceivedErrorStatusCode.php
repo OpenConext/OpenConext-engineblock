388f26b2774c3073f9dd144468e5151119c6d09f
lucasvanlierop <lucas@vanlierop.org>
2013-08-16 15:13:20 +0200

    BACKLOG-1048 Increased severity for idp error status code exception

--- library/EngineBlock/Corto/Exception/ReceivedErrorStatusCode.php
+++ library/EngineBlock/Corto/Exception/ReceivedErrorStatusCode.php
@@ -2,7 +2,7 @@
 
 class EngineBlock_Corto_Exception_ReceivedErrorStatusCode extends EngineBlock_Exception
 {
-    public function __construct($message, $severity = self::CODE_NOTICE, Exception $previous = null)
+    public function __construct($message, $severity = self::CODE_ERROR, Exception $previous = null)
     {
         parent::__construct($message, $severity, $previous);
     }
