d0e85b0008ee134bca2be856faeaaf39e763ab92
lucasvanlierop <lucas@vanlierop.org>
2013-08-21 15:05:08 +0200

    BACKLOG-1144 Corrected interface

--- application/modules/Dummy/Model/Sp/TestCase/TestCaseInterface.php
+++ application/modules/Dummy/Model/Sp/TestCase/TestCaseInterface.php
@@ -1,5 +1,7 @@
 <?php
 interface Dummy_Model_Sp_TestCase_TestCaseInterface
 {
+    public function decorateRequest(SAML2_Request $request);
+    public function setBindingType(&$bindingType);
     public function decorateResponse(SAML2_Response $response);
 }
