b72b9e1cdf4b3f2f3298b5d8b5302dd74e27b5fd
lucasvanlierop <lucas@vanlierop.org>
2013-08-30 16:04:31 +0200

    BACKLOG-1086 Removed obsolete unit test for attribute manipulation

--- tests/library/EngineBlock/AttributeManipulator/fixtures/attribute-manipulations/https___example.com_test_response/manipulations.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$attributes['email'] = $attributes['urn:mace:dir:attribute-def:mail'];
-unset($attributes['urn:mace:dir:attribute-def:mail']);
-$attributes['uid'] = $subjectId;
-$attributes['sp']  = $response['__']['destinationid'];
