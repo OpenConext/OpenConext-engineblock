586bf033baf7eee789e355822531ea364dd3cb6f
lucasvanlierop <lucas@vanlierop.org>
2013-08-30 12:50:47 +0200

    BACKLOG-1059 Unable to receive message exception is now an error instead of notice

--- library/EngineBlock/Corto/Module/Bindings/UnableToReceiveMessageException.php
+++ library/EngineBlock/Corto/Module/Bindings/UnableToReceiveMessageException.php
@@ -2,7 +2,7 @@
 
 class EngineBlock_Corto_Module_Bindings_UnableToReceiveMessageException extends EngineBlock_Corto_Module_Bindings_Exception
 {
-    public function __construct($message, $severity = self::CODE_NOTICE, Exception $previous = null)
+    public function __construct($message, $severity = self::CODE_ERROR, Exception $previous = null)
     {
         parent::__construct($message, $severity, $previous);
     }
