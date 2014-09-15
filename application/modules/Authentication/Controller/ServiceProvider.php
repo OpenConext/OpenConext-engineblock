dcbca3f689b52979b575194850539a7fe77f9018
Boy Baukema <boy@ibuildings.nl>
2014-09-15 17:02:37 +0200

    Revert debug commit

--- application/modules/Authentication/Controller/ServiceProvider.php
+++ application/modules/Authentication/Controller/ServiceProvider.php
@@ -9,86 +9,86 @@ class Authentication_Controller_ServiceProvider extends EngineBlock_Controller_A
 
         $proxyServer = new EngineBlock_Corto_Adapter();
 
-//        try {
+        try {
             $proxyServer->consumeAssertion();
-//        }
-//        catch (EngineBlock_Corto_Exception_UserNotMember $e) {
-//            $application->getLogInstance()->log(
-//                "VO membership required",
-//                EngineBlock_Log::NOTICE,
-//                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
-//            );
-//            $application->handleExceptionWithFeedback($e,
-//                '/authentication/feedback/vomembershiprequired');
-//        }
-//        catch (EngineBlock_Corto_Module_Services_SessionLostException $e) {
-//            $application->getLogInstance()->log(
-//                "Session Lost",
-//                EngineBlock_Log::NOTICE,
-//                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
-//            );
-//            $application->handleExceptionWithFeedback($e,
-//                '/authentication/feedback/session-lost');
-//        }
-//        catch (EngineBlock_Corto_Module_Bindings_UnableToReceiveMessageException $e) {
-//            $application->getLogInstance()->log(
-//                "Unable to receive message",
-//                EngineBlock_Log::NOTICE,
-//                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
-//            );
-//            $application->handleExceptionWithFeedback($e,
-//                '/authentication/feedback/unable-to-receive-message');
-//        }
-//        catch (EngineBlock_Corto_Exception_UnknownIssuer $e) {
-//            $application->getLogInstance()->log(
-//                "Unknown Issuer",
-//                EngineBlock_Log::NOTICE,
-//                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
-//            );
-//            $application->handleExceptionWithFeedback($e,
-//                '/authentication/feedback/unknown-issuer?entity-id='.urlencode($e->getEntityId()).'&destination='.urlencode($e->getDestination()));
-//        }
-//        catch (EngineBlock_Corto_Exception_MissingRequiredFields $e) {
-//            $application->getLogInstance()->log(
-//                "Missing Required Fields",
-//                EngineBlock_Log::NOTICE,
-//                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
-//            );
-//            $application->handleExceptionWithFeedback($e,
-//                '/authentication/feedback/missing-required-fields');
-//        }
-//
-//        catch (EngineBlock_Attributes_Manipulator_CustomException $e) {
-//            $_SESSION['feedback_custom'] = $e->getFeedback();
-//            $application->handleExceptionWithFeedback($e,
-//                '/authentication/feedback/custom');
-//        } catch (EngineBlock_Corto_Module_Bindings_UnsupportedBindingException $e) {
-//            $application->getLogInstance()->log(
-//                "Unsupported Binding",
-//                EngineBlock_Log::NOTICE,
-//                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
-//            );
-//            $application->handleExceptionWithFeedback($e,
-//                '/authentication/feedback/invalid-acs-binding');
-//        } catch (EngineBlock_Corto_Exception_ReceivedErrorStatusCode $e) {
-//            // Add extra feedback info
-//            $application->getLogInstance()->log(
-//                "Received Error Status Code",
-//                EngineBlock_Log::NOTICE,
-//                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
-//            );
-//            $application->handleExceptionWithFeedback($e,
-//                '/authentication/feedback/received-error-status-code',
-//                $e->getFeedbackInfo());
-//        } catch (EngineBlock_Corto_Module_Bindings_VerificationException $e) {
-//            $application->getLogInstance()->log(
-//                "Unable to verify message",
-//                EngineBlock_Log::NOTICE,
-//                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
-//            );
-//            $application->handleExceptionWithFeedback($e,
-//                '/authentication/feedback/received-invalid-response');
-//        }
+        }
+        catch (EngineBlock_Corto_Exception_UserNotMember $e) {
+            $application->getLogInstance()->log(
+                "VO membership required",
+                EngineBlock_Log::NOTICE,
+                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
+            );
+            $application->handleExceptionWithFeedback($e,
+                '/authentication/feedback/vomembershiprequired');
+        }
+        catch (EngineBlock_Corto_Module_Services_SessionLostException $e) {
+            $application->getLogInstance()->log(
+                "Session Lost",
+                EngineBlock_Log::NOTICE,
+                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
+            );
+            $application->handleExceptionWithFeedback($e,
+                '/authentication/feedback/session-lost');
+        }
+        catch (EngineBlock_Corto_Module_Bindings_UnableToReceiveMessageException $e) {
+            $application->getLogInstance()->log(
+                "Unable to receive message",
+                EngineBlock_Log::NOTICE,
+                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
+            );
+            $application->handleExceptionWithFeedback($e,
+                '/authentication/feedback/unable-to-receive-message');
+        }
+        catch (EngineBlock_Corto_Exception_UnknownIssuer $e) {
+            $application->getLogInstance()->log(
+                "Unknown Issuer",
+                EngineBlock_Log::NOTICE,
+                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
+            );
+            $application->handleExceptionWithFeedback($e,
+                '/authentication/feedback/unknown-issuer?entity-id='.urlencode($e->getEntityId()).'&destination='.urlencode($e->getDestination()));
+        }
+        catch (EngineBlock_Corto_Exception_MissingRequiredFields $e) {
+            $application->getLogInstance()->log(
+                "Missing Required Fields",
+                EngineBlock_Log::NOTICE,
+                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
+            );
+            $application->handleExceptionWithFeedback($e,
+                '/authentication/feedback/missing-required-fields');
+        }
+
+        catch (EngineBlock_Attributes_Manipulator_CustomException $e) {
+            $_SESSION['feedback_custom'] = $e->getFeedback();
+            $application->handleExceptionWithFeedback($e,
+                '/authentication/feedback/custom');
+        } catch (EngineBlock_Corto_Module_Bindings_UnsupportedBindingException $e) {
+            $application->getLogInstance()->log(
+                "Unsupported Binding",
+                EngineBlock_Log::NOTICE,
+                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
+            );
+            $application->handleExceptionWithFeedback($e,
+                '/authentication/feedback/invalid-acs-binding');
+        } catch (EngineBlock_Corto_Exception_ReceivedErrorStatusCode $e) {
+            // Add extra feedback info
+            $application->getLogInstance()->log(
+                "Received Error Status Code",
+                EngineBlock_Log::NOTICE,
+                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
+            );
+            $application->handleExceptionWithFeedback($e,
+                '/authentication/feedback/received-error-status-code',
+                $e->getFeedbackInfo());
+        } catch (EngineBlock_Corto_Module_Bindings_VerificationException $e) {
+            $application->getLogInstance()->log(
+                "Unable to verify message",
+                EngineBlock_Log::NOTICE,
+                EngineBlock_Log_Message_AdditionalInfo::createFromException($e)
+            );
+            $application->handleExceptionWithFeedback($e,
+                '/authentication/feedback/received-invalid-response');
+        }
     }
 
     public function processConsentAction()
