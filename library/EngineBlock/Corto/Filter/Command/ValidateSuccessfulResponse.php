84a7ef96e9348620078a8d56c2beb77edd34d961
Boy Baukema <boy@ibuildings.nl>
2014-10-10 11:22:32 +0200

    Improved reporting of non-Success status code to abbreviate code and add second level code if available.

--- library/EngineBlock/Corto/Filter/Command/ValidateSuccessfulResponse.php
+++ library/EngineBlock/Corto/Filter/Command/ValidateSuccessfulResponse.php
@@ -5,26 +5,30 @@
  */
 class EngineBlock_Corto_Filter_Command_ValidateSuccessfulResponse extends EngineBlock_Corto_Filter_Command_Abstract
 {
-    const SAML2_STATUS_CODE_SUCCESS = 'urn:oasis:names:tc:SAML:2.0:status:Success';
-
     public function execute()
     {
         $status = $this->_response->getStatus();
-        $statusCode = $status['Code'];
-        if ($statusCode !== self::SAML2_STATUS_CODE_SUCCESS) {
-            // Idp returned an error
-            $statusMessage = !empty($status['Message']) ? $status['Message'] : '(No message provided)';
-
-            $exception = new EngineBlock_Corto_Exception_ReceivedErrorStatusCode(
-                'Response received with Status: ' .
-                    $statusCode .
-                    ' - ' .
-                    $statusMessage
-            );
-            $exception->setFeedbackStatusCode($statusCode);
-            $exception->setFeedbackStatusMessage($statusMessage);
-
-            throw $exception;
+        if ($status['Code'] === \SAML2_Const::STATUS_SUCCESS) {
+            return;
         }
+
+        $statusCodeDescription = $status['Code'];
+        if (isset($status['SubCode'])) {
+            $statusCodeDescription .= '/' . $status['SubCode'];
+        }
+        $statusCodeDescription = str_replace('urn:oasis:names:tc:SAML:2.0:status:', '', $statusCodeDescription);
+
+        $statusMessage = !empty($status['Message']) ? $status['Message'] : '(No message provided)';
+
+        $exception = new EngineBlock_Corto_Exception_ReceivedErrorStatusCode(
+            'Response received with Status: ' .
+                $statusCodeDescription .
+                ' - ' .
+                $statusMessage
+        );
+        $exception->setFeedbackStatusCode($statusCodeDescription);
+        $exception->setFeedbackStatusMessage($statusMessage);
+
+        throw $exception;
     }
 }
