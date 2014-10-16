5426992f80d4b89d3835a9c28281346555aeddfc
Boy Baukema <boy@ibuildings.nl>
2014-10-16 17:02:54 +0200

    Issue #64, added SamlHelper:: getSpRequesterChain and getDestinationSpMetadata

--- library/EngineBlock/SamlHelper.php
+++ library/EngineBlock/SamlHelper.php
@@ -19,4 +19,44 @@ class EngineBlock_SamlHelper
         }
         return false;
     }
+
+    public static function getSpRequesterChain(
+        array $spEntityMetadata,
+        EngineBlock_Saml2_AuthnRequestAnnotationDecorator $request,
+        EngineBlock_Corto_ProxyServer $server
+    ) {
+        $chain = array($spEntityMetadata);
+
+        $destinationSpMetadata = self::getDestinationSpMetadata($spEntityMetadata, $request, $server);
+        if ($destinationSpMetadata !== $spEntityMetadata) {
+            array_unshift($chain, $destinationSpMetadata);
+        }
+
+        return $chain;
+    }
+
+    public static function getDestinationSpMetadata(
+        array $spEntityMetadata,
+        EngineBlock_Saml2_AuthnRequestAnnotationDecorator $request,
+        EngineBlock_Corto_ProxyServer $server
+    ) {
+        if (!isset($spEntityMetadata['TrustedProxy']) || !$spEntityMetadata['TrustedProxy']) {
+            return $spEntityMetadata;
+        }
+
+        if (!$request->wasSigned()) {
+            return $spEntityMetadata;
+        }
+
+        // Requester IDs are appended to as they pass through a proxy, so we always want the last RequesterID
+        // Note that this is not specified in the spec, but this is what we do and what SSP does.
+        $requesterIds = $request->getSspRequest()->getRequesterID();
+        $lastRequesterEntityId = end($requesterIds);
+
+        if ($lastRequesterEntityId && !$server->hasRemoteEntity($lastRequesterEntityId)) {
+            return $spEntityMetadata;
+        }
+
+        return $server->getRemoteEntity($lastRequesterEntityId);
+    }
 }
