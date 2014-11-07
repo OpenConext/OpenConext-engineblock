38cdfc702905b2fd21eed455246efa5f78bb9ad9
Remold Krol <remold.krol@everett.nl>
2014-11-07 15:01:58 +0100

    #98 Validation of attributes (SHO+uid or ePPN) iis based on config value of openConextIdentifierType

--- library/EngineBlock/Corto/Filter/Command/ValidateRequiredAttributes.php
+++ library/EngineBlock/Corto/Filter/Command/ValidateRequiredAttributes.php
@@ -5,6 +5,7 @@ use OpenConext\Component\EngineBlockMetadata\Entity\IdentityProviderEntity;
 class EngineBlock_Corto_Filter_Command_ValidateRequiredAttributes extends EngineBlock_Corto_Filter_Command_Abstract
 {
     const URN_MACE_TERENA_SCHACHOMEORG = 'urn:mace:terena.org:attribute-def:schacHomeOrganization';
+    const LDAP_ATTR_COLLAB_PERSON_EPPN              = 'eduPersonPrincipalName';
 
     /**
      * This command may modify the response attributes
@@ -24,22 +25,31 @@ class EngineBlock_Corto_Filter_Command_ValidateRequiredAttributes extends Engine
     {
         $errors = array();
 
-        if (isset($this->_idpMetadata['SchacHomeOrganization'])) {
-            // ServiceRegistry override of SchacHomeOrganization, set it and skip validation
-            $this->_responseAttributes[self::URN_MACE_TERENA_SCHACHOMEORG] = array(
-                $this->_idpMetadata['SchacHomeOrganization']
-            );
-        }
-        else {
-            $error = $this->_requireValidSchacHomeOrganization($this->_responseAttributes);
+        $openConextIdentifierType = $this->_getOpenConextIdentifierTypeFromConfig();
+
+        if ($openConextIdentifierType != self::LDAP_ATTR_COLLAB_PERSON_EPPN) {
+            if (isset($this->_idpMetadata['SchacHomeOrganization'])) {
+                // ServiceRegistry override of SchacHomeOrganization, set it and skip validation
+                $this->_responseAttributes[self::URN_MACE_TERENA_SCHACHOMEORG] = array(
+                     $this->_idpMetadata['SchacHomeOrganization']
+                );
+            }
+            else {
+                $error = $this->_requireValidSchacHomeOrganization($this->_responseAttributes);
+                if ($error) {
+                    $errors[] = $error;
+                }
+            }
+
+            $error = $this->_requireValidUid($this->_responseAttributes);
             if ($error) {
                 $errors[] = $error;
             }
-        }
-
-        $error = $this->_requireValidUid($this->_responseAttributes);
-        if ($error) {
-            $errors[] = $error;
+        } else {
+            $error = $this->_requireValidEppn($this->_responseAttributes);
+            if ($error) {
+                $errors[] = $error;
+            }
         }
 
         if (!empty($errors)) {
@@ -132,5 +142,51 @@ class EngineBlock_Corto_Filter_Command_ValidateRequiredAttributes extends Engine
             return "urn:mace:dir:attribute-def:uid is more than 256 characters long!";
         }
         return false;
+    }
+
+    protected function _requireValidEppn($responseAttributes)
+    {
+        /*
+         * urn:mace:dir:attribute-def:eduPersonPrincipalName
+urn:oid:1.3.6.1.4.1.5923.1.1.1.6
+         */
+        if (!isset($responseAttributes['urn:mace:dir:attribute-def:eduPersonPrincipalName'])) {
+            return "urn:mace:dir:attribute-def:eduPersonPrincipalNae missing in attributes!";
+        }
+
+        if (count($responseAttributes['urn:mace:dir:attribute-def:eduPersonPrincipalName']) === 0) {
+            return "urn:mace:dir:attribute-def:eduPersonPrincipalName has no values";
+        }
+
+        if (count($responseAttributes['urn:mace:dir:attribute-def:eduPersonPrincipalName']) > 1) {
+            return "urn:mace:dir:attribute-def:eduPersonPrincipalName has more than one value";
+        }
+
+        $eppn = $responseAttributes['urn:mace:dir:attribute-def:eduPersonPrincipalName'][0];
+        // Length 128 taken from https://www.educause.edu/fidm/attributes
+        // Note that this is a bit naive as the spec states that this can be 128 characters
+        // of UTF-8, so in theory a person could have a uid of 100 kanji characters (which take up 3 bytes)
+        // and our check would fail, even though it is completely valid.
+        // But we'll cross that ブリッジ when we get to it.
+        if (strlen($eppn) > 128) {
+            return "urn:mace:dir:attribute-def:eduPersonPrincipalName is more than 128 characters long!";
+        }
+        return false;
+    }
+
+    protected function _getOpenConextIdentifierTypeFromConfig() {
+        $application = EngineBlock_ApplicationSingleton::getInstance();
+        $openConextIdentifierType = $application->getConfiguration('openConextIdentifierType', self::LDAP_ATTR_COLLAB_PERSON_ID);
+
+        $allowValues = array(
+            self::LDAP_ATTR_COLLAB_PERSON_ID,
+            self::LDAP_ATTR_COLLAB_PERSON_UUID,
+            self::LDAP_ATTR_COLLAB_PERSON_EPPN
+        );
+        if (!in_array ($openConextIdentifierType, $allowValues )) {
+            return self::LDAP_ATTR_COLLAB_PERSON_ID;
+        }
+
+        return $openConextIdentifierType;
     }
 }
