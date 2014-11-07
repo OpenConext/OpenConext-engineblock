df507882fe027e92de27d75bab172a84f95c3b13
Remold Krol <remold.krol@everett.nl>
2014-11-07 15:43:21 +0100

    #98 add missing const const LDAP_ATTR_COLLAB_PERSON_UUID

--- library/EngineBlock/Corto/Filter/Command/ValidateRequiredAttributes.php
+++ library/EngineBlock/Corto/Filter/Command/ValidateRequiredAttributes.php
@@ -6,6 +6,7 @@ class EngineBlock_Corto_Filter_Command_ValidateRequiredAttributes extends Engine
 {
     const URN_MACE_TERENA_SCHACHOMEORG = 'urn:mace:terena.org:attribute-def:schacHomeOrganization';
     const LDAP_ATTR_COLLAB_PERSON_ID                = 'collabpersonid';
+    const LDAP_ATTR_COLLAB_PERSON_UUID              = 'collabpersonuuid';
     const LDAP_ATTR_COLLAB_PERSON_EPPN              = 'eduPersonPrincipalName';
 
     /**
