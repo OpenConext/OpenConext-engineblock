09f51c65a1c3a78f8829021d5eefb6613eb7642c
Boy Baukema <boy@ibuildings.nl>
2015-01-19 13:21:42 +0100

    Added phpDoc and exception for failure to resolve aliases

--- library/EngineBlock/Attributes/Definition/Denormalizer.php
+++ library/EngineBlock/Attributes/Definition/Denormalizer.php
@@ -5,22 +5,27 @@ class EngineBlock_Attributes_Definition_Denormalizer
     public function denormalize(array $definitions)
     {
         foreach ($definitions as $attributeName => $definition) {
+            // Has a definition, no need to denormalize.
             if (is_array($definition)) {
                 continue;
             }
 
+            // Resolve aliases and remember until we get to the actual definition.
             $aliases = array($attributeName);
             while (!is_array($definition)) {
                 $attributeName = $definitions[$attributeName];
 
-                if (empty($definitions[$attributeName])) {
-                    // @todo log
-                    break;
+                if (!isset($definitions[$attributeName])) {
+                    throw new EngineBlock_Exception(
+                        "Unable to resolve definition for '$attributeName', path: " . join(' > ', $aliases)
+                    );
                 }
+
                 $definition = $definitions[$attributeName];
                 $aliases[] = $attributeName;
             }
 
+            // Replace the alias by the definition and add a key to show where the definition came from.
             foreach ($aliases as $alias) {
                 $definitions[$alias] = $definition;
                 if ($attributeName !== $alias && is_array($definitions[$alias])) {
