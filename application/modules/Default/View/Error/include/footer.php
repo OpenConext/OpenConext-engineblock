5b12a7d2d6feec19f81e036537c8606bb3db37a6
Martin de Keijzer <mdkeijzer@ibuildings.nl>
2015-05-29 16:12:29 +0200

    Use latest material design as default

--- application/modules/Default/View/Error/include/footer.php
+++ application/modules/Default/View/Error/include/footer.php
@@ -4,38 +4,31 @@ $log->log('Showing feedback page with message: ' . $layout->title, EngineBlock_L
 $log->getQueueWriter()->flush('feedback page shown');
 ?>
 
-    <hr />
+    <div class="l-overflow">
+      <table>
+      <?php
+      if (!empty($_SESSION['feedbackInfo']) && is_array($_SESSION['feedbackInfo'])) {
+          foreach($_SESSION['feedbackInfo'] as $name => $value) {
+              if (empty($value)) {
+                  continue;
+              }
+          ?>
+          <tr>
+              <td><strong><?= EngineBlock_View::htmlSpecialCharsText($this->t($name))?>:</strong></td>
+              <td><?= EngineBlock_View::htmlSpecialCharsText($value);?></td>
+          </tr>
+          <?php
+          }
+      }
+      ?>
+      </table>
+    </div>
 
-    <table>
-    <?php
-    if (!empty($_SESSION['feedbackInfo']) && is_array($_SESSION['feedbackInfo'])) {
-        foreach($_SESSION['feedbackInfo'] as $name => $value) {
-            if (empty($value)) {
-                continue;
-            }
-        ?>
-            <tr>
-                <td><strong><?= EngineBlock_View::htmlSpecialCharsText($this->t($name))?>:</strong></td>
-                <td><?= EngineBlock_View::htmlSpecialCharsText($value);?></td>
-            </tr>
-        <?php
-        }
-    }
-    ?>
-     </table>
-
-    <hr />
-
-    <p>
-        <?= $this->t('error_help_desc'); ?>
-    </p>
+    <p><?= $this->t('error_help_desc'); ?></p>
 
     <?php
     // @todo make the number of steps in history that the back button makes dynamic, it is not always 2
     ?>
-    <div class="button-row">
-        <a href="#" id="GoBack"  class="submit button-tertiary" onclick="history.back(-2); return false;">
-            <?= $this->t('go_back'); ?>
-            <span class="btn-wrap-right">&nbsp;</span>
-        </a>
-    </div>
+    <a href="#" id="GoBack"  class="c-button" onclick="history.back(-2); return false;">
+      <?= $this->t('go_back'); ?>
+    </a>
