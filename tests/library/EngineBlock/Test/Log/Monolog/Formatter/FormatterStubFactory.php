275097961b307eeb24dfca24e0f87f94627da886
DRvanR <dvrenterghem@ibuildings.nl>
2015-10-06 11:53:06 +0200

    Fix tests

--- tests/library/EngineBlock/Test/Log/Monolog/Formatter/FormatterStubFactory.php
+++ tests/library/EngineBlock/Test/Log/Monolog/Formatter/FormatterStubFactory.php
@@ -1,11 +1,12 @@
 <?php
 
-final class EngineBlock_Test_Log_Monolog_Formatter_FormatterStubFactory extends PHPUnit_Framework_TestCase
+final class EngineBlock_Test_Log_Monolog_Formatter_FormatterStubFactory implements
+    EngineBlock_Log_Monolog_Formatter_FormatterFactory
 {
     public static $stubToReturn;
     public static $expectedConfiguration;
 
-    public static function factory($config)
+    public static function factory(array $config)
     {
         if (self::$expectedConfiguration !== null) {
             PHPUnit_Framework_TestCase::assertEquals(
