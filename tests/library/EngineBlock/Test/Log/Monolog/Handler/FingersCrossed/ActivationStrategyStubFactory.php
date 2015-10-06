236f2365253c932d4b472c1f809dc6527ba550d5
DRvanR <dvrenterghem@ibuildings.nl>
2015-10-06 11:53:06 +0200

    Fix tests

--- tests/library/EngineBlock/Test/Log/Monolog/Handler/FingersCrossed/ActivationStrategyStubFactory.php
+++ tests/library/EngineBlock/Test/Log/Monolog/Handler/FingersCrossed/ActivationStrategyStubFactory.php
@@ -1,11 +1,12 @@
 <?php
 
-final class EngineBlock_Test_Log_Monolog_Handler_FingersCrossed_ActivationStrategyStubFactory
+final class EngineBlock_Test_Log_Monolog_Handler_FingersCrossed_ActivationStrategyStubFactory implements
+    EngineBlock_Log_Monolog_Handler_FingersCrossed_ActivationStrategyFactory
 {
     public static $stubToReturn;
     public static $expectedConfiguration;
 
-    public static function factory($config)
+    public static function factory(array $config)
     {
         if (self::$expectedConfiguration !== null) {
             PHPUnit_Framework_TestCase::assertEquals(
