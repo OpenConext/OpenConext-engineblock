b75a075c4418b8d131ae9bf33ff7b33bac386a4b
DRvanR <dvrenterghem@ibuildings.nl>
2015-10-12 14:47:16 +0200

    Allow UnkonwRemoteEntityException to be thrown based on previous

--- library/EngineBlock/Corto/ProxyServer/UnknownRemoteEntityException.php
+++ library/EngineBlock/Corto/ProxyServer/UnknownRemoteEntityException.php
@@ -2,21 +2,36 @@
 
 class EngineBlock_Corto_ProxyServer_UnknownRemoteEntityException extends EngineBlock_Corto_ProxyServer_Exception
 {
+    /**
+     * @var string
+     */
     protected $_entityId;
 
-    public function __construct($entityId)
+    /**
+     * @param string         $entityId
+     * @param Exception|null $previous
+     */
+    public function __construct($entityId, \Exception $previous = null)
     {
         $this->_entityId = $entityId;
         $message = "Unknown remote entity with entityid '$entityId'";
-        parent::__construct($message, self::CODE_NOTICE);
+
+        parent::__construct($message, self::CODE_NOTICE, $previous);
     }
 
+    /**
+     * @param string $entityId
+     * @return $this
+     */
     public function setEntityId($entityId)
     {
         $this->_entityId = $entityId;
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getEntityId()
     {
         return $this->_entityId;
