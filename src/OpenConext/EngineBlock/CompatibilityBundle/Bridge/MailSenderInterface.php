a2532cd8abb5adccf29b5136c4ba23ecf46d3b36
DRvanR <dvrenterghem@ibuildings.nl>
2015-10-13 10:47:07 +0200

    Remove `@throws` annotation from interface docblock

--- src/OpenConext/EngineBlock/CompatibilityBundle/Bridge/MailSenderInterface.php
+++ src/OpenConext/EngineBlock/CompatibilityBundle/Bridge/MailSenderInterface.php
@@ -7,7 +7,6 @@ interface MailSenderInterface
     /**
      * @param MailMessage $message
      * @return void
-     * @throws \Zend_Mail_Exception
      */
     public function send(MailMessage $message);
 }
