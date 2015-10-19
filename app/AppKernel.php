3d31db0da1850a019e69a6bcb8da36eb4155d501
DRvanR <dvrenterghem@ibuildings.nl>
2015-10-19 13:07:19 +0200

    Removed Profile functionality

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,7 +25,6 @@ class AppKernel extends Kernel
             new OpenConext\EngineBlock\CompatibilityBundle\OpenConextEngineBlockCompatibilityBundle(),
             new OpenConext\EngineBlock\ApiBundle\OpenConextEngineBlockApiBundle(),
             new OpenConext\EngineBlock\AuthenticationBundle\OpenConextEngineBlockAuthenticationBundle(),
-            new OpenConext\EngineBlock\ProfileBundle\OpenConextEngineBlockProfileBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/OpenConext/EngineBlock/ProfileBundle/OpenConextEngineBlockProfileBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OpenConext\EngineBlock\ProfileBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class OpenConextEngineBlockProfileBundle extends Bundle
-{
-}
