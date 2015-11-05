78850c44fdc61cdd7a8ebec785af086ea16aa65e
Reinier Kip <reinier@ibuildings.nl>
2015-11-05 10:25:24 +0100

    Check consent listing API feature is enabled and user has access

--- src/OpenConext/EngineBlock/ApiBundle/Controller/ConsentController.php
+++ src/OpenConext/EngineBlock/ApiBundle/Controller/ConsentController.php
@@ -3,10 +3,14 @@
 namespace OpenConext\EngineBlock\ApiBundle\Controller;
 
 use OpenConext\EngineBlock\ApiBundle\Exception\RuntimeException;
+use OpenConext\EngineBlock\ApiBundle\Http\Exception\ApiAccessDeniedHttpException;
 use OpenConext\EngineBlock\ApiBundle\Http\Exception\ApiInternalServerErrorHttpException;
 use OpenConext\EngineBlock\ApiBundle\Service\ConsentService;
+use OpenConext\EngineBlock\ApiBundle\Service\FeaturesService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 final class ConsentController
 {
@@ -15,13 +19,38 @@ final class ConsentController
      */
     private $consentService;
 
-    public function __construct(ConsentService $consentService)
-    {
-        $this->consentService = $consentService;
+    /**
+     * @var FeaturesService
+     */
+    private $featuresService;
+
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $authorisationChecker;
+
+    public function __construct(
+        ConsentService $consentService,
+        FeaturesService $featuresService,
+        AuthorizationCheckerInterface $authorisationChecker
+    ) {
+        $this->consentService       = $consentService;
+        $this->featuresService      = $featuresService;
+        $this->authorisationChecker = $authorisationChecker;
     }
 
     public function userAction($userId)
     {
+        if (!$this->featuresService->consentListingIsEnabled()) {
+            throw new NotFoundHttpException('Consent listing API is disabled');
+        }
+
+        if (!$this->authorisationChecker->isGranted('ROLE_API_USER_PROFILE')) {
+            throw new ApiAccessDeniedHttpException(
+                'Access to the content listing API requires the role ROLE_API_USER_PROFILE'
+            );
+        }
+
         try {
             $consentList = $this->consentService->findAll($userId)->jsonSerialize();
         } catch (RuntimeException $e) {
