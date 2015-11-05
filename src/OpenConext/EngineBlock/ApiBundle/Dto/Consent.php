2d9bbc24532a874ae1719cba3e89928c1cde5a73
Reinier Kip <reinier@ibuildings.nl>
2015-11-05 11:04:50 +0100

    Move locales to attribute level, add entity ID to service provider

--- src/OpenConext/EngineBlock/ApiBundle/Dto/Consent.php
+++ src/OpenConext/EngineBlock/ApiBundle/Dto/Consent.php
@@ -3,36 +3,32 @@
 namespace OpenConext\EngineBlock\ApiBundle\Dto;
 
 use DateTime;
+use OpenConext\Component\EngineBlockMetadata\ContactPerson;
 use OpenConext\Component\EngineBlockMetadata\Entity\ServiceProvider;
 use OpenConext\EngineBlock\Authentication\Entity\Consent as ConsentEntity;
 
 final class Consent
 {
-    /**
-     * The service for which attribute release consent was given.
-     *
-     * @var ServiceLocaleMap
-     */
-    public $service;
+    const CONTACT_TYPE_SUPPORT = 'support';
 
     /**
-     * @var DateTime
+     * @var ConsentEntity
      */
-    public $consentGivenOn;
+    private $consent;
 
     /**
-     * @var DateTime
+     * @var ServiceProvider
      */
-    public $lastUsedOn;
+    private $serviceProvider;
 
-    public static function fromConsentAndServiceProvider(ConsentEntity $consent, ServiceProvider $serviceProvider)
+    /**
+     * @param ConsentEntity   $consent
+     * @param ServiceProvider $serviceProvider
+     */
+    public function __construct(ConsentEntity $consent, ServiceProvider $serviceProvider)
     {
-        $dto = new Consent();
-        $dto->service = ServiceLocaleMap::fromServiceProvider($serviceProvider);
-        $dto->consentGivenOn = $consent->getDateConsentWasGivenOn();
-        $dto->lastUsedOn = $consent->getDateLastUsedOn();
-
-        return $dto;
+        $this->consent         = $consent;
+        $this->serviceProvider = $serviceProvider;
     }
 
     /**
@@ -40,10 +36,35 @@ final class Consent
      */
     public function jsonSerialize()
     {
+        $supportContacts = array_values(
+            array_filter(
+                $this->serviceProvider->contactPersons,
+                function (ContactPerson $contact) {
+                    return $contact->contactType === Consent::CONTACT_TYPE_SUPPORT;
+                }
+            )
+        );
+
+        $supportEmail = null;
+        if (count($supportContacts) > 0) {
+            $supportEmail = $supportContacts[0]->emailAddress;
+        }
+
+        $serviceProvider = array(
+            'entity_id'    => $this->serviceProvider->entityId,
+            'display_name' => array(
+                'en' => $this->serviceProvider->displayNameEn,
+                'nl' => $this->serviceProvider->displayNameNl,
+            ),
+            'eula_url' => $this->serviceProvider->termsOfServiceUrl,
+            'support_url' => null,
+            'support_email' => $supportEmail,
+        );
+
         return array(
-            'service'          => $this->service->jsonSerialize(),
-            'consent_given_on' => $this->consentGivenOn->format(DateTime::ATOM),
-            'last_used_on'     => $this->lastUsedOn->format(DateTime::ATOM),
+            'service_provider' => $serviceProvider,
+            'consent_given_on' => $this->consent->getDateConsentWasGivenOn()->format(DateTime::ATOM),
+            'last_used_on'     => $this->consent->getDateLastUsedOn()->format(DateTime::ATOM),
         );
     }
 }
--- src/OpenConext/EngineBlock/ApiBundle/Service/ConsentService.php
+++ src/OpenConext/EngineBlock/ApiBundle/Service/ConsentService.php
@@ -79,6 +79,6 @@ final class ConsentService
             return null;
         }
 
-        return Consent::fromConsentAndServiceProvider($consent, $serviceProvider);
+        return new Consent($consent, $serviceProvider);
     }
 }
