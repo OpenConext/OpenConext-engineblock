a500b01f32a293a5bbde488f9c8e161d3441cd88
Boy Baukema <boy@ibuildings.nl>
2015-11-20 14:50:40 +0100

    Rename RemoveVoGroups to FilterReservedMemberOfValues

---
+++ library/EngineBlock/Corto/Filter/Command/FilterReservedMemberOfValues.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Remove any IDP set urn:collab:org:... only SURFconext is allowed to set these.
+ */
+class EngineBlock_Corto_Filter_Command_FilterReservedMemberOfValues extends EngineBlock_Corto_Filter_Command_Abstract
+{
+    const URN_COLLAB_ORG_PREFIX = 'urn:collab:org:';
+    const URN_IS_MEMBER_OF      = 'urn:mace:dir:attribute-def:isMemberOf';
+
+    /**
+     * This command may modify the response attributes
+     *
+     * @return array
+     */
+    public function getResponseAttributes()
+    {
+        return $this->_responseAttributes;
+    }
+
+    public function execute()
+    {
+        if (!isset($this->_responseAttributes[self::URN_IS_MEMBER_OF])) {
+            return;
+        }
+
+        $groups = &$this->_responseAttributes[self::URN_IS_MEMBER_OF];
+
+        for ($i = 0; $i < count($groups); $i++) {
+            $hasVoPrefix = strpos($groups[$i], self::URN_COLLAB_ORG_PREFIX) === 0;
+
+            if (!$hasVoPrefix) {
+                continue;
+            }
+
+            unset($groups[$i]);
+
+            EngineBlock_ApplicationSingleton::getLog()->notice(
+                sprintf(
+                    'FilterReservedMemberOfValue: Removed "%s" value from %s attribute by %s',
+                    $groups[$i],
+                    self::URN_IS_MEMBER_OF,
+                    $this->_identityProvider->entityId
+                )
+            );
+        }
+    }
+}
