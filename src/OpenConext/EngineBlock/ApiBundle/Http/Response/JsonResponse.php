96631d055a7f0ed64e6d3466f2edd3353db9370d
Alex Rothuis <arothuis@ibuildings.nl>
2015-12-24 09:55:36 +0100

    Adds string typecheck to JsonResponse

--- src/OpenConext/EngineBlock/ApiBundle/Http/Response/JsonResponse.php
+++ src/OpenConext/EngineBlock/ApiBundle/Http/Response/JsonResponse.php
@@ -2,12 +2,17 @@
 
 namespace OpenConext\EngineBlock\ApiBundle\Http\Response;
 
+use OpenConext\Value\Exception\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Response;
 
 final class JsonResponse extends Response
 {
     public function __construct($content = '', $status = 200, array $headers = array())
     {
+        if (!is_string($content)) {
+            throw InvalidArgumentException::invalidType('string', 'content', $content);
+        }
+
         parent::__construct($content, $status, $headers);
 
         $this->headers->set('Content-Type', 'application/json');
