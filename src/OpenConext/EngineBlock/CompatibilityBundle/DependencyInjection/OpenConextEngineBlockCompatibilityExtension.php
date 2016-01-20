74a07c60f8180f2bd6fd1c43ffa0e03ad771aacd
Alex Rothuis <arothuis@ibuildings.nl>
2016-01-20 17:54:16 +0100

    Adds EngineBlock configuration as a service

--- src/OpenConext/EngineBlock/CompatibilityBundle/DependencyInjection/OpenConextEngineBlockCompatibilityExtension.php
+++ src/OpenConext/EngineBlock/CompatibilityBundle/DependencyInjection/OpenConextEngineBlockCompatibilityExtension.php
@@ -2,6 +2,9 @@
 
 namespace OpenConext\EngineBlock\CompatibilityBundle\DependencyInjection;
 
+use EngineBlock_Application_Bootstrapper;
+use OpenConext\EngineBlock\CompatibilityBundle\Configuration\EngineBlockConfigurationLoader;
+use OpenConext\EngineBlock\CompatibilityBundle\Configuration\EngineBlockIniFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -20,5 +23,15 @@ class OpenConextEngineBlockCompatibilityExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('event_listeners.yml');
+
+        $engineBlockConfigLoader = new EngineBlockConfigurationLoader($container, new EngineBlockIniFileLoader);
+        $engineblockConfig = $engineBlockConfigLoader->loadFiles(array(
+            ENGINEBLOCK_FOLDER_APPLICATION . EngineBlock_Application_Bootstrapper::CONFIG_FILE_DEFAULT,
+            EngineBlock_Application_Bootstrapper::CONFIG_FILE_ENVIRONMENT,
+        ));
+
+        $container
+            ->getDefinition('eb.compat.engineblock_config')
+            ->replaceArgument(0, $engineblockConfig);
     }
 }
