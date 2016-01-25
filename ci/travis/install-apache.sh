606d0089897f3be8020e2d743f92cd5f25f405d0
DRvanR <dvrenterghem@ibuildings.nl>
2016-01-25 16:19:03 +0100

    Trying to configure travis correctly
    - create vhost if needed for behat
    - split running behat and code-quality checks
    - install code quality tools in dev
    - add code quality configurations
    - added stub files to allow EB to bootstrap completely
    - removed functional testing flag from default config

---
+++ ci/travis/install-apache.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -ev
+
+sudo apt-get update \
+&& sudo apt-get install apache2 libapache2-mod-fastcgi \
+&& sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf \
+&& sudo a2enmod rewrite actions fastcgi alias \
+&& echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini \
+&& ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm \
+&& sudo cp -f build/travis/vhost-apache /etc/apache2/sites-available/default \
+&& sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default \
+&& sudo service apache2 restart
