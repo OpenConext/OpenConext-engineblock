97d803f38b870769c119440a2bef93c54b86e28c
Alex Rothuis <arothuis@ibuildings.nl>
2016-02-02 14:20:22 +0100

    Removes unnecessary code from AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,17 +1,10 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-    public function __construct($environment, $debug)
-    {
-        parent::__construct($environment, $debug);
-    }
-
     public function registerBundles()
     {
         $bundles = array(
@@ -34,27 +27,6 @@ class AppKernel extends Kernel
         }
 
         return $bundles;
-    }
-
-    /**
-     * {@inheritDoc} We're overriding the default functionality here to be able to set the configured layout on
-     * the application singleton. This because the view/layout system has to be configured to point to the correct
-     * directories for rendering templates. Since not all templates are rendered in the controllers, but can be
-     * rendered by the EngineBlock_Corto_ProxyServer we have to ensure that that uses the same configured view and
-     * layout system.
-     * This must be done every request. The only method that is guaranteed to execute for every request is this method.
-     * However, we must run *after* the boot call, which ensures the container is built, since we need a service
-     * from the container. This could also be located in the getHttpKernel method, however that would introduce a
-     * side-effect to a getter method. Another option could be to configure the layout and view in the proyxserver,
-     * however that makes refactoring in the future harder.
-     */
-    public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
-    {
-        if (false === $this->booted) {
-            $this->boot();
-        }
-
-        return $this->getHttpKernel()->handle($request, $type, $catch);
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
