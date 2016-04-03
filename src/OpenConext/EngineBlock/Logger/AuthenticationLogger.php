962eaa835c04654588ad4c1e6e6ea8be284962e4
DRvanR <dvrenterghem@ibuildings.nl>
2016-04-03 13:16:48 +0200

    Add AuthenticationLogger for EngineBlock

---
+++ src/OpenConext/EngineBlock/Logger/AuthenticationLogger.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace OpenConext\EngineBlock\Logger;
+
+use DateTime;
+use OpenConext\EngineBlock\Authentication\Value\CollabPersonId;
+use OpenConext\EngineBlock\Authentication\Value\KeyId;
+use OpenConext\Value\Saml\Entity;
+use Psr\Log\LoggerInterface;
+
+class AuthenticationLogger
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param Entity         $serviceProvider
+     * @param Entity         $identityProvider
+     * @param CollabPersonId $collabPersonId
+     * @param KeyId          $keyId
+     */
+    public function logGrantedLogin(
+        Entity $serviceProvider,
+        Entity $identityProvider,
+        CollabPersonId $collabPersonId,
+        KeyId $keyId
+    ) {
+        $this->logger->info(
+            'login granted',
+            [
+                'login_stamp'   => (new DateTime())->format(DateTime::ISO8601),
+                'user_id'       => $collabPersonId->getCollabPersonId(),
+                'sp_entity_id'  => $serviceProvider->getEntityId()->getEntityId(),
+                'idp_entity_id' => $identityProvider->getEntityId()->getEntityId(),
+                'key_id'        => $keyId->getKeyId(),
+            ]
+        );
+    }
+}
