4e3d58eb7a054b820fb5748abe83ff27d4084074
DRvanR <dvrenterghem@ibuildings.nl>
2016-04-03 14:06:30 +0200

    Add AuthenticationLoggerAdapter

---
+++ src/OpenConext/EngineBlockBridge/Logger/AuthenticationLoggerAdapter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace OpenConext\EngineBlockBridge\Logger;
+
+use OpenConext\Component\EngineBlockMetadata\Entity\IdentityProvider;
+use OpenConext\Component\EngineBlockMetadata\Entity\ServiceProvider;
+use OpenConext\EngineBlock\Authentication\Value\CollabPersonId;
+use OpenConext\EngineBlock\Authentication\Value\KeyId;
+use OpenConext\EngineBlock\Logger\AuthenticationLogger;
+use OpenConext\Value\Saml\Entity;
+use OpenConext\Value\Saml\EntityId;
+use OpenConext\Value\Saml\EntityType;
+
+class AuthenticationLoggerAdapter
+{
+    /**
+     * @var AuthenticationLogger
+     */
+    private $authenticationLogger;
+
+    public function __construct(AuthenticationLogger $authenticationLogger)
+    {
+        $this->authenticationLogger = $authenticationLogger;
+    }
+
+    /**
+     * @param ServiceProvider  $serviceProvider
+     * @param IdentityProvider $identityProvider
+     * @param                  $collabPersonId
+     * @param                  $keyId
+     */
+    public function logLogin(
+        ServiceProvider $serviceProvider,
+        IdentityProvider $identityProvider,
+        $collabPersonId,
+        $keyId
+    ) {
+        $this->authenticationLogger->logGrantedLogin(
+            new Entity(new EntityId($serviceProvider->entityId), EntityType::SP()),
+            new Entity(new EntityId($identityProvider->entityId), EntityType::IdP()),
+            new CollabPersonId($collabPersonId),
+            new KeyId($keyId)
+        );
+    }
+}
