8356149357ea27d5efdc6fd3fb5981b0969693e7
Alex Rothuis <arothuis@ibuildings.nl>
2016-10-21 16:25:54 +0200

    Add ARP API controller

---
+++ src/OpenConext/EngineBlockBundle/Controller/Api/AttributeReleasePolicyController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace OpenConext\EngineBlockBundle\Controller\Api;
+
+use EngineBlock_Arp_AttributeReleasePolicyEnforcer;
+use OpenConext\EngineBlock\Service\MetadataService;
+use OpenConext\EngineBlockBundle\Http\Exception\ApiAccessDeniedHttpException;
+use OpenConext\EngineBlockBundle\Http\Exception\BadApiRequestHttpException;
+use OpenConext\EngineBlockBundle\Http\Response\JsonResponse;
+use OpenConext\Value\Saml\EntityId;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+final class AttributeReleasePolicyController
+{
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $authorizationChecker;
+
+    /**
+     * @var MetadataService
+     */
+    private $metadataService;
+
+    /**
+     * @var EngineBlock_Arp_AttributeReleasePolicyEnforcer
+     */
+    private $arpEnforcer;
+
+    public function __construct(
+        AuthorizationCheckerInterface $authorizationChecker,
+        MetadataService $metadataService,
+        EngineBlock_Arp_AttributeReleasePolicyEnforcer $arpEnforcer
+    ) {
+        $this->authorizationChecker = $authorizationChecker;
+        $this->metadataService      = $metadataService;
+        $this->arpEnforcer          = $arpEnforcer;
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function applyArpAction(Request $request)
+    {
+        if (!$this->authorizationChecker->isGranted('ROLE_API_USER_PROFILE')) {
+            throw new ApiAccessDeniedHttpException('Access to the ARP API requires the role ROLE_API_USER_PROFILE');
+        }
+
+        $body = json_decode($request->getContent(), true);
+
+        if (!is_array($body)) {
+            throw new BadApiRequestHttpException('Unrecognized structure for JSON: ' . var_export($body, true));
+        }
+
+        if (!isset($body['entityIds']) && !is_array($body['entityIds'])) {
+            throw new BadApiRequestHttpException('Unrecognized structure for JSON key "entityIds" not found');
+        }
+
+        if (!isset($body['attributes']) && !is_array($body['attributes'])) {
+            throw new BadApiRequestHttpException('Unrecognized structure for JSON: key "attributes" not found');
+        }
+
+        $releasedAttributes = [];
+        foreach ($body['entityIds'] as $entityId) {
+            $serviceProvider = $this->metadataService->findServiceProvider(new EntityId($entityId));
+
+            if ($serviceProvider === null) {
+                continue;
+            }
+
+            $arp = $this->metadataService->findArpFor($serviceProvider);
+
+            $releasedAttributes[$entityId] = $this->arpEnforcer->enforceArp($arp, $body['attributes']);
+        }
+
+        return new JsonResponse(json_encode($releasedAttributes));
+    }
+}
