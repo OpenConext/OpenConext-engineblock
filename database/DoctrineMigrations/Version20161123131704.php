9ca5ebc54d229135e56d7f7968647d3893b3dd5a
Alex Rothuis <arothuis@ibuildings.nl>
2016-11-23 13:47:34 +0100

    Add migration for remove implicit_vo_id from sso_provider_roles(_eb5)

--- database/DoctrineMigrations/Version20161123131704.php
+++ database/DoctrineMigrations/Version20161123131704.php
@@ -21,6 +21,10 @@ class Version20161123131704 extends AbstractMigration
         $this->addSql('DROP TABLE virtual_organisation');
         $this->addSql('DROP TABLE virtual_organisation_group');
         $this->addSql('DROP TABLE virtual_organisation_idp');
+
+        // Remove the implicit_vo_id from the sso_provider_roles
+        $this->addSql('ALTER TABLE sso_provider_roles DROP COLUMN implicit_vo_id');
+        $this->addSql('ALTER TABLE sso_provider_roles_eb5 DROP COLUMN implicit_vo_id');
     }
 
     /**
@@ -32,5 +36,8 @@ class Version20161123131704 extends AbstractMigration
         $this->addSql('CREATE TABLE IF NOT EXISTS virtual_organisation (vo_id VARCHAR(255) NOT NULL, vo_type enum(\'GROUP\',\'STEM\',\'IDP\', \'MIXED\') NOT NULL, PRIMARY KEY (vo_id)) ENGINE=InnoDB');
         $this->addSql('CREATE TABLE IF NOT EXISTS virtual_organisation_group (vo_id VARCHAR(255) NOT NULL, group_id VARCHAR(255) NOT NULL, PRIMARY KEY (vo_id, group_id)) ENGINE=InnoDB');
         $this->addSql('CREATE TABLE IF NOT EXISTS virtual_organisation_idp (vo_id varchar(255) NOT NULL, idp_id varchar(255) NOT NULL, PRIMARY KEY (vo_id, idp_id)) ENGINE=InnoDB');
+
+        $this->addSql('ALTER TABLE sso_provider_roles ADD implicit_vo_id VARCHAR(255) DEFAULT NULL AFTER is_trusted_proxy');
+        $this->addSql('ALTER TABLE sso_provider_roles_eb5 ADD implicit_vo_id VARCHAR(255) COLLATE utf8_unicode_ci DEFAULT NULL AFTER is_trusted_proxy');
     }
 }
