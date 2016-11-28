d59e293f294dc5ccf0f8f9e44c65294da4d7f091
Alex Rothuis <arothuis@ibuildings.nl>
2016-11-28 16:17:24 +0100

    Cherry-pick: Add setting attributes on MockIdP to MockIdpContext
     Conflicts:
    	src/OpenConext/EngineBlockFunctionalTestingBundle/Features/Context/MockIdpContext.php
    
     cherry-picking commit 4c6f758.

--- src/OpenConext/EngineBlockFunctionalTestingBundle/Features/Context/MockIdpContext.php
+++ src/OpenConext/EngineBlockFunctionalTestingBundle/Features/Context/MockIdpContext.php
@@ -280,4 +280,20 @@ class MockIdpContext extends AbstractSubContext
         $mink = $this->getMainContext()->getMinkContext();
         $mink->pressButton('GO');
     }
+
+    /**
+     * @Given /^the IdP "([^"]*)" sends attribute "([^"]*)" with value "([^"]*)"$/
+     * @param string $idpName
+     * @param string $attributeName
+     * @param string $attributeValue
+     */
+    public function theIdPSendsAttributeWithValue($idpName, $attributeName, $attributeValue)
+    {
+        /** @var MockIdentityProvider $mockIdp */
+        $mockIdp = $this->mockIdpRegistry->get($idpName);
+
+        $mockIdp->setAttribute($attributeName, [$attributeValue]);
+
+        $this->mockIdpRegistry->save();
+    }
 }
