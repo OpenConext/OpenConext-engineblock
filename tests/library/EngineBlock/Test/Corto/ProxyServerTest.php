105d44120ee6fcbed641e6a0fc13c18341ee7d3f
Alex Rothuis <arothuis@ibuildings.nl>
2016-12-19 17:01:16 +0100

    Test that AllowCreate is set in ProxyServer

--- tests/library/EngineBlock/Test/Corto/ProxyServerTest.php
+++ tests/library/EngineBlock/Test/Corto/ProxyServerTest.php
@@ -32,6 +32,31 @@ class EngineBlock_Test_Corto_ProxyServerTest extends PHPUnit_Framework_TestCase
         );
     }
 
+    public function testAllowCreateIsSet()
+    {
+        $proxyServer = $this->factoryProxyServer();
+
+        $originalRequest = $this->factoryOriginalRequest();
+        $identityProvider = $proxyServer->getRepository()->fetchIdentityProviderByEntityId('testIdp');
+        /** @var SAML2_AuthnRequest $enhancedRequest */
+        $enhancedRequest = EngineBlock_Saml2_AuthnRequestFactory::createFromRequest(
+            $originalRequest,
+            $identityProvider,
+            $proxyServer
+        );
+
+        $nameIdPolicy = $enhancedRequest->getNameIdPolicy();
+
+        $this->assertContains(
+            'AllowCreate',
+            array_keys($nameIdPolicy),
+            'The NameIDPolicy should contain the key "AllowCreate"',
+            false,
+            true,
+            true
+        );
+    }
+
     public function testNameIDFormatIsSetFromRemoteMetaData()
     {
         $proxyServer = $this->factoryProxyServer();
