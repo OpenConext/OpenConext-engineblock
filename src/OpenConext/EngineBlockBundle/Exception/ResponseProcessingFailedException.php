b76e595dd31d54f7e71e205d6a1667d77925dadd
Alex Rothuis <arothuis@ibuildings.nl>
2017-01-18 11:27:09 +0100

    Use custom exceptions in the EngineBlock bundle

--- src/OpenConext/EngineBlockBundle/Exception/ResponseProcessingFailedException.php
+++ src/OpenConext/EngineBlockBundle/Exception/ResponseProcessingFailedException.php
@@ -2,8 +2,6 @@
 
 namespace OpenConext\EngineBlockBundle\Exception;
 
-use RuntimeException;
-
 class ResponseProcessingFailedException extends RuntimeException implements AddExecutionTimePadding
 {
 }
