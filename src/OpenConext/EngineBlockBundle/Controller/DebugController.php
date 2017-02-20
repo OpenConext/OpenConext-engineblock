7f3432414e17cfa0dcc50d46c7348f182ae9f232
Alex Rothuis <arothuis@ibuildings.nl>
2017-02-20 09:56:26 +0100

    Make sure an authentication is started on behalf of a debug SP

--- src/OpenConext/EngineBlockBundle/Controller/DebugController.php
+++ src/OpenConext/EngineBlockBundle/Controller/DebugController.php
@@ -5,17 +5,29 @@ namespace OpenConext\EngineBlockBundle\Controller;
 use EngineBlock_ApplicationSingleton;
 use EngineBlock_Corto_Adapter;
 use OpenConext\EngineBlockBridge\ResponseFactory;
+use OpenConext\Value\Saml\Entity;
+use OpenConext\Value\Saml\EntityId;
+use OpenConext\Value\Saml\EntityType;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
-class DebugController
+class DebugController implements AuthenticationLoopThrottlingController
 {
     /**
      * @var EngineBlock_ApplicationSingleton
      */
     private $engineBlockApplicationSingleton;
 
-    public function __construct(EngineBlock_ApplicationSingleton $engineBlockApplicationSingleton)
-    {
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    public function __construct(
+        EngineBlock_ApplicationSingleton $engineBlockApplicationSingleton,
+        SessionInterface $session
+    ) {
         $this->engineBlockApplicationSingleton = $engineBlockApplicationSingleton;
+        $this->session = $session;
     }
 
     /**
@@ -26,6 +38,9 @@ class DebugController
         $proxyServer = new EngineBlock_Corto_Adapter();
 
         $proxyServer->debugSingleSignOn();
+
+        $authenticationState = $this->session->get('authentication_state');
+        $authenticationState->startAuthenticationOnBehalfOf(new Entity(new EntityId('debug_sp'), EntityType::SP()));
 
         return ResponseFactory::fromEngineBlockResponse($this->engineBlockApplicationSingleton->getHttpResponse());
     }
