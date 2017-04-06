a6ca47c2a1a00895acb6a1aa3b439865da5a0006
Nic Wortel <nic@nicwortel.nl>
2017-04-06 15:08:15 +0200

    Allow to override the httpOnly and secure flags of the cookie

--- src/OpenConext/EngineBlockBundle/Http/Cookies/CookieFactory.php
+++ src/OpenConext/EngineBlockBundle/Http/Cookies/CookieFactory.php
@@ -2,6 +2,7 @@
 
 namespace OpenConext\EngineBlockBundle\Http\Cookies;
 
+use Assert\Assertion;
 use DateInterval;
 use DateTime;
 use Symfony\Component\HttpFoundation\Cookie;
@@ -24,15 +25,32 @@ final class CookieFactory
     private $expiry;
 
     /**
+     * @var bool
+     */
+    private $httpOnly;
+
+    /**
+     * @var bool
+     */
+    private $secure;
+
+    /**
      * @param string   $name
      * @param string   $domain
      * @param int|null $expiry
+     * @param bool     $httpOnly
+     * @param bool     $secure
      */
-    public function __construct($name, $domain, $expiry = null)
+    public function __construct($name, $domain, $expiry = null, $httpOnly = false, $secure = false)
     {
+        Assertion::boolean($httpOnly);
+        Assertion::boolean($secure);
+
         $this->name = $name;
         $this->domain = $domain;
         $this->expiry = $expiry;
+        $this->httpOnly = $httpOnly;
+        $this->secure = $secure;
     }
 
     /**
@@ -54,8 +72,8 @@ final class CookieFactory
             $expire,
             '/',
             $this->domain,
-            false,
-            false
+            $this->secure,
+            $this->httpOnly
         );
     }
 }
