705fb5f3e8124c28cdfd61bb22079e12c1de0532
Nic Wortel <nic@nicwortel.nl>
2017-04-06 13:45:09 +0200

    Create an event listener that sets the locale on the Corto translator

---
+++ src/OpenConext/EngineBlockBridge/EventListener/CortoTranslationsLocaleListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace OpenConext\EngineBlockBridge\EventListener;
+
+use EngineBlock_ApplicationSingleton;
+use OpenConext\EngineBlockBundle\Localization\LocaleProvider;
+use Zend_Translate_Adapter;
+
+final class CortoTranslationsLocaleListener
+{
+    /**
+     * @var LocaleProvider
+     */
+    private $localeProvider;
+
+    /**
+     * @var Zend_Translate_Adapter
+     */
+    private $translator;
+
+    public function __construct(LocaleProvider $localeProvider)
+    {
+        $this->localeProvider = $localeProvider;
+        $this->translator = EngineBlock_ApplicationSingleton::getInstance()->getTranslator()->getAdapter();
+    }
+
+    public function onKernelRequest()
+    {
+        $locale = $this->localeProvider->getLocale();
+
+        $this->translator->setLocale($locale);
+    }
+}
