095d0427e342fcfb17d9ed5c6d049536a7a8dd1e
Nic Wortel <nic@nicwortel.nl>
2017-04-11 16:16:39 +0200

    Explain why the listener priorities of the locale listeners are important

--- src/OpenConext/EngineBlockBridge/EventListener/SetCortoTranslationsLocaleListener.php
+++ src/OpenConext/EngineBlockBridge/EventListener/SetCortoTranslationsLocaleListener.php
@@ -7,8 +7,10 @@ use OpenConext\EngineBlockBundle\Localization\LocaleProvider;
 use Zend_Translate_Adapter;
 
 /**
- * This listener depends on the LocaleListener in the EngineBlockBundle, so the priority should be set so that it is
- * called after the LocaleListener.
+ * This listener depends on the LocaleListener in the EngineBlockBundle (which has to scope the LocaleProvider with the
+ * current request), so the priority should be set so that it is called after the LocaleListener. Otherwise, if the
+ * LocaleProvider hasn't been scoped with the request yet, it will fall back to the default locale and all user
+ * preferences are ignored.
  */
 final class SetCortoTranslationsLocaleListener
 {
