7a8cc17091c7c07f1c0691d087823b61b6b6e647
Alex Rothuis <arothuis@ibuildings.nl>
2017-04-12 17:30:16 +0200

    Correct test namespace/dir, clean-up heartbeat test

---
+++ tests/functional/OpenConext/EngineBlockBundle/Controller/Api/HeartbeatControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OpenConext\EngineBlockBundle\Tests;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Component\HttpFoundation\Response;
+
+final class HeartbeatControllerTest extends WebTestCase
+{
+    /**
+     * @test
+     * @group Api
+     */
+    public function engineblock_has_a_heartbeat()
+    {
+        $client = $this->createClient();
+        $client->request('GET', 'https://engine-api.vm.openconext.org/');
+        $this->assertStatusCode(Response::HTTP_OK, $client);
+    }
+}
