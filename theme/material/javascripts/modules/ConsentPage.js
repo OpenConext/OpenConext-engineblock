908d7fd7bcdb1384c90752bf1f9426b5cc6c851c
Joris Steyn <joris@ibuildings.nl>
2017-11-27 20:46:37 +0100

    Show attribute tooltip on consent page

--- theme/material/javascripts/modules/ConsentPage.js
+++ theme/material/javascripts/modules/ConsentPage.js
@@ -1,9 +1,25 @@
 export function initConsentPage() {
+    const tippy = require('./../../../node_modules/tippy.js/dist/tippy.all.min.js');
+
     focusAcceptButton();
     initAttributesToggle();
+    initTooltips();
 
     function focusAcceptButton() {
         document.getElementById('accept_terms_button').focus();
+    }
+
+    function initTooltips() {
+        tippy(
+            document.querySelectorAll('a.tooltip'),
+            {
+                animation: 'scale',
+                arrow: true,
+                duration: 200,
+                placement: 'top',
+                theme: 'light bordered'
+            }
+        );
     }
 
     function initAttributesToggle() {
