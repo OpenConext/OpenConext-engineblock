78a202c469d631d3a2f16f8013434e1e2b197098
Joris Steyn <joris@ibuildings.nl>
2017-12-01 16:32:33 +0100

    Remove tooltip from attribute values

--- theme/material/javascripts/modules/ConsentPage.js
+++ theme/material/javascripts/modules/ConsentPage.js
@@ -1,36 +1,10 @@
 export function initConsentPage() {
-    const tippy = require('./../../../node_modules/tippy.js/dist/tippy.all.min.js');
-
     focusAcceptButton();
     initAttributesToggle();
-    initTooltips();
     initSlideIns();
 
     function focusAcceptButton() {
         document.getElementById('accept_terms_button').focus();
-    }
-
-    function initTooltips() {
-        const anchors = document.querySelectorAll('a.tooltip');
-
-        tippy(
-            anchors,
-            {
-                animation: 'scale',
-                arrow: true,
-                duration: 200,
-                placement: 'top',
-                theme: 'light bordered'
-            }
-        );
-
-        Array.prototype.forEach.call(
-            anchors,
-            (anchor) => anchor.addEventListener(
-                'click',
-                (event) => event.preventDefault()
-            )
-        );
     }
 
     function initAttributesToggle() {
