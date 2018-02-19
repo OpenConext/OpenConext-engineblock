24929c0f2842d66284fa86375e83e07ff472f2ad
Joris Steyn <joris@ibuildings.nl>
2018-02-19 17:06:16 +0100

    Add doctrine configuration for the consent table

---
+++ src/OpenConext/EngineBlockBundle/Authentication/Entity/Consent.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace OpenConext\EngineBlockBundle\Authentication\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Note: this entity is currently only used to configure doctrine to create
+ * the schema on installation. The ConsentService and ConsentRepository do not
+ * use entities.
+ *
+ * @ORM\Entity()
+ * @ORM\Table(indexes={
+ *     @ORM\Index(name="hashed_user_id", columns={"hashed_user_id"}),
+ *     @ORM\Index(name="service_id", columns={"service_id"}),
+ * })
+ */
+class Consent
+{
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="consent_date", type="datetime", options={"default": 0})
+     */
+    public $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Id
+     * @ORM\Column(type="string", length=80)
+     */
+    public $hashedUserId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Id
+     * @ORM\Column(type="string")
+     */
+    public $serviceId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=80)
+     */
+    public $attribute;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="consent_type", type="string", nullable=true, length=20, options={"default": "explicit"})
+     */
+    public $type;
+}
