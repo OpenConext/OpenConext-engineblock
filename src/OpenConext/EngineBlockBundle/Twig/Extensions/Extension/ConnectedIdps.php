e9942efeca6fcddb1aa2b813900e9620208be187
Michiel Kodde <mkodde@ibuildings.nl>
2018-02-23 09:24:41 +0100

    Add Wayf Twig extension
    The Wayf Twig extension is used primarily to filter and normalize
     the list of IdPs for the template to use.
    
    A helper value object was added to contain the various IdP lists used
     to build the JSON config for the Wayf JavaScripts. The same value
     object is used to retrieve lists of connected and unconnected IdP's
     for display on the page.

---
+++ src/OpenConext/EngineBlockBundle/Twig/Extensions/Extension/ConnectedIdps.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Copyright 2018 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenConext\EngineBlockBundle\Twig\Extensions\Extension;
+
+/**
+ * Representation of an IdP list. This model is used in the Wayf template to load and filter
+ * IdP data.
+ */
+class ConnectedIdps
+{
+    private $formattedPreviousSelectionList = [];
+
+    private $formattedIdpList = [];
+
+    public function __construct(array $formattedPreviousSelectionList, array $formattedIdpList)
+    {
+        $this->formattedPreviousSelectionList = $formattedPreviousSelectionList;
+        $this->formattedIdpList = $formattedIdpList;
+    }
+
+    /**
+     * Merges the Previously selected IdP's and the currently available IdP's into one single array.
+     *
+     * @return array
+     */
+    public function getMergedIdpList()
+    {
+        return array_merge($this->formattedPreviousSelectionList, $this->getConnectedIdps());
+    }
+
+    /**
+     * Filters out all connected IdP's from the formattedIdpList.
+     * @return array
+     */
+    public function getConnectedIdps()
+    {
+        return array_filter(
+            $this->formattedIdpList,
+            function ($idp) {
+                return $idp['connected'];
+            }
+        );
+    }
+
+    public function getFormattedIdpList()
+    {
+        return $this->formattedIdpList;
+    }
+
+    public function getFormattedPreviousSelectionList()
+    {
+        return $this->formattedPreviousSelectionList;
+    }
+}
---
+++ src/OpenConext/EngineBlockBundle/Twig/Extensions/Extension/Wayf.php
@@ -0,0 +1,185 @@
+<?php
+
+/**
+ * Copyright 2018 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenConext\EngineBlockBundle\Twig\Extensions\Extension;
+
+use OpenConext\EngineBlock\Metadata\Entity\ServiceProvider;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Twig\TwigFunction;
+use Twig_Extension;
+use Zend_Translate_Adapter;
+
+class Wayf extends Twig_Extension
+{
+    const PREVIOUS_SELECTION_COOKIE_NAME = 'selectedidps';
+    const REMEMBER_CHOICE_COOKIE_NAME = 'rememberchoice';
+
+
+    /**
+     * @var Zend_Translate_Adapter
+     */
+    private $translator;
+
+    /**
+     * @var array|null
+     */
+    private $previousSelection;
+
+    public function __construct(RequestStack $requestStack, Zend_Translate_Adapter $translator)
+    {
+        $this->previousSelection = $this->loadPreviousSelectionFromCookie($requestStack);
+        $this->translator = $translator;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction(
+                'wayfConfig',
+                [$this, 'getWayfJsonConfig']
+            ),
+            new TwigFunction(
+                'connectedIdps',
+                [$this, 'getConnectedIdps']
+            ),
+
+        ];
+    }
+
+    /**
+     * @param array $idpList
+     * @param $locale
+     *
+     * @return ConnectedIdps
+     */
+    public function getConnectedIdps(
+        array $idpList,
+        $locale
+    ) {
+        $previousSelectionIndex = [];
+        if (!empty($this->previousSelection)) {
+            foreach ($this->previousSelection as $idp) {
+                $previousSelectionIndex[$idp['idp']] = $idp;
+            }
+        }
+
+        $formattedPreviousSelectionList = [];
+        $formattedIdpList = [];
+        foreach ($idpList as $idp) {
+            $idpKeywords = $idp['Keywords'] === 'Undefined' ? array() : array_values((array)$idp['Keywords']);
+
+            if (isset($previousSelectionIndex[$idp['EntityID']])) {
+                $formattedPreviousSelectionList[] = array_merge(
+                    $previousSelectionIndex[$idp['EntityID']],
+                    [
+                        'entityId' => $idp['EntityID'],
+                        'connected' => $idp['Access'] === '1',
+                        'displayTitle' => $idp['Name_' . $locale],
+                        'title' => strtolower($idp['Name_' . $locale]),
+                        'keywords' => strtolower(join('|', $idpKeywords)),
+                        'logo' => $idp['Logo'],
+                    ]
+                );
+                continue;
+            }
+
+            $formattedIdpList[] = [
+                'entityId' => $idp['EntityID'],
+                'connected' => $idp['Access'] === '1',
+                'displayTitle' => $idp['Name_'.$locale],
+                'title' => strtolower($idp['Name_'.$locale]),
+                'keywords' => strtolower(join('|', $idpKeywords)),
+                'logo' => $idp['Logo'],
+            ];
+        }
+
+        return new ConnectedIdps($formattedPreviousSelectionList, $formattedIdpList);
+    }
+
+    /**
+     * Retrieve the Wayf config used in JavaScript
+     *
+     * @param ConnectedIdps $connectedIdPs,
+     * @param ServiceProvider $serviceProvider
+     * @param string $currentLocale
+     * @param bool $showRequestAccess Show unconnected IdP's ?
+     * @param bool $rememberChoiceFeature Remember the chosen IdP in Wayf?
+     * @param int $cutoffPointForShowingUnfilteredIdps The cutoff point for showing unfiltered IdP's
+     *
+     * @return string Returns a json encoded config string. Used by the JavaScript of the Wayf to behave as intended.
+     */
+    public function getWayfJsonConfig(
+        ConnectedIdps $connectedIdPs,
+        ServiceProvider $serviceProvider,
+        $currentLocale,
+        $showRequestAccess,
+        $rememberChoiceFeature,
+        $cutoffPointForShowingUnfilteredIdps
+    ) {
+
+        if ($showRequestAccess === true) {
+            $unconnectedIdps = array_filter(
+                $connectedIdPs->getFormattedIdpList(),
+                function ($idp) {
+                    return !$idp['connected'];
+                }
+            );
+        } else {
+            $unconnectedIdps = [];
+        }
+
+        return json_encode(
+            [
+                'previousSelectionCookieName' => self::PREVIOUS_SELECTION_COOKIE_NAME,
+                'previousSelectionList' => $connectedIdPs->getFormattedPreviousSelectionList(),
+                'connectedIdps' => array_values($connectedIdPs->getConnectedIdps()),
+                'unconnectedIdps' => array_values($unconnectedIdps),
+                'cutoffPointForShowingUnfilteredIdps' => $cutoffPointForShowingUnfilteredIdps,
+                'rememberChoiceCookieName' => self::REMEMBER_CHOICE_COOKIE_NAME,
+                'rememberChoiceFeature' => $rememberChoiceFeature,
+                'messages' => [
+                    'pressEnterToSelect' => $this->translator->translate('press_enter_to_select'),
+                    'moreIdpResults' => $this->translator->translate('more_idp_results'),
+                    'requestAccess' => $this->translator->translate('request_access'),
+                ],
+                'requestAccessUrl' => '/authentication/idp/requestAccess?'.http_build_query(
+                    [
+                        'lang' => $currentLocale,
+                        'spEntityId' => $serviceProvider->entityId,
+                        'spName' => $serviceProvider->getDisplayName($currentLocale),
+                    ]
+                ),
+            ],
+            JSON_PRETTY_PRINT
+        );
+    }
+
+    private function loadPreviousSelectionFromCookie(RequestStack $requestStack)
+    {
+        $request = $requestStack->getCurrentRequest();
+        $previousSelection = null;
+        if ($request) {
+            $previousSelection = json_decode(
+                $request->cookies->get(self::PREVIOUS_SELECTION_COOKIE_NAME, null),
+                true
+            );
+        }
+
+        return $previousSelection;
+    }
+}
