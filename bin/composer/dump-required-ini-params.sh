OpenConext/OpenConext-engineblock

commit 917901f7a22867c1fcdb1f8ffa8c675bfcdc13ca

Move email configuration to symfony DI parameters

bin/composer/dump-required-ini-params.sh
@@ -189,6 +189,10 @@ $ymlContent = array(
             '%%',
             $config->get('openconext.termsOfUse')
         ),
+
+        // Email configuration
+        'email_help_address'                                      => $config->get('email.help'),
+        'email_idp_debugging'                                     => $config->get('email.idpDebugging'),
     )
 );
 

library/EngineBlock/Application/DiContainer.php
@@ -414,4 +414,12 @@ class EngineBlock_Application_DiContainer extends Pimple
     {
         return (bool) $this->container->getParameter('cookie.secure');
     }
+
+    /**
+     * @return array
+     */
+    public function getEmailIdpDebuggingConfiguration()
+    {
+        return (array) $this->container->getParameter('email_idp_debugging');
+    }
 }

library/EngineBlock/Corto/Module/Service/SingleSignOn.php
@@ -449,11 +449,13 @@ class EngineBlock_Corto_Module_Service_SingleSignOn extends EngineBlock_Corto_Mo
             )
         );
 
-        $emailConfiguration = EngineBlock_ApplicationSingleton::getInstance()->getConfigurationValue('email')->idpDebugging;
+        $emailConfiguration = EngineBlock_ApplicationSingleton::getInstance()->getDiContainer()
+            ->getEmailIdpDebuggingConfiguration();
+
         $mailer = new Zend_Mail('UTF-8');
-        $mailer->setFrom($emailConfiguration->from->address, $emailConfiguration->from->name);
-        $mailer->addTo($emailConfiguration->to->address, $emailConfiguration->to->name);
-        $mailer->setSubject(sprintf($emailConfiguration->subject, $identityProvider->nameEn));
+        $mailer->setFrom($emailConfiguration['from']['address'], $emailConfiguration['from']['name']);
+        $mailer->addTo($emailConfiguration['to']['address'], $emailConfiguration['to']['name']);
+        $mailer->setSubject(sprintf($emailConfiguration['subject'], $identityProvider->nameEn));
         $mailer->setBodyText($output);
         $mailer->send();
 

src/OpenConext/EngineBlockBundle/Resources/config/bridge.yml
@@ -13,7 +13,7 @@ services:
     engineblock.bridge.mailer:
         class: OpenConext\EngineBlockBridge\Mail\ZendMailSender
         arguments:
-            - "@=service('engineblock.bridge.config').get('email.help')"
+            - "%email_help_address%"
             - "@logger"
 
     engineblock.bridge.authentication_logger_adapter:

commit 54afb3dc135812ee6ce6d118ab85f3414ac92ea9

Fix missing idp debug mail configuration
The configuration was always set to 'null' in ini_parameters.yml, due
to a bug introduced in 917901f.

bin/composer/dump-required-ini-params.sh
@@ -204,7 +204,7 @@ $ymlContent = array(
 
         // Email configuration
         'email_help_address'                                      => $config->get('email.help'),
-        'email_idp_debugging'                                     => $config->get('email.idpDebugging'),
+        'email_idp_debugging'                                     => $config->get('email.idpDebugging')->toArray(),
 
         // Profile.
         'profile_base_url'                                        => $config->get('profile.baseUrl', ''),