da518e60b4e46bc423bb7ef9df4eeef4b1b8b712
Bas Strooband <bstrooband@ibuildings.nl>
2018-08-09 10:02:36 +0200

    Fix migration class name

--- database/DoctrineMigrations/Version20180804090135.php
+++ database/DoctrineMigrations/Version20180804090135.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180610194638 extends AbstractMigration
+class Version20180804090135 extends AbstractMigration
 {
     /**
      * @param Schema $schema
