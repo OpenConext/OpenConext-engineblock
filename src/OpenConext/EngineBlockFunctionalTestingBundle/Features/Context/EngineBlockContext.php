e02428764676c6d4e4cc1fa71901e3a5ab8cde16
Michiel Kodde <mkodde@ibuildings.nl>
2018-08-21 12:16:19 +0200

    Fix the broken Behat scenarios
    Some Behat 2 -> 3 migration issues needed to be addressed:
    
     - Profiles are now called suites, fixed this in the build.xml
    
     - ExecutionTimeTracker utilizes the ExecutionTime::of method that
       ascertains if the time is integer (miliseconds) this is no longer
       true when useing the Symfony stopwatch. It can and will return double
       representations of the time.
    
     - Gherkins TableNode has been made immutable, this required a small
       change in the EngineBlockContext
    
     - The base path is used in the behat.yml making it possible to run the
       tests from outside the application root directory. This is
       particularly useful when running the tests from an IDE

--- src/OpenConext/EngineBlockFunctionalTestingBundle/Features/Context/EngineBlockContext.php
+++ src/OpenConext/EngineBlockFunctionalTestingBundle/Features/Context/EngineBlockContext.php
@@ -206,7 +206,7 @@ HTML;
         array_pop($rows);
         array_pop($rows);
 
-        $actualTable = $actualTable::fromList($rows);
+        $actualTable = new TableNode($rows);
 
         $assert = new \Ingenerator\BehatTableAssert\AssertTable;
         $assert->isComparable($attributes, $actualTable, []);
