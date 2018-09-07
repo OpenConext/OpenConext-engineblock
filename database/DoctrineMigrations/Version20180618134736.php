22b8e1c9e74ecd6a587fa7d295c9bddb7e78ccb4
Bas Strooband <bstrooband@ibuildings.nl>
2018-09-07 12:19:18 +0200

    Add rolling update 5.8 compatibility support
    Add backwards compatibilty support for the following issues:
    /#544 consent configuration object oin column changed
    /#540 requesterid_required column added
    /#572 attribute_aggregation_required column added

--- database/DoctrineMigrations/Version20180618134736.php
+++ database/DoctrineMigrations/Version20180618134736.php
@@ -6,6 +6,8 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
+ * From now on rolling updates are supported.
+ *
  * Auto-generated Migration: Please modify to your needs!
  */
 class Version20180618134736 extends AbstractMigration
@@ -18,7 +20,7 @@ class Version20180618134736 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sso_provider_roles_eb5 CHANGE sps_entity_ids_without_consent consent_settings LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\'');
+        $this->addSql('ALTER TABLE sso_provider_roles_eb5 ADD consent_settings LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\'');
     }
 
     /**
@@ -29,6 +31,6 @@ class Version20180618134736 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sso_provider_roles_eb5 CHANGE consent_settings sps_entity_ids_without_consent LONGTEXT DEFAULT NULL COLLATE utf8_unicode_ci COMMENT \'(DC2Type:array)\'');
+        $this->addSql('ALTER TABLE sso_provider_roles_eb5 DROP consent_settings');
     }
 }
