722ee33e8902baa98c5ddc7d359874e0fc00f98c
Tim van Dijen <tvdijen@gmail.com>
2018-11-15 21:12:20 +0100

    Update dump-required-ini-params.sh

--- bin/composer/dump-required-ini-params.sh
+++ bin/composer/dump-required-ini-params.sh
@@ -198,7 +198,6 @@ $ymlContent = array(
         'view_default_title'                                      => $config->get('defaults.title'),
         'view_default_header'                                     => $config->get('defaults.header'),
         'view_default_logo'                                       => $config->get('defaults.logo'),
-        'ui_return_to_sp_link'                                    => (bool) $config->get('ui.return_to_sp_link.active'),
 
         // Store attributes with their values, meaning that if an Idp suddenly
         // sends a new value (like a new e-mail address) consent has to be
