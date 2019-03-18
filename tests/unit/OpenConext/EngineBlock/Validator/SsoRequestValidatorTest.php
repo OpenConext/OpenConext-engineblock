f3d5802be4c261feb311ae4e6f1d87c2bcb21394
Michiel Kodde <mkodde@ibuildings.nl>
2019-03-18 08:25:02 +0100

    Improve SsoRequestValidatorTest
     - Stop using annotation based expected exception statements
     - Start using @backupGlobals

--- tests/unit/OpenConext/EngineBlock/Validator/SsoRequestValidatorTest.php
+++ tests/unit/OpenConext/EngineBlock/Validator/SsoRequestValidatorTest.php
@@ -3,15 +3,8 @@
 namespace OpenConext\EngineBlock\Validator;
 
 use Mockery as m;
-use OpenConext\EngineBlock\Authentication\Model\User;
-use OpenConext\EngineBlock\Authentication\Repository\ConsentRepository;
-use OpenConext\EngineBlock\Authentication\Repository\UserDirectory;
-use OpenConext\EngineBlock\Authentication\Value\CollabPersonId;
-use OpenConext\EngineBlock\Authentication\Value\CollabPersonUuid;
-use OpenConext\EngineBlockBundle\Authentication\Entity\SamlPersistentId;
-use OpenConext\EngineBlockBundle\Authentication\Entity\ServiceProviderUuid;
-use OpenConext\EngineBlockBundle\Authentication\Repository\SamlPersistentIdRepository;
-use OpenConext\EngineBlockBundle\Authentication\Repository\ServiceProviderUuidRepository;
+use OpenConext\EngineBlock\Exception\InvalidBindingException;
+use OpenConext\EngineBlock\Exception\InvalidRequestMethodException;
 use PHPUnit_Framework_TestCase as TestCase;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -59,12 +52,11 @@ class SsoRequestValidatorTest extends TestCase
         $this->assertTrue($this->validator->isValid($request));
     }
 
-    /**
-     * @expectedException \OpenConext\EngineBlock\Exception\InvalidRequestMethodException
-     * @expectedExceptionMessage The HTTP request method "PATCH" is not supported on this SAML SSO endpoint
-     */
     public function test_post_binding_is_not_supported()
     {
+        $this->expectException(InvalidRequestMethodException::class);
+        $this->expectExceptionMessage('The HTTP request method "PATCH" is not supported on this SAML SSO endpoint');
+
         $request = m::mock(Request::class);
         $request
             ->shouldReceive('getMethod')
@@ -73,12 +65,11 @@ class SsoRequestValidatorTest extends TestCase
         $this->validator->isValid($request);
     }
 
-    /**
-     * @expectedException \OpenConext\EngineBlock\Exception\InvalidBindingException
-     * @expectedExceptionMessage No SAMLRequest parameter was found in the HTTP "GET" request parameters
-     */
     public function test_used_invalid_binding()
     {
+        $this->expectException(InvalidBindingException::class);
+        $this->expectExceptionMessage('No SAMLRequest parameter was found in the HTTP "GET" request parameters');
+
         // Under the hood, the Binding::getCurrentBinding method is used, which directly reads from the super globals
         $_SERVER['REQUEST_METHOD'] = 'GET';
         // Even though we are handling a AuthNRequest, the binding is based on a get parameter named SAMLRequest
@@ -92,12 +83,11 @@ class SsoRequestValidatorTest extends TestCase
         $this->validator->isValid($request);
     }
 
-    /**
-     * @expectedException \OpenConext\EngineBlock\Exception\InvalidBindingException
-     * @expectedExceptionMessage The binding type "SAML2\HTTPArtifact" is not supported on this SAML SSO endpoint
-     */
     public function test_used_unsupported_binding()
     {
+        $this->expectException(InvalidBindingException::class);
+        $this->expectExceptionMessage('The binding type "SAML2\HTTPArtifact" is not supported on this SAML SSO endpoint');
+
         // Under the hood, the Binding::getCurrentBinding method is used, which directly reads from the super globals
         $_SERVER['REQUEST_METHOD'] = 'GET';
         // EB does not support artifact binding
