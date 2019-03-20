1af2cd735a9fc8d8fc6349b4e2b270877597174c
Michiel Kodde <mkodde@ibuildings.nl>
2019-03-20 16:22:07 +0100

    Create the AuthenticationStateHelper
    This helper allows us to read the authentication_state session from
    within the legacy Engineblock code.

---
+++ src/OpenConext/EngineBlock/Service/AuthenticationStateHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright 2019 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenConext\EngineBlock\Service;
+
+use OpenConext\EngineBlockBundle\Authentication\AuthenticationState;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class AuthenticationStateHelper implements AuthenticationStateHelperInterface
+{
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @return AuthenticationState
+     */
+    public function getAuthenticationState()
+    {
+        return $this->session->get('authentication_state');
+    }
+}
