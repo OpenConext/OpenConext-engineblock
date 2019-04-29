0df470c384881dda8991b5abbb4081a333d67e92
Michiel Kodde <mkodde@ibuildings.nl>
2019-04-29 09:34:16 +0200

    Make error pages more realistic
    Based on the way the actual pages are rendered, some additional feedback
    info params have been added to several of the preconfigured error page
    urls.

--- theme/material/javascripts/tests/visual-regression/error-page/ErrorPage.test.js
+++ theme/material/javascripts/tests/visual-regression/error-page/ErrorPage.test.js
@@ -7,7 +7,7 @@ expect.extend({toMatchImageSnapshot});
 const errorPages = [
     {
         name: 'unable-to-receive-message',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=unable-to-receive-message'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=unable-to-receive-message&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98", "serviceProvider":"https://current-sp.entity-id.org/metadata","serviceProviderName":"OpenConext Drop Supplies SP"}'
     },
     {
         name: 'session-lost',
@@ -19,23 +19,23 @@ const errorPages = [
     },
     {
         name: 'no-idps',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=no-idps'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=no-idps&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata", "serviceProviderName":"OpenConext Drop Supplies SP"}'
     },
     {
         name: 'invalid-acs-location',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=invalid-acs-location'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=invalid-acs-location&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata", "serviceProviderName":"OpenConext Drop Supplies SP","identityProvider":"https://current-idp.entity-id.org/metadata"}'
     },
     {
         name: 'unsupported-acs-location-scheme',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=unsupported-acs-location-scheme'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=unsupported-acs-location-scheme&feedback-info={"statusCode": "418", "statusMessage": "(No message provided)", "requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata", "serviceProviderName": "OpenConext Drop Supplies SP","identityProvider":"https://current-idp.entity-id.org/metadata"}'
     },
     {
         name: 'missing-required-fields',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=missing-required-fields'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=missing-required-fields&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98", "serviceProvider":"https://current-sp.entity-id.org/metadata", "serviceProviderName": "OpenConext Drop Supplies SP","identityProvider":"https://current-idp.entity-id.org/metadata"}'
     },
     {
         name: 'invalid-acs-binding',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=invalid-acs-binding'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=invalid-acs-binding&feedback-info={"serviceProvider":"https://current-sp.entity-id.org/metadata", "serviceProviderName": "OpenConext Drop Supplies SP","identityProvider":"https://current-idp.entity-id.org/metadata"}'
     },
     {
         name: 'received-error-status-code',
@@ -43,15 +43,15 @@ const errorPages = [
     },
     {
         name: 'received-invalid-signed-response',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=received-invalid-signed-response'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=received-invalid-signed-response&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata", "serviceProviderName": "OpenConext Drop Supplies SP","identityProvider":"https://current-idp.entity-id.org/metadata"}'
     },
     {
         name: 'received-invalid-response',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=received-invalid-response'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=received-invalid-response&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata", "serviceProviderName": "OpenConext Drop Supplies SP","identityProvider":"https://current-idp.entity-id.org/metadata"}'
     },
     {
         name: 'no-consent',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=no-consent'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=no-consent&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata", "serviceProviderName": "OpenConext Drop Supplies SP","identityProvider":"https://current-idp.entity-id.org/metadata"}'
     },
     {
         name: 'unknown-service',
@@ -59,15 +59,15 @@ const errorPages = [
     },
     {
         name: 'unknown-preselected-idp',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=unknown-preselected-idp'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=unknown-preselected-idp&feedback-info={"Idp Hash": "64531cc179d0d2e66243c30e58125f0a", "requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata", "serviceProviderName": "OpenConext Drop Supplies SP"}'
     },
     {
         name: 'stuck-in-authentication-loop',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=stuck-in-authentication-loop'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=stuck-in-authentication-loop&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata", "serviceProviderName": "OpenConext Drop Supplies SP","identityProvider":"https://current-idp.entity-id.org/metadata"}'
     },
     {
         name: 'clock-issue',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=clock-issue'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=clock-issue&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata", "serviceProviderName": "OpenConext Drop Supplies SP","identityProvider":"https://current-idp.entity-id.org/metadata"}'
     },
     {
         name: 'unknown-issuer',
@@ -75,15 +75,15 @@ const errorPages = [
     },
     {
         name: 'unsupported-signature-method',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=unsupported-signature-method&parameters={"signatureMethod":"http://www.w3.org/2000/09/xmldsig%23%0Arsa-sha1"}'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=unsupported-signature-method&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata","serviceProviderName":"OpenConext Drop Supplies SP"}&parameters={"signatureMethod":"http://www.w3.org/2000/09/xmldsig%23%0Arsa-sha1"}'
     },
     {
         name: 'unknown-service-provider',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=unknown-service-provider&parameters={"entityId":"https://serviceregistry.vm.openconext.org/simplesaml/module.php/saml/sp/metadata.php/default-sp"}'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=unknown-service-provider&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata","serviceProviderName":"OpenConext Drop Supplies SP"}&parameters={"entityId":"https://serviceregistry.vm.openconext.org/simplesaml/module.php/saml/sp/metadata.php/default-sp"}'
     },
     {
         name: 'invalid-attribute-value',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=invalid-attribute-value&parameters={"attributeName":"schacHomeOrganization","attributeValue":"openconext"}'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=invalid-attribute-value&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata","serviceProviderName":"OpenConext Drop Supplies SP","identityProvider":"https://current-idp.entity-id.org/metadata"}&parameters={"attributeName":"schacHomeOrganization","attributeValue":"openconext"}'
     },
     {
         name: 'no-authentication-request-received',
@@ -91,7 +91,7 @@ const errorPages = [
     },
     {
         name: 'authorization-policy-violation',
-        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=authorization-policy-violation&parameters={"logo":{"height":"96","width":"96","url":"https://static.vm.openconext.org/media/conext_logo.png"},"policyDecisionMessage":"No localized deny messages present"}'
+        url: 'https://engine.vm.openconext.org/functional-testing/feedback?template=authorization-policy-violation&feedback-info={"requestId":"5cb4bd3879b49","artCode":"31914", "ipAddress":"192.168.66.98","serviceProvider":"https://current-sp.entity-id.org/metadata","serviceProviderName":"OpenConext Drop Supplies SP","identityProvider":"https://current-idp.entity-id.org/metadata"}&parameters={"logo":{"height":"96","width":"96","url":"https://static.vm.openconext.org/media/conext_logo.png"},"policyDecisionMessage":"No localized deny messages present"}'
     },
 ];
 
