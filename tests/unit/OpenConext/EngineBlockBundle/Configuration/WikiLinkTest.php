130d23f4505678760950642009c8839a6c564765
Michiel Kodde <mkodde@ibuildings.nl>
2019-08-14 11:59:29 +0200

    Catch the ErrorFeedbackConfig in value objects
    Will be used to correctly represent the wiki link in the twig templates.
    
    The objects are shipped with unit tests.

---
+++ tests/unit/OpenConext/EngineBlockBundle/Configuration/WikiLinkTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Copyright 2019 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+namespace OpenConext\EngineBlockBundle\Configuration;
+
+use OpenConext\EngineBlock\Exception\InvalidArgumentException;
+use PHPUnit_Framework_TestCase as UnitTest;
+
+class WikiLinkTest extends UnitTest
+{
+    /**
+     * @test
+     * @group EngineBlockBundle
+     * @group Configuration
+     * @dataProvider \OpenConext\TestDataProvider::notArray()
+     * @param mixed $notArray
+     */
+    public function wiki_links_is_required_to_be_an_array($notArray)
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new WikiLink($notArray, 'https://fallback.uri');
+    }
+
+    /**
+     * @test
+     * @group EngineBlockBundle
+     * @group Configuration
+     * @dataProvider \OpenConext\TestDataProvider::notString()
+     * @param mixed $notString
+     */
+    public function fallback_must_be_a_non_empty_string($notString)
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new WikiLink(['en' => 'https://wiki.co.uk/page1'], $notString);
+    }
+
+    /**
+     * @test
+     * @group EngineBlockBundle
+     * @group Configuration
+     */
+    public function the_provided_links_can_be_retrieved_by_language()
+    {
+        $link = new WikiLink(
+            [
+                'en' => 'https://wiki.co.uk/page1',
+                'nl' => 'https://wiki.nl/page1',
+                'pt' => 'https://wiki.pt/page1',
+            ],
+            'fallback'
+        );
+
+        $this->assertEquals('https://wiki.co.uk/page1', $link->getLink('en'));
+        $this->assertEquals('https://wiki.nl/page1', $link->getLink('nl'));
+        $this->assertEquals('https://wiki.pt/page1', $link->getLink('pt'));
+    }
+
+    /**
+     * @test
+     * @group EngineBlockBundle
+     * @group Configuration
+     */
+    public function when_link_not_found_a_fallback_is_returned()
+    {
+        $link = new WikiLink(
+            [
+                'en' => 'https://wiki.co.uk/page1',
+            ],
+            'https://wiki.co.uk'
+        );
+        $this->assertEquals('https://wiki.co.uk', $link->getLink('es'));
+
+    }
+}
