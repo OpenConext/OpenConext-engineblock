c53c05f3d353dcdf7596a591f20dc1736fdcc055
Bas Strooband <bstrooband@ibuildings.nl>
2019-08-16 13:35:06 +0200

    Fix all missing copyright notices at once
    On various places the copyright notices were missing. Because this
    takes sometime a considerable amount in the review process a automated
    task could identify this without the extra review hassle.
    
    All copyright notices use a fixed year now.

--- library/EngineBlock/Saml2/NameIdResolver.php
+++ library/EngineBlock/Saml2/NameIdResolver.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Copyright 2014 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 use OpenConext\EngineBlock\Metadata\Entity\ServiceProvider;
 use Ramsey\Uuid\Uuid;
 use SAML2\AuthnRequest;
@@ -37,7 +53,7 @@ class EngineBlock_Saml2_NameIdResolver
      * @var string[]
      */
     private $cache = array();
-    
+
     /**
      * @param EngineBlock_Saml2_AuthnRequestAnnotationDecorator $request
      * @param EngineBlock_Saml2_ResponseAnnotationDecorator $response
@@ -213,7 +229,7 @@ class EngineBlock_Saml2_NameIdResolver
 
         // Store the persistent name id in cache for the duration of the request
         $this->cache[$cacheIdentifier] = $persistentId;
-        
+
         return $persistentId;
     }
 
