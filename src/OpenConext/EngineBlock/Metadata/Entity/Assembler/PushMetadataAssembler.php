b7fd84f600d5e7a189998b0e0e98fbee534d0772
Michiel Kodde <mkodde@ibuildings.nl>
2019-09-30 16:19:51 +0200

    Mark edugain fields as deprecated
    In order to easily roll back to version 6.0 of EB, the publish in
    edugain coin columns are still tracked in version 6.1. But they
    are marked as deprecated. And the intention to remove them in the next
    release is clearly stated.

--- src/OpenConext/EngineBlock/Metadata/Entity/Assembler/PushMetadataAssembler.php
+++ src/OpenConext/EngineBlock/Metadata/Entity/Assembler/PushMetadataAssembler.php
@@ -291,6 +291,12 @@ class PushMetadataAssembler implements MetadataAssemblerInterface
         $properties += $this->assembleOrganization($connection, 'en');
         $properties += $this->setPathFromObjectString(array($connection, 'metadata:keywords:en'), 'keywordsEn');
         $properties += $this->setPathFromObjectString(array($connection, 'metadata:keywords:nl'), 'keywordsNl');
+
+        /**
+         * @deprecated: This coin is no longer used in EngineBlock and will be removed in release 6.2
+         */
+        $properties += $this->setPathFromObjectString(array($connection, 'metadata:coin:publish_in_edugain'), 'publishInEdugain');
+
         $properties += $this->assembleCertificates($connection);
         $properties += $this->setPathFromObjectString(array($connection, 'state'), 'workflowState');
         $properties += $this->assembleContactPersons($connection);
@@ -298,6 +304,12 @@ class PushMetadataAssembler implements MetadataAssemblerInterface
         $properties += $this->setPathFromObjectArray(array($connection, 'metadata:NameIDFormats'), 'supportedNameIdFormats');
         $properties += $this->assembleSingleLogoutServices($connection);
         $properties += $this->setPathFromObjectBool(array($connection, 'metadata:coin:disable_scoping'), 'disableScoping');
+
+        /**
+         * @deprecated: This coin is no longer used in EngineBlock and will be removed in release 6.2
+         */
+        $properties += $this->assemblePublishInEdugainDate($connection);
+
         $properties += $this->setPathFromObjectBool(array($connection, 'metadata:coin:additional_logging'), 'additionalLogging');
         $properties += $this->setPathFromObjectString(array($connection, 'metadata:coin:signature_method'), 'signatureMethod');
         $properties += $this->setPathFromObjectBool(array($connection, 'metadata:redirect:sign'), 'requestsMustBeSigned');
@@ -343,6 +355,18 @@ class PushMetadataAssembler implements MetadataAssemblerInterface
             $connection->metadata->OrganizationDisplayName->$langCode,
             $connection->metadata->OrganizationURL->$langCode
         ));
+    }
+
+    /**
+     * @deprecated: The coins assembled in this method are longer used in EngineBlock and will be removed in release 6.2
+     */
+    private function assemblePublishInEdugainDate(stdClass $connection)
+    {
+        if (empty($connection->coin->publish_in_edugain)) {
+            return array();
+        }
+
+        return new DateTime($connection->coin->publish_in_edugain);
     }
 
     private function assembleCertificates(stdClass $connection)
