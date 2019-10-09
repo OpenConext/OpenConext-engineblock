118edbdc324c1efc78ac99d6f64a13cebeb49818
Michiel Kodde <mkodde@ibuildings.nl>
2019-10-09 15:35:48 +0200

    Offload EB SP entity creation to factory

--- src/OpenConext/EngineBlock/Metadata/Factory/Factory/ServiceProviderFactory.php
+++ src/OpenConext/EngineBlock/Metadata/Factory/Factory/ServiceProviderFactory.php
@@ -18,11 +18,15 @@
 namespace OpenConext\EngineBlock\Metadata\Factory\Factory;
 
 use EngineBlock_Attributes_Metadata as AttributesMetadata;
+use OpenConext\EngineBlock\Metadata\ContactPerson;
 use OpenConext\EngineBlock\Metadata\Factory\Adapter\ServiceProviderEntity;
+use OpenConext\EngineBlock\Metadata\Factory\Decorator\EngineBlockServiceProviderMetadata;
 use OpenConext\EngineBlock\Metadata\Factory\Decorator\ServiceProviderProxy;
 use OpenConext\EngineBlock\Metadata\Factory\ServiceProviderEntityInterface;
 use OpenConext\EngineBlock\Metadata\Entity\ServiceProvider;
 use OpenConext\EngineBlock\Metadata\IndexedService;
+use OpenConext\EngineBlock\Metadata\Logo;
+use OpenConext\EngineBlock\Metadata\Organization;
 use OpenConext\EngineBlock\Metadata\Service;
 use OpenConext\EngineBlock\Metadata\X509\KeyPairFactory;
 use OpenConext\EngineBlock\Metadata\X509\X509KeyPair;
@@ -54,6 +58,46 @@ class ServiceProviderFactory
         return new ServiceProviderEntity($entity);
     }
 
+    public function createEngineBlockEntityFrom(
+        string $entityId,
+        string $acsLocation,
+        string $keyId
+    ): ServiceProviderEntityInterface {
+        $entity = $this->buildServiceProviderEntity($entityId,$acsLocation, $keyId);
+        $entity->nameEn = 'NAME EN';
+        $entity->nameNl = 'NAME NL';
+        $entity->descriptionEn = 'DESCRIPTION EN';
+        $entity->descriptionNl = 'DESCRIPTION NL';
+        $entity->organizationEn = new Organization('ORG EN', 'ORG EN', 'SUPPORT URL EN');
+
+        $administrative = new ContactPerson('administrative');
+        $administrative->givenName = 'To';
+        $administrative->surName = 'Do';
+        $administrative->emailAddress = 'todo@todo.com';
+
+        $technical = new ContactPerson('technical');
+        $technical->emailAddress = 'todo@todo.com';
+        $technical->givenName = 'To';
+        $technical->surName = 'Do';
+
+        $support = new ContactPerson('support');
+        $support->emailAddress = 'todo@todo.com';
+        $support->givenName = 'To';
+        $support->surName = 'Do';
+
+        $entity->contactPersons = [
+            $administrative,
+            $technical,
+            $support
+        ];
+
+        $entity->logo = new Logo('https://example.logo.com/logo.gif');
+        $entity->logo->height = 200;
+        $entity->logo->width = 200;
+
+        return new EngineBlockServiceProviderMetadata($this->createEntityFromEntity($entity));
+    }
+
     public function createProxyFromEntity(
         ServiceProvider $entity,
         X509KeyPair $proxyKeyPair,
@@ -68,6 +112,16 @@ class ServiceProviderFactory
         string $keyId,
         string $acsBindingMethod = Constants::BINDING_HTTP_POST
     ): ServiceProviderEntityInterface {
+        $entity = $this->buildServiceProviderEntity($entityId, $acsLocation, $keyId, $acsBindingMethod);
+        return $this->createEntityFromEntity($entity);
+    }
+
+    private function buildServiceProviderEntity(
+        string $entityId,
+        string $acsLocation,
+        string $keyId,
+        string $acsBindingMethod = Constants::BINDING_HTTP_POST
+    ) : ServiceProvider {
         $entity = new ServiceProvider($entityId);
         $entity->certificates[] = $this->keyPairFactory->buildFromIdentifier($keyId)->getCertificate();
         $entity->assertionConsumerServices[] = new IndexedService(
@@ -76,6 +130,6 @@ class ServiceProviderFactory
             0
         );
 
-        return $this->createEntityFromEntity($entity);
+        return $entity;
     }
 }
