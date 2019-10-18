253fb1270bcea7a54e6859157486caabe6b28e4e
Bas Strooband <bstrooband@ibuildings.nl>
2019-10-18 17:29:23 +0200

    Move stepup files to appropriate namespace
    The Stepup entities needed to be moved to a different namespace.

--- src/OpenConext/EngineBlock/Stepup/StepupDecision.php
+++ src/OpenConext/EngineBlock/Stepup/StepupDecision.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-namespace OpenConext\EngineBlockBundle\Stepup;
+namespace OpenConext\EngineBlock\Stepup;
 
 use OpenConext\EngineBlock\Exception\InvalidStepupConfigurationException;
 use OpenConext\EngineBlock\Metadata\Entity\IdentityProvider;
