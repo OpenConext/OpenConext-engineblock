efeeec8f2dc9f0849ec6d2053c111b901fe2f885
Bas Strooband <bstrooband@ibuildings.nl>
2020-03-31 10:28:54 +0200

    Fix visual snapshot testing

--- theme/material/javascripts/tests/visual-regression/consent/ConsentPage.test.js
+++ theme/material/javascripts/tests/visual-regression/consent/ConsentPage.test.js
@@ -1,14 +1,14 @@
-const timeout = 15000;
+const timeout = 5000;
 const {toMatchImageSnapshot} = require('jest-image-snapshot');
 
 // Extend Jest expect
 expect.extend({toMatchImageSnapshot});
 
-const consentPages = [
-    {
-        name: 'consent',
-        url: 'https://engine.vm.openconext.org/functional-testing/consent"}'
-    },
+const pageTests = [
+    [
+        'consent',
+        'https://engine.vm.openconext.org/functional-testing/consent',
+    ],
 ];
 
 const viewports = [
@@ -16,41 +16,22 @@ const viewports = [
     {width: 1920, height: 1080},
 ];
 
-describe(
-    'Verify',
-    () => {
-        let page;
+describe.each(pageTests)('Verify (%s)', (name, url) => {
+    for (const viewport of viewports) {
+        it(`${name}-${viewport.width}x${viewport.height}`, async () => {
+            let page = await global.__BROWSER__.newPage();
+            await page.setViewport(viewport);
+            await page.goto(url, 2000);
+            await page.waitFor(".mod-content", 2000);
 
-        beforeAll(async () => {
-            page = await global.__BROWSER__.newPage();
-            jest.setTimeout(20000);
-        }, timeout);
-
-        let sets = [];
-        for (const consentPage of consentPages) {
-            for (const viewport of viewports) {
-                sets.push({
-                    name: consentPage.name,
-                    url: consentPage.url,
-                    viewport: viewport,
-                    expect: expect,
-                    page: page,
-                });
-            }
-        }
-
-        for (const s of sets) {
-            it(`${s.name}-${s.viewport.width}x${s.viewport.height}`, async () => {
-                await s.page.goto(s.url);
-                await s.page.setViewport(s.viewport);
-                await s.page.waitFor("tr.toggle-attributes a");
-                const screenshot = await s.page.screenshot({
-                    fullPage: true,
-                    path: `./material/javascripts/tests/visual-regression/consent/screenshots/consent/${s.name}-${s.viewport.width}x${s.viewport.height}.png`
-                });
-                s.expect(screenshot).toMatchImageSnapshot();
+            const screenshot = await page.screenshot({
+                fullPage: true,
+                path: `./material/javascripts/tests/visual-regression/consent/screenshots/consent/${name}-${viewport.width}x${viewport.height}.png`
             });
-        }
-    },
-    timeout,
-);
+
+            await page.close();
+
+            expect(screenshot).toMatchImageSnapshot();
+        }, timeout);
+    }
+});
