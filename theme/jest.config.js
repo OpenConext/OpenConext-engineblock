f4b366476d4b1131db5e564bc191f08519f30855
Michiel Kodde <mkodde@ibuildings.nl>
2020-08-26 11:23:16 +0200

    Remove Jest and Puppeteer

--- theme/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    globalSetup: './puppeteer/setup.js',
-    globalTeardown: './puppeteer/teardown.js',
-    testEnvironment: './puppeteer/puppeteer_environment.js',
-    collectCoverage: false,
-};
