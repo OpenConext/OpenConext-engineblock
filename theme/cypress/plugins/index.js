01d3d7269c7302d38a01e63c5edc557a85211073
Koen Cornelis <kcornelis@ibuildings.nl>
2020-09-20 14:03:51 +0200

    add cypress a11y and html validation tests

--- theme/cypress/plugins/index.js
+++ theme/cypress/plugins/index.js
@@ -1,6 +1,8 @@
 const { initPlugin } = require('cypress-plugin-snapshots/plugin');
+const htmlvalidate = require('cypress-html-validate/dist/plugin');
 
 module.exports = (on, config) => {
+    htmlvalidate.install(on);
     initPlugin(on, config);
     return config;
 };
