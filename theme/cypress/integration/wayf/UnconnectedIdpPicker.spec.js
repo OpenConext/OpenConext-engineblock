a2fe57a2c876bd21d89e5c4d2041d199717a985d
Michiel Kodde <mkodde@ibuildings.nl>
2020-09-22 13:06:38 +0200

    Move the material theme to the base theme folder
    The base theme will be the simplest version of the base theme, for now
    based on the current material theme.

--- theme/cypress/integration/wayf/UnconnectedIdpPicker.spec.js
+++ theme/cypress/integration/wayf/UnconnectedIdpPicker.spec.js
@@ -1,4 +1,4 @@
-import {UnconnectedIdpPicker} from "../../../material/javascripts/modules/UnconnectedIdpPicker";
+import {UnconnectedIdpPicker} from "../../../base/javascripts/modules/UnconnectedIdpPicker";
 
 it('Check if the UnconnectedIdpPicker constructor was called', () => {
     const unconnectedIdpPicker = new UnconnectedIdpPicker(null, null, null);
