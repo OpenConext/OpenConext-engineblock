9f89a705aed4bb77a3d46f96e1e854c1589ff367
Koen Cornelis <kcornelis@ibuildings.nl>
2020-09-22 15:35:57 +0200

    Feature/consent new design (#895)
    Introduce new theme with styling for the consent page
    
    A new theme for the wayf/consent page was discussed here: https://wiki.surfnet.nl/pages/viewpage.action?spaceKey=coininfra&title=Vormgeving+WAYF+en+Consent+conform+styleboard.  As the first step in the new theme this commit adds a new theme (skeune) and all js/html + 95% of the css needed to make the new consent page a reality.
    This will serve as the basis for a complete overhaul of how the theming works for engineblock, as discussed in https://www.pivotaltracker.com/story/show/174902390

--- theme/cypress/integration/consent/consent.material.spec.js
+++ theme/cypress/integration/consent/consent.material.spec.js
@@ -2,51 +2,51 @@
 
 context('Consent', () => {
   beforeEach(() => {
-    cy.visit('https://engine.vm.openconext.org/functional-testing/consent')
-  })
+    cy.visit('https://engine.vm.openconext.org/functional-testing/consent');
+  });
 
   it('gives openconext information', () => {
     cy.get('a.help[data-slidein="about"]')
       .click()
       .get('section h1')
       .should('be.visible')
-      .and('contain.text', 'Logging in through OpenConext')
+      .and('contain.text', 'Logging in through OpenConext');
 
     cy.get('div.about a.close')
-      .click()
-  })
+      .click();
+  });
 
   it('shows information on how to report incorrect data', () => {
     cy.get('a.small')
       .click()
       .get('section h1')
       .should('be.visible')
-      .and('contain.text', 'Is the data shown incorrect?')
+      .and('contain.text', 'Is the data shown incorrect?');
 
     cy.get('div.correction-idp a.close')
-      .click()
-  })
+      .click();
+  });
 
   it('can show additional attributes', () => {
     cy.get('span.show-more')
       .click()
       .get('td[data-identifier="urn:mace:dir:attribute-def:isMemberOf"]')
       .should('be.visible')
-      .and('contain.text', 'Member of organization')
-  })
+      .and('contain.text', 'Member of organization');
+  });
 
   it('can decline consent', () => {
     cy.get('div.slidein.reject')
-      .should('be.hidden')
+      .should('be.hidden');
 
     cy.get('a#decline-terms')
       .click()
       .get('section h1')
       .should('be.visible')
-      .and('contain.text', 'You don\'t want to share your data with the service')
+      .and('contain.text', 'You don\'t want to share your data with the service');
 
     cy.get('div.slidein.reject')
-      .should('be.visible')
-  })
+      .should('be.visible');
+  });
 
-})
+});
--- theme/cypress/plugins/index.js
+++ theme/cypress/plugins/index.js
@@ -1,6 +1,18 @@
 const { initPlugin } = require('cypress-plugin-snapshots/plugin');
+const htmlvalidate = require('cypress-html-validate/dist/plugin');
 
 module.exports = (on, config) => {
+    htmlvalidate.install(on, {
+        "rules": {
+            "prefer-button": "off",
+            "prefer-native-element": [ "error", {
+                "exclude": [ "button" ],
+            }],
+            "require-sri": [ "error", {
+                "target": "crossorigin",
+            }],
+        },
+    });
     initPlugin(on, config);
     return config;
 };
