cd51202c8379dc773ea3a71b0c095df4d7b51188
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-02 21:09:42 +0200

    Show correct IdpList on load

--- theme/base/javascripts/wayf.js
+++ theme/base/javascripts/wayf.js
@@ -1,15 +1,43 @@
 // Temporary code to get wayf to function. Only submit by clicking on a IdP functions for now
+import {handleUnconnectedKeypicker, returnNewIdpPicker} from './modules/IdpPicker';
+import {RequestAccessModalHelper} from './modules/RequestAccessModalHelper';
+import {KeyboardListener} from './modules/KeyboardListener';
+import {toggleVisibility} from './utility/toggleVisibility';
+import {searchBarEventListeners} from './wayf/searchBarEventListeners';
+import {interactionListeners} from './wayf/interactionListeners';
+import {MouseListener} from './modules/MouseListener';
+import {searchFormEventListeners} from './wayf/searchFormEventListeners';
+import {focusSearchBar} from './wayf/focusSearchBar';
+import {initializePage} from './page';
+import {hideNoScript} from '../../openconext/javascripts/hideNoScript';
+import {showIdpsOnLoad} from './wayf/showIdpsOnLoad';
+
 export function initializeWayf() {
-  const form = document.querySelector('.wayf__search');
-  if (form !== null) {
-    // Set a quick and dirty click listener on every IdP
-    document.querySelectorAll('.wayf__idp').forEach((article) => {
-      article.addEventListener('click', (event) => {
-        event.preventDefault();
-        // And set the EnittyId associated with that IdP on the hidden form field. And submit the form
-        form.elements['form-idp'].value = event.target.closest('article').dataset.entityId;
-        form.submit();
-      });
-    });
-  }
+  // const form = document.querySelector('.wayf__search');
+  // if (form !== null) {
+  //   // Set a quick and dirty click listener on every IdP
+  //   document.querySelectorAll('.wayf__idp').forEach((article) => {
+  //     article.addEventListener('click', (event) => {
+  //       event.preventDefault();
+  //       // And set the EnittyId associated with that IdP on the hidden form field. And submit the form
+  //       form.elements['form-idp'].value = event.target.closest('article').dataset.entityId;
+  //       form.submit();
+  //     });
+  //   });
+  // }
+
+  const callbacksAfterLoad = () => {
+    // Initialize variables
+    const selectedIdps = document.querySelector('.wayf__previousSelection');
+    const remainingIdps = document.querySelector('.wayf__remainingIdps');
+    const noResults = document.querySelector('.wayf__noResults');
+    const noAccess = document.querySelector('.wayf__noAccess');
+    const searchBar = document.querySelector('#wayf_search');
+    const searchForm = document.querySelector('.wayf__search');
+    const configuration = JSON.parse(document.getElementById('wayf-configuration').innerHTML);
+
+    showIdpsOnLoad(selectedIdps, remainingIdps);
+  };
+
+  initializePage('main.wayf', callbacksAfterLoad);
 }
