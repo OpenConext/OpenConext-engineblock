8f2258c33ff3826be2af02265e665556eb2bc8f4
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-05 16:14:46 +0200

    Refactor JS code for consent page (#916)
    * Add a class to the ok button for consent
    
    So we can use it as a hook for the js
    
    * Refactor the js for the consent page
    
    Made a generic initializePage function.
    Split up all js functions in seperate files for the consent page, put those in a consent subfolder.
    Created a initializeConsent function for both openconext / base (skeune) which contains all relevant JS with updated selectors.
    Removed the inline JS from default.html.twig.
    Imported the JS for consent in application.js for both openconext / base (skeune).

--- theme/openconext/javascripts/application.js
+++ theme/openconext/javascripts/application.js
@@ -1,6 +1,6 @@
 import {initializeIndex} from "../../base/javascripts";
-import {initializeConsent} from "../../base/javascripts/consent";
 import {initializeWayf} from "../../base/javascripts/wayf";
+import {initializeConsent} from './consent';
 
 initializeIndex();
 initializeWayf();
