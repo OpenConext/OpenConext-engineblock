7e87842e4979ecd2ec9e41e159e9bd4f918be3bb
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-06 16:15:30 +0200

    Add going up and down by arrow in idp-list

--- theme/base/javascripts/wayf/keyboardBehaviour.js
+++ theme/base/javascripts/wayf/keyboardBehaviour.js
@@ -2,10 +2,21 @@ import {showRemaining} from './showRemaining';
 import {fireClickEvent} from '../utility/fireClickEvent';
 import {handleDeleteDisable} from './handleDeleteDisable';
 import {submitForm} from './submitForm';
+import {focusOnNextIdp} from './idpFocus/focusOnNextIdp';
+import {focusOnPreviousIdp} from './idpFocus/focusOnPreviousIdp';
+import {isFocusOn} from '../utility/isFocusOn';
 
 export const keyboardBehaviour = () => {
+  const ENTER      = 13;
+  const ARROW_UP   = 38;
+  const ARROW_DOWN = 40;
+  const searchBar = document.querySelector('.search__field');
+  const eduId = document.querySelector('.wayf__eduIdLink');
+  const firstIdp = document.querySelector('.wayf__remainingIdps li:first-of-type > .wayf__idp');
+  const lastIdp = document.querySelector('.wayf__remainingIdps li:last-of-type > .wayf__idp');
+
   document.querySelector('body').addEventListener('keydown', function(e) {
-    if (e.keyCode === 13) {
+    if (e.keyCode === ENTER) {
       var target = e.target;
       switch (target.className) {
         // Show remaining idp section when hitting the add account button
@@ -17,6 +28,7 @@ export const keyboardBehaviour = () => {
         // handle pressing the disable/delete button
         case 'idp__deleteDisable':
           handleDeleteDisable(e); break;
+        // handle selecting an idp
         case 'wayf__idp':
         case 'idp__content':
         case 'idp__title':
@@ -28,6 +40,37 @@ export const keyboardBehaviour = () => {
           console.log(e);
           console.log(target);
       }
+
+      return;
+    }
+
+    if (e.keyCode === ARROW_DOWN) {
+      if (isFocusOn(searchBar)) {
+        eduId.focus();
+        return;
+      } else if (isFocusOn(eduId)) {
+        firstIdp.focus();
+        return;
+      } else if (isFocusOn(lastIdp)) {
+        searchBar.focus();
+      }
+
+      focusOnNextIdp();
+      return;
+    }
+
+    if (e.keyCode === ARROW_UP) {
+      if (isFocusOn(searchBar)) {
+        lastIdp.focus();
+      } else if (isFocusOn(firstIdp)) {
+        eduId.focus();
+        return;
+      } else if (isFocusOn(eduId)) {
+        searchBar.focus();
+      }
+
+      focusOnPreviousIdp();
+      return;
     }
   });
 };
