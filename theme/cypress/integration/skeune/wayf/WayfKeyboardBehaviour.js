a1b649cc4bb9d5f90f38fe229e9e6d7981be62cb
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-07 11:55:26 +0200

    Test arrow navigation on remaining idplist

--- theme/cypress/integration/skeune/wayf/WayfKeyboardBehaviour.js
+++ theme/cypress/integration/skeune/wayf/WayfKeyboardBehaviour.js
@@ -17,14 +17,36 @@ context('WAYF when using the keyboard', () => {
     cy.visit('https://engine.vm.openconext.org/functional-testing/wayf');
   });
 
+  // todo after adding eduId feature flag this should be adjusted to take that into account
+  // todo if html spec is changed, or cypress fixes bug 6207, get rid of the manual focus on search.  See https://github.com/cypress-io/cypress/issues/6207
+  it('Should be able to traverse the remaining idp section with arrow keys', () => {
+    // check if pressing down works as expected
+    cy.get('.search__field').focus();
+    cy.pressArrowOnIdpList('down', 'search__field');
+    cy.pressArrowOnIdpList('down', 'wayf__eduIdLink');
+    cy.pressArrowOnIdpList('down', 'wayf__idp', '1');
+    cy.pressArrowOnIdpList('down', 'wayf__idp', '2');
+    cy.pressArrowOnIdpList('down', 'wayf__idp', '3');
+    cy.pressArrowOnIdpList('down', 'wayf__idp', '4');
+    cy.pressArrowOnIdpList('down', 'wayf__idp', '5');
+    cy.pressArrowOnIdpList('down', 'search__field');
+
+    // check if pressing up works as expected
+    cy.visit('https://engine.vm.openconext.org/functional-testing/wayf');
+    cy.get('.search__field').focus();
+    cy.pressArrowOnIdpList('up', 'search__field');
+    cy.pressArrowOnIdpList('up', 'wayf__idp', '5');
+    cy.pressArrowOnIdpList('up', 'wayf__idp', '4');
+    cy.pressArrowOnIdpList('up', 'wayf__idp', '3');
+    cy.pressArrowOnIdpList('up', 'wayf__idp', '2');
+    cy.pressArrowOnIdpList('up', 'wayf__idp', '1');
+    cy.pressArrowOnIdpList('up', 'wayf__eduIdLink');
+    cy.pressArrowOnIdpList('up', 'search__field');
+  });
+
   // todo: test once no access has been implemented
   it.skip('Should show a fully functional no access section when a disabled account is selected', () => {
     cy.visit('https://engine.vm.openconext.org/functional-testing/wayf');
-
-    // Test if the disabled account can be chosen
-    cy.get('.idp__disabled')
-      .eq(1)
-      .type('{enter}');
 
     // Test if the no access section shows up
     cy.contains('.noAccess__title', 'Sorry, no access for this account');
@@ -34,6 +56,11 @@ context('WAYF when using the keyboard', () => {
     cy.get('.noAccess__title')
       .should('not.exist');
 
+    // Test if the disabled account can be chosen
+    cy.get('.idp__disabled')
+      .eq(1)
+      .type('{enter}');
+
     // Test if the request access button works
     cy.focusAndEnter('.cta__request');
     cy.contains('.noAccess__requestForm label', 'Your name');
@@ -41,7 +68,7 @@ context('WAYF when using the keyboard', () => {
 
   // todo: test once previous selection has been implemented
   it.skip('Should show a fully functional previous selection section', () => {
-    cy.visit('https://engine.vm.openconext.org/functional-testing/wayf');
+    cy.visit('https://engine.vm.openconext.org/functional-testing/wayf'); // adjust so we have a previous section
 
     // Test if section exists with the right title
     cy.contains('.previousSelection__title', 'Your accounts');
@@ -49,6 +76,8 @@ context('WAYF when using the keyboard', () => {
     // Test if it contains the right amount of idps
     cy.get('.wayf__previousSelection .wayf__idp h3')
       .should('have.length', 2);
+
+    // todo: add test for autofocus on first Idp, once bug 6207 for cypress is resolved.  See https://github.com/cypress-io/cypress/issues/6207
 
     // Test if the section contains the add account button
     cy.contains('.previousSelection__addAccount', 'Add another account');
