b9f95d9bec1ddcd2646e8d372d988b3f8131b2f9
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-08 11:16:09 +0200

    Add all previous selection functionality (#929)
    * Show correct IdpList on load
    
    * Show correct elements based on nojs
    
    * Add behaviour for add an account button
    
    * Ensure form & styles work
    
    Added tabindexes, focus & hover styles.
    Added a description for the article, as it get's focus.
    
    * Add previous selection functionality
    
    Add selected idp to cookie
    Remove deleted idp from cookie
    Increase count on selected "previous" selected idp
    Remove deleted idp from previous selection
    Move deleted idp to remaining idps
    Reindex idps
    Put focus on the first previously selected idp remaining
    Hide previous selection when there's no more elements & focus on searchbar
    
    * Test the previous selection functionality

--- theme/base/javascripts/wayf/submitForm.js
+++ theme/base/javascripts/wayf/submitForm.js
@@ -1,8 +1,23 @@
-export const submitForm = (e) => {
+import {addSelectedIdp} from './deleteDisable/addSelectedIdp';
+
+/**
+ * Submit the form for the selected idp.
+ * This ensures you select the idp to log in.
+ *
+ * @param e
+ * @param previouslySelectedIdps
+ */
+export const submitForm = (e, previouslySelectedIdps) => {
   let element = e.target;
+
+  if (e.target.className === 'idp__deleteDisable' || e.target.className === 'idp__delete' || e.target.className === 'idp__disabled') {
+    return;
+  }
+
   if (e.target.tagName !== 'ARTICLE') {
     element = e.target.closest('.wayf__idp');
   }
 
+  addSelectedIdp(previouslySelectedIdps, element);
   element.querySelector('.idp__form').submit();
 };
