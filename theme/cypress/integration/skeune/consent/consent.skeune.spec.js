5a532effdeac6cc28bf6333819b1703d524ad777
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-08 11:49:33 +0200

    Refactor cypress tests for legibility (#930)
    * Show correct IdpList on load
    
    * Show correct elements based on nojs
    
    * Refactor the general WAYF tests for legibility
    
    * Refactor the consent tests for legibility

--- theme/cypress/integration/skeune/consent/consent.skeune.spec.js
+++ theme/cypress/integration/skeune/consent/consent.skeune.spec.js
@@ -1,63 +1,81 @@
 context('Consent on Skeune theme', () => {
+  describe('Handles additional attributes correctly', () => {
+    it('shows the correct amount of attributes on load', () => {
+      cy.visit('https://engine.vm.openconext.org/functional-testing/consent');
+      cy.get('ul.consent__attributes > li')
+        .should('have.length', '7');
+    });
 
-  beforeEach(() => {
-    cy.visit('https://engine.vm.openconext.org/functional-testing/consent');
+    it('Should not show the extra attributes on load', () => {
+      cy.get('ul.consent__attributes--nested')
+        .should('be.hidden');
+    });
+
+    it('Should show the more info label', () => {
+      cy.contains('label', 'Show more information')
+        .click();
+    });
+
+    it('Should show the extra attributes after clicking the label', () => {
+      cy.contains('label', 'Show less information');
+      cy.get('ul.consent__attributes--nested')
+        .should('be.visible');
+    });
   });
 
-  it('gives openconext information', () => {
-    cy.get('label[for="consent_disclaimer_about"] + section h3')
-      .should('be.hidden');
+  describe('gives openconext information', () => {
+    it('Should not show the about-modal on load', () => {
+      cy.get('label[for="consent_disclaimer_about"] + section h3')
+        .should('be.hidden');
+    });
 
-    cy.contains('label', 'OpenConext')
-      .click({force: true})
-      .next()
-      .contains('h3', 'Logging in through OpenConext');
+    it('Should show the about-modal after selection', () => {
+      cy.contains('label', 'OpenConext')
+        .click({force: true})
+        .next()
+        .contains('h3', 'Logging in through OpenConext');
+    });
   });
 
-  it('shows information on how to report incorrect data', () => {
-    cy.get('label[for="cta_consent_nok"] + section h3')
-      .should('be.hidden');
+  describe('shows information on how to report incorrect data', () => {
+    it('Should not show the nok-modal on load', () => {
+      cy.get('label[for="cta_consent_nok"] + section h3')
+        .should('be.hidden');
+    });
 
-    cy.contains('label', 'Something incorrect?')
-      .click();
-    cy.get('.idpRow > section.modal__value')
-      .contains('h3', 'Is the data shown incorrect?');
+    it('Should show the nok-modal after selection', () => {
+      cy.contains('label', 'Something incorrect?')
+        .click();
+      cy.get('.idpRow > section.modal__value')
+        .contains('h3', 'Is the data shown incorrect?');
+    });
   });
 
-  it('can show additional attributes', () => {
-    cy.get('ul.consent__attributes--nested')
-      .should('be.hidden');
+  describe('gives explanation about the unique identifier', () => {
+    it('Should not show the number modal on load', () => {
+      cy.get('label[for="consent_disclaimer_number"] + section a[href="https://example.org"]')
+        .should('be.hidden');
+    });
 
-    cy.contains('label', 'Show more information')
-      .click();
-
-    cy.contains('label', 'Show less information');
-    cy.get('ul.consent__attributes--nested')
-      .should('be.visible');
+    it('Should show the number modal after selection', () => {
+      cy.contains('label', 'a number that uniquely identifies you for this service.')
+        .click({force: true})
+        .next()
+        .contains('a', 'Read more');
+    });
   });
 
-  it('shows the correct amount of attributes on load', () => {
-    cy.get('ul.consent__attributes > li')
-      .should('have.length', '7');
-  });
+  describe('can decline consent', () => {
+    it('Should not show the decline consent modal on load', () => {
+      cy.get('label[for="cta_consent_nok"] + section h3')
+        .should('be.hidden');
+    });
 
-  it('gives explanation about the unique identifier', () => {
-    cy.get('label[for="consent_disclaimer_number"] + section a[href="https://example.org"]')
-      .should('be.hidden');
-
-    cy.contains('label', 'a number that uniquely identifies you for this service.')
-      .click({force: true})
-      .next()
-      .contains('a', 'Read more');
-  });
-
-  it('can decline consent', () => {
-    cy.get('label[for="cta_consent_nok"] + section h3')
-      .should('be.hidden');
-
-    cy.contains('label', 'No, I do not agree')
-      .click({force: true})
-      .next()
-      .contains('h3', 'You don\'t want to share your data with the service');
+    it('Should show the decline consent modal after selection', () => {
+      cy.contains('label', 'No, I do not agree')
+        .click({force: true})
+        .next()
+        .contains('h3', 'You don\'t want to share your data with the service');
+    });
   });
 });
--- theme/cypress/integration/skeune/wayf/WayfGeneralBehaviour.js
+++ theme/cypress/integration/skeune/wayf/WayfGeneralBehaviour.js
@@ -2,26 +2,6 @@
  * Tests for behaviour that has nothing to do with clicking / pressing enter.
  */
 context('WAYF behaviour not tied to mouse / keyboard navigation', () => {
-  beforeEach(() => {
-    cy.visit('https://engine.vm.openconext.org/functional-testing/wayf');
-  });
-
-  it('Should show five connected IdPs, the search field and the eduId CTA', () => {
-      cy.visit('https://engine.vm.openconext.org/functional-testing/wayf');
-
-      // Load the connected IdPs by selecting their h3 titles
-      cy.get('.wayf__idp h3')
-        .should('have.length', 5)
-        .eq(2)
-        .should('have.text', 'Login with Connected IdP 3 en');
-
-      // Check if the search field is present
-      cy.get('.wayf__search').should('exist');
-
-      // Check if the eduId is present
-      cy.contains('.remainingIdps__eduId', 'eduID is available as an alternative');
-  });
-
   // todo: test after search is functional
   it.skip('Should be able to search for an idp', () => {
     cy.visit('https://engine.vm.openconext.org/functional-testing/wayf');
@@ -34,7 +14,6 @@ context('WAYF behaviour not tied to mouse / keyboard navigation', () => {
 
   it('Should not show backLink and rememberChoice', () => {
     cy.visit('https://engine.vm.openconext.org/functional-testing/wayf');
-    // Ensure some elements are NOT on the page
     cy.notOnPage('Identity providers without access').should('not.exist');
     cy.notOnPage('Remember my choice');
     cy.notOnPage('Return to service provider');
@@ -64,28 +43,52 @@ context('WAYF behaviour not tied to mouse / keyboard navigation', () => {
     cy.get('wayf__noresults').should('be.visible');
   });
 
-  // todo: test after backLink is added
-  it.skip('Should show the return to service link when configured', () => {
-      cy.visit('https://engine.vm.openconext.org/functional-testing/wayf?connectedIdps=5&backLink=true');
-      cy.onPage('Select an organisation to login to the service');
-      cy.onPage('Return to service provider');
+  describe('Should show five connected IdPs, the search field and the eduId CTA', () => {
+    it('Load the page', () => {
+      cy.visit('https://engine.vm.openconext.org/functional-testing/wayf');
+    });
 
-      // Ensure some elements are NOT on the page
-      cy.notOnPage('Identity providers without access');
-      cy.notOnPage('Remember my choice');
+    it('Get the connected IdPs & check if it\'s correct', () => {
+      cy.get('.wayf__idp h3')
+        .should('have.length', 5)
+        .eq(2)
+        .should('have.text', 'Login with Connected IdP 3 en');
+    });
 
-      // To be more precise, the links should be in the header and footer
-      cy.get('.mod-header .comp-links li:nth-child(1) a').should('have.text', 'Return to service provider');
-      cy.get('.footer-menu .comp-links li:nth-child(2) a').should('have.text', 'Return to service provider');
+    it('Check if the search field is present', () => {
+      cy.get('.wayf__search').should('exist');
+    });
+
+    it('Check if the eduId is present', () => {
+      cy.contains('.remainingIdps__eduId', 'eduID is available as an alternative');
+    });
   });
 
-  it('Should show the remember my choice option', () => {
-      cy.visit('https://engine.vm.openconext.org/functional-testing/wayf?connectedIdps=5&rememberChoiceFeature=true');
-      // Ensure some elements are on the page
-      cy.onPage('Select an organisation to login to the service');
-      cy.onPage('Remember my choice');
-      // Ensure some elements are NOT on the page
-      cy.notOnPage('Identity providers without access');
-      cy.notOnPage('Return to service provideraccess');
+  // todo: test after backLink is added
+  describe('Should show the return to service link when configured', () => {
+      it.skip('Load the page', () => {
+        cy.visit('https://engine.vm.openconext.org/functional-testing/wayf?connectedIdps=5&backLink=true');
+      });
+
+      it.skip('To be more precise, the links should be in the header and footer', () => {
+        cy.get('.mod-header .comp-links li:nth-child(1) a').should('have.text', 'Return to service provider');
+        cy.get('.footer-menu .comp-links li:nth-child(2) a').should('have.text', 'Return to service provider');
+      });
+  });
+
+  describe('Should show the remember my choice option', () => {
+      it('Load the page with rememberChoiceFeature', () => {
+        cy.visit('https://engine.vm.openconext.org/functional-testing/wayf?connectedIdps=5&rememberChoiceFeature=true');
+      });
+
+      it('Ensure some elements are on the page', () => {
+        cy.onPage('Select an organisation to login to the service');
+        cy.onPage('Remember my choice');
+      });
+
+      it('Ensure some elements are NOT on the page', () => {
+        cy.notOnPage('Identity providers without access');
+        cy.notOnPage('Return to service provideraccess');
+      });
   });
 });
