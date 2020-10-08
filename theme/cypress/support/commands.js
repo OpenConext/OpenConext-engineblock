f1a456f4486246cfff70883f12b7b4d752181990
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-08 10:15:53 +0200

    Test the previous selection functionality

--- theme/cypress/support/commands.js
+++ theme/cypress/support/commands.js
@@ -39,3 +39,35 @@ Cypress.Commands.add('pressArrowOnIdpList', (direction, className, index) => {
 
   cy.focused().should('have.class', className).type(`{${direction}arrow}`);
 });
+
+Cypress.Commands.add('selectFirstIdp', (click = true, firstElementSelector = '.wayf__remainingIdps .wayf__idp[data-index="1"]') => {
+  if (click) {
+    cy.get(firstElementSelector).click({force: true});
+    return;
+  }
+
+  cy.get(firstElementSelector).type('{enter}');
+});
+
+Cypress.Commands.add('selectFirstIdpAndReturn', (click = true, url = 'https://engine.vm.openconext.org/functional-testing/wayf') => {
+  cy.selectFirstIdp(click);
+  cy.visit(url);
+});
+
+Cypress.Commands.add('toggleEditButton', (click = true, buttonSelector = '.previousSelection__edit') => {
+  if (click) {
+    cy.get(buttonSelector).click({ force: true });
+    return;
+  }
+
+  cy.get(buttonSelector).type('{enter}');
+});
+
+Cypress.Commands.add('hitDeleteButton', (click = true, deleteSelector = '.wayf__previousSelection .wayf__idp[data-index="1"] .idp__deleteDisable') => {
+  if (click) {
+    cy.get(deleteSelector).focus().click({ force: true });
+    return;
+  }
+
+  cy.get(deleteSelector).focus().type('{enter}');
+});
