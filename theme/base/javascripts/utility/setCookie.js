6bc2951735b7eac4fde9fa42a74f39ef71bfd122
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-11 22:54:53 +0200

    Add extra security options to setcookie

--- theme/base/javascripts/utility/setCookie.js
+++ theme/base/javascripts/utility/setCookie.js
@@ -3,11 +3,15 @@ import * as Cookies from 'js-cookie';
 /**
  * Save a cookie, by default for 365 days
  *
+ * Note: httpOnly is not an option here as it's not supported by js-cookie atm.
+ *
  * @param content       the content you want to put in it
  * @param cookieName    the name for the cookie
- * @param expires       when the cookie expires (number)
+ * @param expires       when the cookie expires (number of days)
  * @param path          on which domain to save it
+ * @param secure        whether or not it's a secure cookie
+ * @param sameSite      whether or not it's samesite
  */
-export const setCookie = (content, cookieName, expires = 365, path = '/') => {
-  Cookies.set(cookieName, content, { expires: expires, path: path });
+export const setCookie = (content, cookieName, expires = 365, path = '/', secure = true, sameSite = 'strict') => {
+  Cookies.set(cookieName, content, { expires: expires, path: path, secure: secure, sameSite: sameSite });
 };
