334210a1447ad9cbbe82a3a65b5746bf65460312
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-13 16:32:56 +0200

    Handle submit

--- theme/base/javascripts/wayf/noAccess/handleCancelNoAccess.js
+++
@@ -1,7 +0,0 @@
-import {toggleVisibility} from '../../utility/toggleVisibility';
-
-export const handleCancelNoAccess = (parentSection, noAccessSection) => {
-  toggleVisibility(noAccessSection);
-  toggleVisibility(parentSection);
-  // todo show banner
-};
