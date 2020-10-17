d8c0c49f026c4d01a570d43eb547c9e4f41bf6f0
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-17 16:46:38 +0200

    Add a11y support when hitting edit/done button

--- theme/base/javascripts/wayf/mouseBehaviour.js
+++ theme/base/javascripts/wayf/mouseBehaviour.js
@@ -1,10 +1,15 @@
 import {submitForm} from './submitForm';
+import {handleAriaPressed} from '../utility/handleAriaPressed';
 
 export const mouseBehaviour = (previouslySelectedIdps) => {
-// allow chosing an idp to login
+  // allow chosing an idp to login
   const idpLists = document
     .querySelectorAll('.wayf__idpList');
   idpLists.forEach(list => list.addEventListener('click', (e) => {
     submitForm(e, previouslySelectedIdps);
   }));
+
+  // add a11y support for all labels with an aria-pressed attribute.
+  const labels = document.querySelectorAll('label[aria-pressed]');
+  handleAriaPressed(labels);
 };
