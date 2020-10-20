86f7942be66bacba2c85c996f8636e9d82feeadd
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-20 18:46:06 +0200

    Add keyboard & mouse tests for consent
    Refactored naming of spec files to be consistent.
    Added keyboard tests for consent.
    Added mouse tests for consent.
    Seperated general tests for consent.

--- theme/base/javascripts/consent/addA11ySupport.js
+++ theme/base/javascripts/consent/addA11ySupport.js
@@ -2,16 +2,23 @@ import {fireClickEvent} from '../utility/fireClickEvent';
 import {animateInteractiveSections} from '../utility/animateInteractiveSections';
 
 export const addAccessibilitySupport = () => {
+  const ENTER      = 13;
+
   /**
    * Ensure hitting enter on a label will trigger the same interaction as it does when clicking it with a mouse.
    * This makes the tooltips / modals reachable by keyboard.
    * It also ensures the "show more info" button is reachable by keyboard.
    */
   document.querySelector('body').addEventListener('keydown', function(e) {
-    if (e.keyCode === 13) {
-      const label = e.target;
-      if (label.tagName === 'LABEL' && label.tabIndex === 0) {
-        fireClickEvent(label);
+    if (e.keyCode === ENTER) {
+      const target = e.target;
+      console.log(e.target);
+      if (target.tagName === 'LABEL' && target.tabIndex === 0) {
+        fireClickEvent(target);
+      }
+
+      if (target.tagName === 'DIV' && target.tabIndex === 0) {
+        fireClickEvent(target.querySelector('label.modal'));
       }
     }
   });
