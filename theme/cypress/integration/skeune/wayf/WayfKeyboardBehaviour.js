ebb68de03fdede81d344a19c13dab65149b55694
Koen Cornelis <kcornelis@ibuildings.nl>
2020-10-20 11:39:41 +0200

    Test the new behaviour

--- theme/cypress/integration/skeune/wayf/WayfKeyboardBehaviour.js
+++ theme/cypress/integration/skeune/wayf/WayfKeyboardBehaviour.js
@@ -16,11 +16,10 @@ context('WAYF when using the keyboard', () => {
     });
   });
 
-  // todo after adding eduId feature flag this should be adjusted to take that into account
   // todo if html spec is changed, or cypress fixes bug 6207, get rid of the manual focus on search.  See https://github.com/cypress-io/cypress/issues/6207
   describe('Should be able to traverse the remaining idp section with arrow keys', () => {
     it('check if pressing down works as expected', () => {
-      cy.visit('https://engine.vm.openconext.org/functional-testing/wayf');
+      cy.visit('https://engine.vm.openconext.org/functional-testing/wayf?showIdpBanner=1');
       cy.get('.search__field').focus();
       cy.pressArrowOnIdpList('down', 'search__field');
       cy.pressArrowOnIdpList('down', 'wayf__eduIdLink');
@@ -33,7 +32,7 @@ context('WAYF when using the keyboard', () => {
     });
 
     it('check if pressing up works as expected', () => {
-      cy.visit('https://engine.vm.openconext.org/functional-testing/wayf');
+      cy.visit('https://engine.vm.openconext.org/functional-testing/wayf?showIdpBanner=1');
       cy.get('.search__field').focus();
       cy.pressArrowOnIdpList('up', 'search__field');
       cy.pressArrowOnIdpList('up', 'wayf__idp', '5');
@@ -46,7 +45,7 @@ context('WAYF when using the keyboard', () => {
     });
   });
 
-  describe.only('Should show a fully functional no access section when a disabled account is selected', () => {
+  describe('Should show a fully functional no access section when a disabled account is selected', () => {
     it('Should show the no access section on selecting a disabled account', () => {
       cy.visit('https://engine.vm.openconext.org/functional-testing/wayf?displayUnconnectedIdpsWayf=true&unconnectedIdps=5');
       cy.focusAndEnter('.wayf__idp[data-entityid="https://unconnected.example.com/entityid/1"]');
@@ -100,6 +99,23 @@ context('WAYF when using the keyboard', () => {
     it('Should also not show the form fields after selecting a new disabled account', () => {
       cy.get('.noAccess__requestForm fieldset')
         .should('not.be.visible');
+    });
+  });
+
+  describe('Should have a working default Idp Banner', () => {
+    it('Should have a default Idp banner visible', () => {
+      cy.visit('https://engine.vm.openconext.org/functional-testing/wayf?showIdpBanner=1');
+      cy.get('.wayf__eduIdLink').should('be.visible');
+    });
+
+    it('Should scroll to the default Idp when clicking the banner link', () => {
+      cy.visit('https://engine.vm.openconext.org/functional-testing/wayf?connectedIdps=10&defaultIdpEntityId=https://example.com/entityId/9&showIdpBanner=1');
+
+      // click the banner link & check if it did what it should have
+      cy.focusAndEnter('.wayf__eduIdLink');
+      cy.get('#defaultIdp')
+        .should('be.visible')
+        .should('have.focus');
     });
   });
 
