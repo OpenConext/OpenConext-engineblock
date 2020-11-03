fe42b1b38bb392cf6ae0159f3cd4ba98794ccf64
Michiel Kodde <mkodde@ibuildings.nl>
2020-11-03 15:56:46 +0100

    Add the reset search terms button
    It is shown once the user starts typing text into the search bar. When
    the search term is cleared, the poirot icon is shown again. This toggle
    is performed using the toggleSearchAndResetButton function. It is also
    triggered when the user manually clicks the x button.

--- theme/base/javascripts/wayf/searchBehaviour.js
+++ theme/base/javascripts/wayf/searchBehaviour.js
@@ -2,18 +2,22 @@ import {throttle} from '../utility/throttle';
 import {nodeListToArray} from '../utility/nodeListToArray';
 import {searchAndSortIdps} from './search/searchAndSortIdps';
 import {toggleDefaultIdPLinkVisibility} from "./search/toggleDefaultIdPLinkVisibility";
+import {toggleSearchAndResetButton} from "./search/toggleSearchAndResetButton";
 
 export const searchBehaviour = () => {
   const idpList = document.querySelectorAll('.wayf__remainingIdps .wayf__idpList > li');
   const searchBar = document.querySelector('.search__field');
+  const resetButton = document.querySelector('.search__reset');
   const idpArray = nodeListToArray(idpList);
 
   // attach handler to search field
   searchBar.addEventListener('keyup', throttle(event => searchAndSortIdps(idpArray, event.target.value), 250));
   searchBar.addEventListener('keyup', event => toggleDefaultIdPLinkVisibility(event.target.value));
+  searchBar.addEventListener('keyup',  event => toggleSearchAndResetButton(event.target.value));
   searchBar.addEventListener('click', event => searchAndSortIdps(idpArray, event.target.value));
   searchBar.addEventListener('input', event => searchAndSortIdps(idpArray, event.target.value));
 
+  resetButton.addEventListener('click', event => toggleSearchAndResetButton(''));
   // attach handler to search form
   document.querySelector('.wayf__search').addEventListener('submit', event => {
     event.preventDefault();
