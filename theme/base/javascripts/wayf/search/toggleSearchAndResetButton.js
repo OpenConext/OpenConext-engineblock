96677875a2ca4d46064587d0db4518db8edd6e6a
Michiel Kodde <mkodde@ibuildings.nl>
2020-11-17 14:29:22 +0100

    Show Poirot on page load
    And hide the reset button visually. When typing, show the reset button
    (remove the class).
    
    The background has also been removed from the poirot element. This
    prevents setting extra logic to color with the parent elements focus
    style. E.g. when the search bar is focussed, you want the poirot to be
    focussed too

--- theme/base/javascripts/wayf/search/toggleSearchAndResetButton.js
+++ theme/base/javascripts/wayf/search/toggleSearchAndResetButton.js
@@ -4,6 +4,9 @@ import {showElement} from "../../utility/showElement";
 export const toggleSearchAndResetButton = (searchTerm) => {
   const searchButton = document.querySelector('.search__submit');
   const resetButton = document.querySelector('.search__reset');
+  if (resetButton.classList.contains('visually-hidden')) {
+    resetButton.classList.remove('visually-hidden');
+  }
 
   if (searchTerm !== '') {
     hideElement(searchButton);
