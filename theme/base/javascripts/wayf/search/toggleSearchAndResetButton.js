88fb1642fd16ce6b50de5b1fedbfd966d617e25c
Michiel Kodde <mkodde@ibuildings.nl>
2020-11-19 10:10:26 +0100

    Add IdP list reset to form reset action

--- theme/base/javascripts/wayf/search/toggleSearchAndResetButton.js
+++ theme/base/javascripts/wayf/search/toggleSearchAndResetButton.js
@@ -1,7 +1,8 @@
 import {hideElement} from "../../utility/hideElement";
 import {showElement} from "../../utility/showElement";
+import {searchAndSortIdps} from "./searchAndSortIdps";
 
-export const toggleSearchAndResetButton = (searchTerm) => {
+export const toggleSearchAndResetButton = (idpArray, searchTerm) => {
   const searchButton = document.querySelector('.search__submit');
   const resetButton = document.querySelector('.search__reset');
 
@@ -9,6 +10,8 @@ export const toggleSearchAndResetButton = (searchTerm) => {
     hideElement(searchButton);
     showElement(resetButton);
   } else {
+    // Reset the list/search results
+    searchAndSortIdps(idpArray, searchTerm);
     showElement(searchButton);
     hideElement(resetButton);
   }
