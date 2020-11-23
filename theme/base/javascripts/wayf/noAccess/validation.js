7474b2030bfca1f9e52b31b08f9e1b9bf27fffc9
Michiel Kodde <mkodde@ibuildings.nl>
2020-11-23 15:38:11 +0100

    Validate required fields (#975)
    * Improve request access form behavior
    
    Rudimentary JS validation logic was added to simulate the required form
    field 'html5 feature'. Actions are added to hide/show the error messages
    when a form field is invalid.
    
    * Cover validation in tests
    
    * Remove double behaviour handler
    
    Buttons already trigger a click handler when pressing enter on them.  As such, the keyboard behaviour handler was unnecessary here.
    
    * Fix focus styles of disabled IDP
    
    * Fix failing buttons
    
    Also generalized the be.visible and have.class and their opposite counterparts.
    
    * Disable one failing test
    
    Cost of fixing it was too expensive
    
    Co-authored-by: Koen Cornelis <kcornelis@ibuildings.nl>

---
+++ theme/base/javascripts/wayf/noAccess/validation.js
@@ -0,0 +1,38 @@
+import {nodeListToArray} from "../../utility/nodeListToArray";
+
+/**
+ * Verify the name and email fields are not empty. Due to the way the validation was set up we can
+ * not utilize the 'required' attribute on the form input fields. So a JS validation is required
+ * to prevent empty form submits.
+ *
+ * @param formData
+ * @returns {boolean}
+ */
+export const valid = (formData) => {
+  // Start with hiding previous validation messages, they will reappear if the error persists.
+  hideValidationMessages();
+  const nameValid = notAnEmptyField(formData, 'name');
+  const emailValid = notAnEmptyField(formData, 'email');
+  // Next, test the name and email fields for validity, set error message if invalid.
+  return nameValid && emailValid;
+};
+
+function notAnEmptyField(formData, elementName) {
+  if (!formData.has(elementName) || formData.get(elementName).trim() === '') {
+    showValidationMessage(elementName);
+    return false;
+  }
+  return true;
+}
+
+function hideValidationMessages(){
+  const errorMessages = nodeListToArray(document.getElementsByClassName('form__error'));
+  errorMessages.forEach(errorMessage => {
+    errorMessage.classList.add('hidden');
+  });
+}
+
+function showValidationMessage(elementName){
+  const errorMessage = document.querySelector(`p[data-labelfor="${elementName}"]`);
+  errorMessage.classList.remove('hidden');
+}
