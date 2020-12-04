6e68050544f466c02957efd418775b51b2183cc7
Koen Cornelis <kcornelis@ibuildings.nl>
2020-12-04 09:44:19 +0100

    Separate translations / theme (#998)
    * Clean out translations in skeune
    
    * Clean out openconext-specific translations

--- theme/skeune/translations/messages.nl.php
+++ theme/skeune/translations/messages.nl.php
@@ -2,12 +2,12 @@
 return [
     // General
     'button'        =>  'Knop: ',
-    'logo'          =>  'logo',
     'required'      =>  'Verplicht',
     'search'        =>  'Zoeken',
     'send_request'  =>  'Verzoek sturen',
     'back'          =>  'Terug',
     'noscript'      => 'Om deze pagina optimaal te laten functioneren moet JavaScript aan staan.',
+    'log_in_to'                 => 'Selecteer een %organisationNoun% en login bij %arg1%',
 
     // Forms
     'form_general_error'    =>  'Er ging iets fout tijdens het insturen van uw formulier.  Dit kan een probleem zijn met uw internetverbinding of iets anders.  Controleer uw invoer en probeer het later opnieuw.  Mocht het probleem zich blijven voordoen, neem dan contact op met uw beheerder.',
@@ -19,7 +19,6 @@ return [
     'wayf_apu'                  => 'Probeert u het opnieuw met andere zoektermen',
     'wayf_noscript_warning_intro'     => 'Zonder JavaScript zal u geen vorig gebruikte accounts kunnen onthouden.  Indien u deze functionaliteit toch wenst te gebruiken, vragen wij u vriendelijk om JavaScript aan te zetten.',
     'wayf_noscript_warning_end'     => 'U kan, vanzelfsprekend, nog steeds inloggen.',
-    'wayf_no_access_account'    => 'Geen toegang met deze account',
     'wayf_delete_account'       => 'Verwijderen uit uw accounts',
     'wayf_remaining_idps_title' => 'Voeg een account toe uit de onderstaande lijst',
     'wayf_select_account'       => 'Selecteer een account uit de onderstaande lijst',
@@ -27,8 +26,9 @@ return [
     'wayf_search_aria'          => 'Zoek een identity provider',
     'wayf_your_accounts'        => 'Uw accounts',
     'wayf_add_account'          => 'Gebruik een andere account',
-    'wayf_no_access_helpdesk'   => 'U kan toegang vragen tot deze dienst.  We sturen deze aanvraag door naar de helpdesk van uw instituut.',
     'wayf_no_access'            => 'Sorry, geen toegang voor deze account',
+    'wayf_no_access_account'    => 'Geen toegang met deze account',
+    'wayf_no_access_helpdesk'   => 'U kan toegang vragen tot deze dienst.  We sturen deze aanvraag door naar de helpdesk van uw instituut.',
     'wayf_noaccess_name'        => 'Uw naam',
     'wayf_noaccess_email'       => 'Uw e-mailadres',
     'wayf_noaccess_motivation'  => 'Motivatie',
@@ -36,7 +36,6 @@ return [
     'wayf_defaultIdp_start'     => 'Indien uw %organisation_noun% niet opgelijst staat,',
     'wayf_defaultIdp_linkText'  => 'is %defaultIdpName% beschikbaar als alternatief.',
     'wayf_idp_title_screenreader' => 'Inloggen met ',
-    'log_in_to'                 => 'Selecteer een %organisationNoun% en login bij %arg1%',
 
     // Consent
     'consent_h1_text'   => 'Geeft u toestemming om uw informatie te delen?',
@@ -47,13 +46,13 @@ return [
     'consent_identifier_explanation'    => 'De id voor deze dienst wordt gegenereerd door %suite_name% en is verschillend voor elke dienst waarvan u gebruikt maakt via %suite_name%. De dienst kan u daarom herkennen als dezelfde gebruiker wanneer u terugkeert.  Diensten kunnen u echter niet herkennen als dezelfde gebruiker wanneer zij gegevens uitwisselen.',
     'consent_provided_by'   => 'aangeboden door',
     'consent_tooltip_screenreader'  => 'Waarom hebben we deze informatie nodig?',
-    'consent_nojs_skeune'   => 'Tooltips / modals op deze pagina hebben om te werken met het toetsenbord JavaScript nodig.  Indien u enkel een toetsenbord gebruikt, gelieve dan JavaScript in te schakelen voor deze functionaliteit.',
+    'consent_nojs'   => 'Tooltips / modals op deze pagina hebben om te werken met het toetsenbord JavaScript nodig.  Indien u enkel een toetsenbord gebruikt, gelieve dan JavaScript in te schakelen voor deze functionaliteit.',
     'consent_disclaimer_privacy_statement' => 'heeft deze informatie nodig om te kunnen werken',
     'consent_disclaimer_privacy_read'    => 'lees hun',
     'consent_disclaimer_privacy_policy'  => 'privacy beleid',
     'consent_disclaimer_secure' => 'wordt gebruikt door uw organisatie om informatie op een veilige manier te versturen naar uw instituut (leer meer over',
-    'consent_slidein_reject_text_skeune_header'    => 'U wil geen gegevens delen met deze dienst',
-    'consent_slidein_reject_text_skeune_body'    => 'De dienst waar u bij wil inloggen heeft deze gegevens nodig om te kunnen functioneren.  Indien u verkiest om uw data niet te delen, kan u de dienst niet gebruiken.  Door uw browser of door deze tab te sluiten verhinderd u dat uw informatie gedeeld wordt.  Mocht u later van gedacht veranderen, kan u opnieuw inloggen bij deze dienst en krijgt u dit scherm opnieuw te zien.',
+    'consent_reject_text_skeune_header'    => 'U wil geen gegevens delen met deze dienst',
+    'consent_reject_text_skeune_body'    => 'De dienst waar u bij wil inloggen heeft deze gegevens nodig om te kunnen functioneren.  Indien u verkiest om uw data niet te delen, kan u de dienst niet gebruiken.  Door uw browser of door deze tab te sluiten verhinderd u dat uw informatie gedeeld wordt.  Mocht u later van gedacht veranderen, kan u opnieuw inloggen bij deze dienst en krijgt u dit scherm opnieuw te zien.',
     'consent_nok_title'     => "U wil geen gegevens delen met deze dienst",
     'consent_nok_text'      => "De dienst waar u bij wil inloggen heeft deze gegevens nodig om te kunnen functioneren.  Indien u verkiest om uw data niet te delen, kan u de dienst niet gebruiken.  Door uw browser of door deze tab te sluiten verhinderd u dat uw informatie gedeeld wordt.  Mocht u later van gedacht veranderen, kan u opnieuw inloggen bij deze dienst en krijgt u dit scherm opnieuw te zien.",
     'consent_groupmembership_show_more'     => 'Toon meer',
