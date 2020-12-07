6fb8e37b550d42a81c93faffe45fda48239e698c
Koen Cornelis <kcornelis@ibuildings.nl>
2020-12-07 15:53:06 +0100

    Use organisation noun for request access message

--- theme/skeune/translations/messages.en.php
+++ theme/skeune/translations/messages.en.php
@@ -28,7 +28,7 @@ return [
     'wayf_add_account'          => 'Use another account',
     'wayf_no_access'            => 'Sorry, no access for this account',
     'wayf_no_access_account'    => 'No access with this account',
-    'wayf_no_access_helpdesk'   => 'If you want, you can request access to this service. We will send the request to the helpdesk of your institution.',
+    'wayf_no_access_helpdesk'   => 'If you want, you can request access to this service. We will send the request to the helpdesk of your %orgNoun%.',
     'wayf_noaccess_name'        => 'Your name',
     'wayf_noaccess_email'       => 'Your emailaddress',
     'wayf_noaccess_motivation'  => 'Motivation',
