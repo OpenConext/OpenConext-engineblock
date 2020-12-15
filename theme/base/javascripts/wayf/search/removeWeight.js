94349e7c90c63c45be76d1881d133f7d38218a7a
Koen Cornelis <kcornelis@ibuildings.nl>
2020-12-15 12:22:18 +0100

    Ensure everything works in IE, both functionally and styling-wise (#1026)
    Styling:
    
        correctly position tooltips
        ensure opentoggle still works
        remove bg-image for nok-button
        ensure idprow looks good
    
    Functionality:
    
        ensure no errors are in the console
        add a bunch of polyfills

--- theme/base/javascripts/wayf/search/removeWeight.js
+++ theme/base/javascripts/wayf/search/removeWeight.js
@@ -1,3 +1,10 @@
 export const removeWeight = (element) => {
-  element.removeAttribute('data-weight');
+  try {
+    element.removeAttribute('data-weight');
+  } catch (e) {
+    // We are now in IE11 territory
+    if (!!element) {
+      element.setAttribute('data-weight', null);
+    }
+  }
 };
