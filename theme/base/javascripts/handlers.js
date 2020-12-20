942ad462547af71be637cbc41d43f49b85b1d9a9
Koen Cornelis <kcornelis@ibuildings.nl>
2020-12-20 15:10:51 +0100

    Make initializeConsent overridable

--- theme/base/javascripts/handlers.js
+++ theme/base/javascripts/handlers.js
@@ -1,8 +1,14 @@
 import {enterHandler} from './consent/enterHandler';
 import {keyboardBehaviour} from './consent/keyboardBehaviour';
 import {animateInteractiveSections} from './utility/animateInteractiveSections';
+import {addAccessibilitySupport} from './consent/addA11ySupport';
+import {addNokListener} from './consent/addNokListener';
 
 /** Consent Handlers **/
+export const consentCallbackAfterLoad = () => {
+  addAccessibilitySupport();
+  addNokListener();
+};
 export const consentEnterHandler = (target) => enterHandler(target);
 export const consentKeyboardBehaviourHandler = keyboardBehaviour;
 export const consentAnimateInteractiveElements = (selector) => animateInteractiveSections(selector);
