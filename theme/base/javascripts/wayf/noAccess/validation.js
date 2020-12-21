0f2cb1c33cec72d6fa0bcebe467b62dc31cba07f
Koen Cornelis <kcornelis@ibuildings.nl>
2020-12-21 22:09:12 +0100

    Ensure validated fields are overrideable

--- theme/base/javascripts/wayf/noAccess/validation.js
+++ theme/base/javascripts/wayf/noAccess/validation.js
@@ -1,33 +1,42 @@
 import {nodeListToArray} from "../../utility/nodeListToArray";
+import {formErrorClass, noAccessFieldsToValidy} from '../../selectors';
 
 /**
  * Verify the name and email fields are not empty. Due to the way the validation was set up we can
  * not utilize the 'required' attribute on the form input fields. So a JS validation is required
  * to prevent empty form submits.
  *
+ * Starts by hiding previous validation messages, they will reappear if the error persists.
+ *
  * @param formData
- * @returns {boolean}
+ * @returns {boolean}   true if all is valid, false otherwise
  */
 export const valid = (formData) => {
-  // Start with hiding previous validation messages, they will reappear if the error persists.
+  let isValid = true;
+
   hideValidationMessages();
-  const nameValid = notAnEmptyField(formData, 'name');
-  const emailValid = notAnEmptyField(formData, 'email');
-  // Next, test the name and email fields for validity, set error message if invalid.
-  return nameValid && emailValid;
+  noAccessFieldsToValidy.forEach(fieldName => {
+    if (isAnEmptyField(formData, fieldName)) {
+      isValid = false;
+    }
+  });
+
+  return isValid;
 };
 
-function notAnEmptyField(formData, elementName) {
+function isAnEmptyField(formData, elementName) {
   const value = document.getElementById(elementName).value.trim();
+
   if (!value) {
     showValidationMessage(elementName);
-    return false;
+    return true;
   }
-  return true;
+
+  return false;
 }
 
 function hideValidationMessages(){
-  const errorMessages = nodeListToArray(document.getElementsByClassName('form__error'));
+  const errorMessages = nodeListToArray(document.getElementsByClassName(formErrorClass));
   errorMessages.forEach(errorMessage => {
     errorMessage.classList.add('hidden');
   });
