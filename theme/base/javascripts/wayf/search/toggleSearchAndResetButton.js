47531acac5cf65910bec2032e51b829eeda43380
Koen Cornelis <kcornelis@ibuildings.nl>
2021-01-25 12:08:35 +0100

    Ensure hitting reset shows default idp
    Prior to this change, when the user pressed the reset button whilst searching; the default idp remained hidden.
    
    This change ensures the default idp is shown in the above scenario.  It also adds the visibility of the default idp to the test, so if it fails it'll get detected.
    
    [Issue brought up as part of the feedback round](https://wiki.surfnet.nl/display/coininfra/Bevindingen+nav+tests+door+supportteam)

--- theme/base/javascripts/wayf/search/toggleSearchAndResetButton.js
+++ theme/base/javascripts/wayf/search/toggleSearchAndResetButton.js
@@ -2,6 +2,7 @@ import {hideElement} from "../../utility/hideElement";
 import {showElement} from "../../utility/showElement";
 import {searchAndSortIdps} from "./searchAndSortIdps";
 import {searchResetSelector, searchSubmitSelector} from '../../selectors';
+import {toggleDefaultIdPLinkVisibility} from './toggleDefaultIdPLinkVisibility';
 
 export const toggleSearchAndResetButton = (idpArray, searchTerm) => {
   const searchButton = document.querySelector(searchSubmitSelector);
@@ -16,6 +17,7 @@ export const toggleSearchAndResetButton = (idpArray, searchTerm) => {
   } else {
     // Reset the list/search results
     searchAndSortIdps(idpArray, searchTerm);
+    toggleDefaultIdPLinkVisibility('');
     showElement(searchButton, true);
     hideElement(resetButton, true);
   }
