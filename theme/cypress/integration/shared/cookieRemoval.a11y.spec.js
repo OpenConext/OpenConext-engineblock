a5c8281bacecc1e4e8442120e0747b71cbd9c935
Koen Cornelis <kcornelis@ibuildings.nl>
2021-04-28 08:55:23 +0200

    Make cookieremoval test a manual one

--- theme/cypress/integration/shared/cookieRemoval.a11y.spec.js
+++ theme/cypress/integration/shared/cookieRemoval.a11y.spec.js
@@ -1,27 +1,9 @@
 import {terminalLog} from '../../functions/terminalLog';
 
-context('Cookie removal page verify a11y', () => {
-  /**
-   * Setting the required parameter.yml variables and then clearing cache to execute this test correctly.
-   * Using library js-yaml, repo can be found at: https://github.com/nodeca/js-yaml
-   * Tutorial can be found at https://stackabuse.com/reading-and-writing-yaml-to-a-file-in-node-js-javascript/
-   */
-  before(() => {
-    try {
-      const fs = require('fs');
-      const yaml = require('js-yaml');
-      const config = `${__dirname}/../../app/config/parameters.yml`;
-
-      const fileContents = fs.readFileSync(config, 'utf8');
-      const parameters = yaml.safeLoadAll(fileContents);
-      parameters[0].parameters['wayf.remember_choice'] = true;
-      fs.writeFileSync(config, yaml.safeDump(parameters[0]));
-      executeShellCommand(`php bin/console cache:clear`);
-    } catch(e) {
-      console.log(e);
-    }
-  });
-
+/**
+ * This doesn't run in CI, which is why it's skipped.  You can run it locally by setting the wayf.remember_choice flag to true in parameters.yml.
+ */
+context.skip('Cookie removal page verify a11y', () => {
   beforeEach(() => {
     cy.visit('https://engine.vm.openconext.org/authentication/idp/remove-cookies');
   });
@@ -35,20 +17,3 @@ context('Cookie removal page verify a11y', () => {
     cy.htmlvalidate();
   });
 });
-
-function executeShellCommand(command) {
-  const { exec } = require('child_process');
-  exec(command, (error, stdOut, stdError) => {
-    if (error) {
-      console.log(`exec error: ${error}`);
-      return;
-    }
-
-    if (!stdOut && stdError) {
-      console.log(`error info: ${stdError}`);
-    }
-
-    console.log(stdOut);
-    return process.exit(0);
-  });
-}
