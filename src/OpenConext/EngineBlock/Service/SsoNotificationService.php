132dd4d8369036dccb53ac7c4126cd6f0c6cf019
Vincent Jong <jong.vincent@kpmg.nl>
2021-07-16 11:09:30 +0200

    Add feature for SSO Notifications - code improvement

--- src/OpenConext/EngineBlock/Service/SsoNotificationService.php
+++ src/OpenConext/EngineBlock/Service/SsoNotificationService.php
@@ -37,8 +37,9 @@ class SsoNotificationService
     private $logger;
 
     public function __construct(string $encryptionKey, string $encryptionSalt, string $encryptionAlgorithm,
-                                LoggerInterface $logger)
-    {
+        LoggerInterface $logger
+    ) {
+    
         $this->encryptionKey = $encryptionKey;
         $this->encryptionSalt = $encryptionSalt;
         $this->encryptionAlgorithm = $encryptionAlgorithm;
@@ -48,8 +49,8 @@ class SsoNotificationService
     /**
      * Parses the SSO notification cookie and if successful starts an authentication with the parsed IdP.
      *
-     * @param ParameterBag $cookies the cookies in the current HTTP request
-     * @param EngineBlock_Corto_ProxyServer $server the proxy server to start authentication from
+     * @param  ParameterBag                  $cookies the cookies in the current HTTP request
+     * @param  EngineBlock_Corto_ProxyServer $server  the proxy server to start authentication from
      * @return string the entity ID of a known Identity Provider, otherwise an empty string
      */
     public function handleSsoNotification(ParameterBag $cookies, EngineBlock_Corto_ProxyServer $server): string
@@ -66,8 +67,10 @@ class SsoNotificationService
                     $this->logger->warning("SSO notification found for unknown IdP: '$idpEntityId'");
                 }
             } else {
-                $this->logger->warning("Field '" . self::FIELD_ENTITY_ID . "' not found in parsed SSO " .
-                    "notification: " . json_encode($parsedSsoNotification));
+                $this->logger->warning(
+                    "Field '" . self::FIELD_ENTITY_ID . "' not found in parsed SSO " .
+                    "notification: " . json_encode($parsedSsoNotification)
+                );
             }
         }
 
@@ -77,10 +80,11 @@ class SsoNotificationService
     /**
      * Retrieves the SSO notification cookie from the provided set of cookies.
      *
-     * @param ParameterBag $cookies the set of cookies to retrieve the SSO notification cookie from
+     * @param  ParameterBag $cookies the set of cookies to retrieve the SSO notification cookie from
      * @return mixed|null the SSO notification cookie or null if not present
      */
-    public function getSsoCookie(ParameterBag $cookies) {
+    public function getSsoCookie(ParameterBag $cookies) 
+    {
         return $cookies->get(self::SSO_NOT_COOKIE_NAME);
     }
 
@@ -89,7 +93,7 @@ class SsoNotificationService
      * The provided SSO notification should be a base64 string of a cipher to decrypt and an initialization vector to
      * decrypt with.
      *
-     * @param string $ssoNotification the SSO notification as a base64 string
+     * @param  string $ssoNotification the SSO notification as a base64 string
      * @return array array containing the data of the SSO notification or an empty array in case of an error
      */
     private function parseSsoNotification(string $ssoNotification): array
@@ -101,15 +105,19 @@ class SsoNotificationService
         $iv = substr($base64Decoded, 0, self::IV_SIZE);
         $cipherText = substr($base64Decoded, self::IV_SIZE);
         // Construct encryption key
-        $key = hash_pbkdf2('sha256', $this->encryptionKey, $this->encryptionSalt, self::ITERATION_COUNT,
-            self::KEY_SIZE, true);
+        $key = hash_pbkdf2(
+            'sha256', $this->encryptionKey, $this->encryptionSalt, self::ITERATION_COUNT,
+            self::KEY_SIZE, true
+        );
 
         $jsonString = $this->decryptSsoNotification($cipherText, $key, $this->encryptionAlgorithm, $iv);
         try {
             $data = JsonResponseParser::parse($jsonString);
         } catch (InvalidJsonException $exception) {
-            $this->logger->error("Failed to parse JSON string '$jsonString' from SSO notification",
-                array('exception' => $exception));
+            $this->logger->error(
+                "Failed to parse JSON string '$jsonString' from SSO notification",
+                array('exception' => $exception)
+            );
         }
         return $data;
     }
@@ -118,19 +126,22 @@ class SsoNotificationService
      * Decrypts the SSO notification using the provided key, encryption algorithm and initialization vector.
      * Returns a JSON string or an empty string if the SSO notification cannot be decrypted.
      *
-     * @param string $ssoNotification the SSO notification to decrypt
-     * @param string $encryptionKey the encryption key to decrypt with
-     * @param string $encryptionAlgorithm the encryption algorithm
-     * @param string $iv the initialization vector to decrypt with
+     * @param  string $ssoNotification     the SSO notification to decrypt
+     * @param  string $encryptionKey       the encryption key to decrypt with
+     * @param  string $encryptionAlgorithm the encryption algorithm
+     * @param  string $iv                  the initialization vector to decrypt with
      * @return string a JSON string or an empty string in case the data could not be decrypted
      */
     private function decryptSsoNotification(string $ssoNotification, string $encryptionKey, string $encryptionAlgorithm,
-                                            string $iv): string
-    {
+        string $iv
+    ): string {
+    
         $data = openssl_decrypt($ssoNotification, $encryptionAlgorithm, $encryptionKey, OPENSSL_RAW_DATA, $iv);
         if (!$data) {
-            $this->logger->error("Failed to decrypt SSO notification '$ssoNotification' using algorithm " .
-                "'$encryptionAlgorithm', returning empty string");
+            $this->logger->error(
+                "Failed to decrypt SSO notification '$ssoNotification' using algorithm " .
+                "'$encryptionAlgorithm', returning empty string"
+            );
 
             return '';
         }
