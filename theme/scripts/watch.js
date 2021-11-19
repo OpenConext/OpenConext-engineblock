2520bb9f36794f4d1289dd0e91c937f44f6c1c74
Stephan Kok <s.kok@kennisnet.nl>
2021-11-19 09:43:27 +0100

    Fix build script to fail on error and add watch script to update the web folder on changes

---
+++ theme/scripts/watch.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+/** Simple script to watch based on CLI or chosen theme in parameters.yml
+ * Using library js-yaml, repo can be found at: https://github.com/nodeca/js-yaml
+ * Tutorial can be found at https://stackabuse.com/reading-and-writing-yaml-to-a-file-in-node-js-javascript/
+ *
+ * Everything else used is part of node
+ *
+ * Use: node build.js or  EB_THEME=skeune node build.js
+ * Replace "skeune" in the above by whatever theme you want to build.
+ **/
+const fs = require('fs');
+const yaml = require('js-yaml');
+const config = `${__dirname}/../../app/config/parameters.yml`;
+
+try {
+  console.log('Reading contents of parameters.yml.\n');
+  const fileContents = fs.readFileSync(config, 'utf8');
+  const parameters = yaml.safeLoadAll(fileContents);
+  const theme = process.env.EB_THEME || parameters[0].parameters['theme.name'] || 'skeune';
+
+  console.log(`Using theme ${theme} to run the watch.\n`);
+  executeShellCommand(`cd ${__dirname}/.. && EB_THEME=${theme} npm run watch:css`);
+  executeShellCommand(`cd ${__dirname}/.. && EB_THEME=${theme} npm run watch:js`);
+} catch (e) {
+  console.log(e);
+}
+
+function executeShellCommand(command) {
+  var exec = require('child_process').exec;
+  var process = exec(command);
+
+  process.stdout.on('data', function (data) {
+    console.log(data);
+  });
+  process.stderr.on('data', function (data) {
+    console.log(data);
+  });
+}
