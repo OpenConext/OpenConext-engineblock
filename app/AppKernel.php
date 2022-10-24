6ea13be7668baab1bd49e670a3412e98e71e2992
Michiel Kodde <mkodde@ibuildings.nl>
2022-10-24 11:33:30 +0200

    Uninstall Sensio distribution bundle
    The distribution bundle is not used in the project anymore. We used to
    include it to use the security checks. And possibly relied on it's
    post-composer-script running feature.
    
    Removing this legacy, no longer maintained, and conflicting bundle
    is the way forward.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
 
             // Sensio helpers
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
             new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
 
             // Doctrine integration
