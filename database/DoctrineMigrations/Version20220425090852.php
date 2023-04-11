3874c01311c447527ccde5223bb0738294826f64
Thijs Kinkhorst <thijs@kinkhorst.com>
2023-04-11 12:07:17 +0200

    Drop unused index deleted_at from migration

--- database/DoctrineMigrations/Version20220425090852.php
+++ database/DoctrineMigrations/Version20220425090852.php
@@ -17,7 +17,6 @@ final class Version20220425090852 extends AbstractMigration
 
         $this->addSql('ALTER TABLE consent DROP PRIMARY KEY');
         $this->addSql('ALTER TABLE consent ADD deleted_at DATETIME NOT NULL DEFAULT "0000-00-00 00:00:00"');
-        $this->addSql('CREATE INDEX deleted_at ON consent (deleted_at)');
         $this->addSql('ALTER TABLE consent ADD PRIMARY KEY (hashed_user_id, service_id, deleted_at)');
     }
 
@@ -26,7 +25,6 @@ final class Version20220425090852 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP INDEX deleted_at ON consent');
         $this->addSql('ALTER TABLE consent DROP PRIMARY KEY');
         $this->addSql('ALTER TABLE consent DROP deleted_at');
         $this->addSql('ALTER TABLE consent ADD PRIMARY KEY (hashed_user_id, service_id)');
