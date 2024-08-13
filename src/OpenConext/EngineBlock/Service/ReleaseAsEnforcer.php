9fa7a4d4e6c038a1b79d4ec37c7d0bc6b137166a
Michiel Kodde <mkodde@ibuildings.nl>
2024-08-13 11:57:28 +0200

    Add the ReleaseAsEnforcer
    This actually overwrites the attributes that are about to be released
    and ensures the correct values are overwritten

---
+++ src/OpenConext/EngineBlock/Service/ReleaseAsEnforcer.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Copyright 2024 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenConext\EngineBlock\Service;
+
+use Psr\Log\LoggerInterface;
+
+class ReleaseAsEnforcer implements ReleaseAsEnforcerInterface
+{
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function enforce(array $attributes, array $releaseAsOverrides)
+    {
+        foreach ($releaseAsOverrides as $oldAttributeName => $overrideValue) {
+            $newAttributeName = $overrideValue[0]['release_as'];
+            $attributeValue = $attributes[$oldAttributeName];
+            unset($attributes[$oldAttributeName]);
+            $this->logger->notice(
+                sprintf(
+                    'Releasing attribute "%s" as "%s" as specified in the release_as ARP setting',
+                    $oldAttributeName,
+                    $newAttributeName
+                )
+            );
+            $attributes[$newAttributeName] = $attributeValue;
+        }
+        return $attributes;
+    }
+}
