8dd62f8d575b6bb2e95f01def439c0465c9ae01e
Michiel Kodde <mkodde@ibuildings.nl>
2024-08-15 09:27:42 +0200

    Integrate NameId substitution in AddIdentityAttributes
    The output filter will handle the name id substitution logic. As it
    already juggles the correctly setting of the desired identity (nameid,
    collabpersonid) values.

--- library/EngineBlock/Corto/Filter/Output.php
+++ library/EngineBlock/Corto/Filter/Output.php
@@ -62,7 +62,6 @@ class EngineBlock_Corto_Filter_Output extends EngineBlock_Corto_Filter_Abstract
     {
         $diContainer = EngineBlock_ApplicationSingleton::getInstance()->getDiContainer();
         $logger = EngineBlock_ApplicationSingleton::getLog();
-
         return array(
             // If EngineBlock is in Processing mode (redirecting to it's self)
             // Then don't continue with the rest of the modifications
@@ -84,7 +83,7 @@ class EngineBlock_Corto_Filter_Output extends EngineBlock_Corto_Filter_Abstract
             new EngineBlock_Corto_Filter_Command_ApplyTrustedProxyBehavior($logger),
 
             // Add the appropriate NameID to the 'eduPeronTargetedID' and the Assertions NameId.
-            new EngineBlock_Corto_Filter_Command_AddIdentityAttributes($logger),
+            new EngineBlock_Corto_Filter_Command_AddIdentityAttributes($diContainer->getNameIdSubstituteResolver(), $logger),
 
             // Convert all attributes to their OID format (if known) and add these.
             new EngineBlock_Corto_Filter_Command_DenormalizeAttributes(),
