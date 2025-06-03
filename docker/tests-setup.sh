fb1e139d1b03c22cf49e93047c4ce0a38a248afc
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2025-06-03 20:38:06 +0200

    en de goede dir...

--- docker/tests-setup.sh
+++ docker/tests-setup.sh
@@ -23,7 +23,7 @@ docker compose exec -T engine.dev.openconext.local bash -c '
 
 docker compose exec -T engine.dev.openconext.local bash -c '
     ./app/console cache:clear --env=ci;
-    chmod 1777 app/cache/ci app/cache/logs
+    chmod 1777 app/cache/ci app/logs/ci
 '
 
 docker compose exec -T engine.dev.openconext.local bash -c '
