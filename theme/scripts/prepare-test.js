295d15a715c25f28de98c8a72d085ae43c7b6066
Stephan Kok <kok.stephan@kpmg.nl>
2025-06-03 15:23:51 +0200

    symfony 4 - Upgrade

--- theme/scripts/prepare-test.js
+++ theme/scripts/prepare-test.js
@@ -4,13 +4,13 @@
  * Prepare the application environment to run a Cypress E2E test
  * The prepare-test.js script is tasked with:
  *
- * 1. Update the parameters.yml (setting the Twig template theme)
+ * 1. Update the parameters.yaml (setting the Twig template theme)
  * 2. Clear the CI cache
  * 3. Build the theme using the `yarn build` scripts
  *
  * Note this script is most suitable to use on CI environments.
  * Use with caution on your development environment. As it will
- * overwrite parameters.yml
+ * overwrite parameters.yaml
  */
 const fs = require('fs');
 const yaml = require('js-yaml');
