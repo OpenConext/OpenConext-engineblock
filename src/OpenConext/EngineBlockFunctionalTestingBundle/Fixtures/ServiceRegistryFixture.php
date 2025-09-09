a24d73b036240ceca6831e998db1e475b1c0ac3d
Johan Kromhout <jkromhout@ibuildings.nl>
2025-09-09 15:19:38 +0200

    Resolve multiple Doctrine Entity Managers
    Prior to this change, the tests would fail.
    There were mulitple services that were essentially the same entity manager. But because they were separate, calling `persist` on a service, one instance of the EM would try to insert a new instance, as it was untracked in that instance of the EM.
    
    This change injects the same EM in all services, eliminating multiple state issues.
    
    However, I have no idea why the extra `persist` calls are needed. But many tests fail if they are absent.
    
    (Also, what was the reason for the compat EM in the first place?)

--- src/OpenConext/EngineBlockFunctionalTestingBundle/Fixtures/ServiceRegistryFixture.php
+++ src/OpenConext/EngineBlockFunctionalTestingBundle/Fixtures/ServiceRegistryFixture.php
@@ -89,6 +89,7 @@ class ServiceRegistryFixture
             );
         }
 
+        // Why is this needed?
         $this->entityManager->persist($entity);
 
         return $entity;
@@ -211,6 +212,8 @@ QUERY;
         foreach ($sps as $spEntityId) {
             $sp = $this->repository->findServiceProviderByEntityId($spEntityId['entity_id']);
             $sp->allowedIdpEntityIds[] = $idp->entityId;
+
+            // Why is this needed?
             $this->entityManager->persist($sp);
         }
 
@@ -368,7 +371,8 @@ QUERY;
     public function allowAttributeValueForSp($entityId, $arpAttribute, $attributeValue, $attributeSource = null, $motivation = null)
     {
         /** @var AttributeReleasePolicy $arp */
-        $arp = $this->getServiceProvider($entityId)->attributeReleasePolicy;
+        $entity = $this->getServiceProvider($entityId);
+        $arp = $entity->attributeReleasePolicy;
 
         $rules = [];
 
@@ -388,7 +392,7 @@ QUERY;
 
         $rules[$arpAttribute] = [$arpRule];
 
-        $this->getServiceProvider($entityId)->attributeReleasePolicy = new AttributeReleasePolicy($rules);
+        $entity->attributeReleasePolicy = new AttributeReleasePolicy($rules);
 
         return $this;
     }
