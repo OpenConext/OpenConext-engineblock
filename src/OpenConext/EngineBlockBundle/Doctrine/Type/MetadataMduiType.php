e9849bff0a5225f0712f1fafa051f58096798edb
Johan Kromhout <johan.kromhout@dawn.tech>
2026-02-26 15:48:14 +0100

    Prevent migration to json
    Prior to this change, doctrine wants to change the longtext types of consent_settings and idp_discoveries to `json`.
    This has some implications, as at a db level, as it requires a migration.
    This migration is not strictly needed.
    
    Ensure no database migrations are applied by configuring doctrine to use the old legacy mappings, until we have reason to upgrade in a coordinated operation.
    
    Even with this change, doctrine wants to remove the comments. But we can safely apply this in the next release, without causing any big migrations.

--- src/OpenConext/EngineBlockBundle/Doctrine/Type/MetadataMduiType.php
+++ src/OpenConext/EngineBlockBundle/Doctrine/Type/MetadataMduiType.php
@@ -32,8 +32,8 @@ class MetadataMduiType extends Type
     {
         // We want a `TEXT` field declaration in our column, the `LONGTEXT` default causes issues when running the
         // DBMS in strict mode.
-        $column['length'] = 65535;
-        return $platform->getJsonTypeDeclarationSQL($column);
+        $column['length'] = null; // Ensure a longtext, this is currently on prod.
+        return $platform->getClobTypeDeclarationSQL($column);
     }
 
     public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
