9a4aa7c8f2339a616bc8b621d4a3d187a46b1d17
Kay Joosten <kay.joosten@dawn.tech>
2026-04-17 10:51:48 +0200

    Call Di-Container once

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,30 +21,6 @@ use App\Kernel;
 define('TEST_RESOURCES_DIR', __DIR__ . '/resources');
 
 require_once realpath(__DIR__) . '/../vendor/autoload.php';
-
-$worktreeRoot   = (string) realpath(__DIR__ . '/..');
-$vendorRealPath = (string) realpath(__DIR__ . '/../vendor');
-if (!str_starts_with($vendorRealPath, $worktreeRoot . DIRECTORY_SEPARATOR)) {
-    $worktreeLibrary = $worktreeRoot . '/library';
-    $worktreeSrc     = $worktreeRoot . '/src';
-    spl_autoload_register(static function (string $class) use ($worktreeLibrary, $worktreeSrc): bool {
-        $psr0File = $worktreeLibrary . '/' . str_replace('_', '/', $class) . '.php';
-        if (file_exists($psr0File)) {
-            require $psr0File;
-            return true;
-        }
-        if (str_starts_with($class, 'OpenConext\\')) {
-            $relative = substr($class, strlen('OpenConext\\'));
-            $psr4File = $worktreeSrc . '/OpenConext/' . str_replace('\\', '/', $relative) . '.php';
-            if (file_exists($psr4File)) {
-                require $psr4File;
-                return true;
-            }
-        }
-        return false;
-    }, true, true);
-}
-
 require_once realpath(__DIR__) . '/../src/Kernel.php';
 
 $kernel = new Kernel('test', true);
